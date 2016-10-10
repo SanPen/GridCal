fc0acc40e577de5817f01de62f8abdb122847a68
Santiago Peñate Vera <SanPen@users.noreply.github.com>
2016-10-10 09:11:45 +0200

    Update Helm.py

--- UnderDevelopment/GridCal/grid/Helm.py
+++ UnderDevelopment/GridCal/grid/Helm.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
+# This file is part of GridCal.
+#
+# GridCal is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GridCal is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GridCal.  If not, see <http://www.gnu.org/licenses/>.
 
 import Desarrollos.power_flow_research.example_grids as grids
 import numpy as np
