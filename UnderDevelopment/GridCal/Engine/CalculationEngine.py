d4564e9a1093b1cbe17bf807d829576db2837ef2
santi <santiago.penate.vera@gmail.com>
2018-01-20 21:34:11 +0100

    Continued the vast refactor on how power flow data is treated

--- UnderDevelopment/GridCal/Engine/CalculationEngine.py
+++ UnderDevelopment/GridCal/Engine/CalculationEngine.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with GridCal.  If not, see <http://www.gnu.org/licenses/>.
 
-__GridCal_VERSION__ = 1.97
+__GridCal_VERSION__ = 1.98
 
 import os
 import pickle as pkl
