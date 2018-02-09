f78c06742b498fb8f19c2fb1004658d1c895258d
Santiago Peñate Vera <spenate@indra.es>
2018-02-09 10:34:17 +0100

    Fixed auto-link in import

--- UnderDevelopment/GridCal/Gui/ProfilesInput/profile_dialogue.py
+++ UnderDevelopment/GridCal/Gui/ProfilesInput/profile_dialogue.py
@@ -359,7 +359,11 @@ class ProfileInputGUI(QtWidgets.QDialog):
             idx_s = 0
             for source in self.profile_names:
 
-                if self.normalize_string(source) in self.normalize_string(objective.name) or source in objective:
+                if self.normalize_string(source) in self.normalize_string(objective.name) \
+                        or self.normalize_string(objective.name) in self.normalize_string(source)  \
+                        or source in objective.name \
+                        or objective.name in source:
+
                     self.make_association(idx_s, idx_o, mult)
 
                 idx_s += 1
