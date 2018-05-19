d19b846e71710fa62241cb8319b7e8470bbc4fcb
santi <santiago.penate.vera@gmail.com>
2018-05-19 17:57:44 +0200

    fixed time events plot wrong behaviour

--- UnderDevelopment/GridCal/Engine/CalculationEngine.py
+++ UnderDevelopment/GridCal/Engine/CalculationEngine.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with GridCal.  If not, see <http://www.gnu.org/licenses/>.
 
-__GridCal_VERSION__ = 2.293
+__GridCal_VERSION__ = 2.294
 
 import os
 import pickle as pkl
