c271578017db53eb076b824ffdd2d3269fd845e8
santi <santiago.penate.vera@gmail.com>
2018-09-22 18:49:46 +0200

    Massive changes in the interfaces and the engine

--- UnderDevelopment/tests/API_test.py
+++ UnderDevelopment/tests/API_test.py
@@ -13,38 +13,31 @@
 # You should have received a copy of the GNU General Public License
 # along with GridCal.  If not, see <http://www.gnu.org/licenses/>.
 
-from GridCal.Engine.CalculationEngine import *
-from GridCal.Engine.PowerFlowDriver import PowerFlowOptions, SolverType
+from GridCal.Engine.All import *
 from matplotlib import pyplot as plt
-
-import os.path, sys
 
 if __name__ == '__main__':
 
-    grid = MultiCircuit()
+    main_circuit = MultiCircuit()
     # fname = '/Data/Doctorado/spv_phd/GridCal_project/GridCal/IEEE_300BUS.xls'
     # fname = 'Pegasus 89 Bus.xlsx'
     # fname = 'Illinois200Bus.xlsx'
     # fname = 'IEEE_30_new.xlsx'
     # fname = 'lynn5buspq.xlsx'
+    # fname = "C:\\Users\\spenate\\Documents\\PROYECTOS\\Sensible\\Evora reduced (no switchs, corrected, profiles 1W@15T).xlsx"
     # fname = '/home/santi/Documentos/GitHub/GridCal/Grids_and_profiles/grids/IEEE_30_new.xlsx'
-    # fname = 'D:\\GitHub\\GridCal\\Grids_and_profiles\\grids\\IEEE_30_new.xlsx'
-    fname = '..\\..\\Grids_and_profiles\\grids\\IEEE 30 Bus with storage.xlsx'
+    fname = 'D:\\GitHub\\GridCal\\Grids_and_profiles\\grids\\IEEE_30_new.xlsx'
+    # fname = 'D:\\GitHub\\GridCal\\Grids_and_profiles\\grids\\IEEE 30 Bus with storage.xlsx'
+    # fname = '/home/santi/Documentos/GitHub/GridCal/Grids_and_profiles/grids/IEEE 30 Bus with storage.xlsx'
     # fname = '/home/santi/Documentos/GitHub/GridCal/Grids_and_profiles/grids/IEEE39.xlsx'
     # fname = '/Data/Doctorado/spv_phd/GridCal_project/GridCal/IEEE_14.xls'
     # fname = '/Data/Doctorado/spv_phd/GridCal_project/GridCal/IEEE_39Bus(Islands).xls'
 
-    if os.path.isfile(fname):
-        print('Reading...')
-        grid.load_file(fname)
-    else:
-        print('Error: File does not exist: {}'.format(fname))
-        sys.exit()
-    grid.compile()
-
-    options = PowerFlowOptions(SolverType.LM, verbose=False, robust=False,
+    print('Reading...')
+    main_circuit.load_file(fname)
+    options = PowerFlowOptions(SolverType.NR, verbose=False, robust=False,
                                initialize_with_existing_solution=False,
-                               multi_core=False, dispatch_storage=True)
+                               multi_core=False, dispatch_storage=True, control_q=False)
 
     # grid.export_profiles('ppppppprrrrroooofiles.xlsx')
     # exit()
@@ -52,114 +45,91 @@ if __name__ == '__main__':
     ####################################################################################################################
     # PowerFlow
     ####################################################################################################################
-    # from GridCal.Engine.PowerFlowDriver import PowerFlow
-    #
-    # print('\n\n')
-    # power_flow = PowerFlow(grid, options)
-    # power_flow.run()
-    #
-    # for c in grid.circuits:
-    #     print(c.name)
-    #     # print(pd.DataFrame(circuit.power_flow_input.Ybus.todense()))
-    #     # print('\tV:', c.power_flow_results.voltage)
-    #     print('\t|V|:', abs(c.power_flow_results.voltage))
-    #     print('\t|Sbranch|:', abs(c.power_flow_results.Sbranch))
-    #     print('\t|loading|:', abs(c.power_flow_results.loading) * 100)
-    #     print('\terr:', c.power_flow_results.error)
-    #     print('\tConv:', c.power_flow_results.converged)
-    #
-    # print('\n\n', grid.name)
-    # print('\t|V|:', abs(grid.power_flow_results.voltage))
-    # print('\t|Sbranch|:', abs(grid.power_flow_results.Sbranch))
-    # print('\t|loading|:', abs(grid.power_flow_results.loading) * 100)
-    # print('\terr:', grid.power_flow_results.error)
-    # print('\tConv:', grid.power_flow_results.converged)
+    print('\n\n')
+    power_flow = PowerFlow(main_circuit, options)
+    power_flow.run()
+
+    print('\n\n', main_circuit.name)
+    print('\t|V|:', abs(power_flow.results.voltage))
+    print('\t|Sbranch|:', abs(power_flow.results.Sbranch))
+    print('\t|loading|:', abs(power_flow.results.loading) * 100)
+    print('\tReport')
+    print(power_flow.results.get_report_dataframe())
 
     ####################################################################################################################
     # Short circuit
     ####################################################################################################################
-    # from GridCal.Engine.ShortCircuitDriver import ShortCircuitOptions, ShortCircuit
-    #
-    # print('\n\n')
-    # print('Short Circuit')
-    # sc_options = ShortCircuitOptions(bus_index=[16])
-    # sc = ShortCircuit(grid, sc_options)
-    # sc.run()
-    #
-    # print('\n\n', grid.name)
-    # print('\t|V|:', abs(grid.short_circuit_results.voltage))
-    # print('\t|Sbranch|:', abs(grid.short_circuit_results.Sbranch))
-    # print('\t|loading|:', abs(grid.short_circuit_results.loading) * 100)
+    print('\n\n')
+    print('Short Circuit')
+    sc_options = ShortCircuitOptions(bus_index=[16])
+    sc = ShortCircuit(main_circuit, sc_options, power_flow.results)
+    sc.run()
+
+    print('\n\n', main_circuit.name)
+    print('\t|V|:', abs(main_circuit.short_circuit_results.voltage))
+    print('\t|Sbranch|:', abs(main_circuit.short_circuit_results.Sbranch))
+    print('\t|loading|:', abs(main_circuit.short_circuit_results.loading) * 100)
 
     ####################################################################################################################
     # Time Series
     ####################################################################################################################
-    from GridCal.Engine.TimeSeriesDriver import TimeSeries, TimeSeriesResultsAnalysis
-    
-    print('Running TS...'
-          '')
-    ts = TimeSeries(grid=grid, options=options, start=0, end=96)
-    ts.run()
+    # print('Running TS...', '')
+    # ts = TimeSeries(grid=main_circuit, options=options, start=0, end=96)
+    # ts.run()
 
-    if grid.time_series_results is not None:
-        print('\n\nVoltages:\n')
-        print(grid.time_series_results.voltage)
-        print(grid.time_series_results.converged)
-        print(grid.time_series_results.error)
+    # TODO: not working due to results analysis of overloads etc...
+    # ts_analysis = TimeSeriesResultsAnalysis(ts.results)
+    # lst = np.array(list(range(ts.results.n)), dtype=int)
+    # ts.results.plot('Bus voltage', indices=lst, names=lst)
+    # ts.results.plot('Bus voltage', indices=list(range(len(main_circuit.buses))), names=main_circuit.bus_names)
 
-        # plot(grid.master_time_array, abs(grid.time_series_results.loading)*100)
-        # show()
-    ts_analysis = TimeSeriesResultsAnalysis(grid.circuits[0].time_series_results)
-    lst = np.array(list(range(ts.results.n)), dtype=int)
-    ts.results.plot('Bus voltage', indices=lst, names=lst)
-
-    plt.figure()
-    batteries = grid.get_batteries()
-    batteries[0].power_array.plot(label='Battery power')
-    batteries[0].energy_array.plot(label='Battery energy')
-    plt.legend()
-
-    plt.show()
+    # plt.figure()
+    # batteries = grid.get_batteries()
+    # batteries[0].power_array.plot(label='Battery power')
+    # batteries[0].energy_array.plot(label='Battery energy')
+    # plt.legend()
 
     ####################################################################################################################
     # Voltage collapse
     ####################################################################################################################
-    # from GridCal.Engine.VoltageCollapseDriver import VoltageCollapseOptions, VoltageCollapseInput, VoltageCollapse
-    #
     # vc_options = VoltageCollapseOptions()
-    # Sbase = zeros(len(grid.buses), dtype=complex)
-    # for c in grid.circuits:
-    #     Sbase[c.bus_original_idx] = c.power_flow_input.Sbus
-    # unitary_vector = -1 + 2 * np.random.random(len(grid.buses))
+    #
+    # # just for this test
+    # numeric_circuit = main_circuit.compile()
+    # numeric_inputs = numeric_circuit.compute()
+    # Sbase = zeros(len(main_circuit.buses), dtype=complex)
+    # Vbase = zeros(len(main_circuit.buses), dtype=complex)
+    # for c in numeric_inputs:
+    #     Sbase[c.original_bus_idx] = c.Sbus
+    #     Vbase[c.original_bus_idx] = c.Vbus
+    #
+    # unitary_vector = -1 + 2 * np.random.random(len(main_circuit.buses))
+    #
     # # unitary_vector = random.random(len(grid.buses))
     # vc_inputs = VoltageCollapseInput(Sbase=Sbase,
-    #                                  Vbase=grid.power_flow_results.voltage,
+    #                                  Vbase=Vbase,
     #                                  Starget=Sbase * (1+unitary_vector))
-    # vc = VoltageCollapse(grid=grid, options=vc_options, inputs=vc_inputs)
+    # vc = VoltageCollapse(circuit=main_circuit, options=vc_options, inputs=vc_inputs)
     # vc.run()
-
     # vc.results.plot()
-    # plt.show()
 
     ####################################################################################################################
     # Monte Carlo
     ####################################################################################################################
-    # print('Running MC...')
-    # mc_sim = MonteCarlo(grid, options, mc_tol=1e-5, max_mc_iter=1000000)
-    # mc_sim.run()
-    # lst = np.array(list(range(mc_sim.results.n)), dtype=int)
-    # mc_sim.results.plot('Bus voltage avg', indices=lst, names=lst)
-    # plt.show()
+    print('Running MC...')
+    mc_sim = MonteCarlo(main_circuit, options, mc_tol=1e-5, max_mc_iter=1000000)
+    mc_sim.run()
+    lst = np.array(list(range(mc_sim.results.n)), dtype=int)
+    mc_sim.results.plot('Bus voltage avg', indices=lst, names=lst)
+    plt.show()
 
     ####################################################################################################################
     # Latin Hypercube
     ####################################################################################################################
-
-    # lhs_sim = LatinHypercubeSampling(grid, options, sampling_points=100)
-    # lhs_sim.run()
-    #
-    # lhs_sim.results.plot('Bus voltage avg')
-
+    print('Running LHC...')
+    lhs_sim = LatinHypercubeSampling(main_circuit, options, sampling_points=100)
+    lhs_sim.run()
+    lhs_sim.results.plot('Bus voltage avg')
 
     ####################################################################################################################
     # Cascading
@@ -179,10 +149,10 @@ if __name__ == '__main__':
     ####################################################################################################################
     # Fuck up the voltage
     ####################################################################################################################
-
+    # print('Run optimization to f**k up the voltage')
     # options = PowerFlowOptions(SolverType.LM, verbose=False, robust=False, initialize_with_existing_solution=False)
-    # opt = Optimize(grid, options, max_iter=10000)
+    # opt = Optimize(main_circuit, options, max_iter=100)
     # opt.run()
     # opt.plot()
-    #
-    # plt.show()
+
+    plt.show()
