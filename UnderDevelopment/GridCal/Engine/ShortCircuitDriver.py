cde06d27ff274680327a0d5619e516a7bbf84de6
santi <santiago.penate.vera@gmail.com>
2018-12-08 12:30:36 +0100

    Added the mid-line fault capabilities (not tested yet)

--- UnderDevelopment/GridCal/Engine/ShortCircuitDriver.py
+++ UnderDevelopment/GridCal/Engine/ShortCircuitDriver.py
@@ -24,7 +24,7 @@ from GridCal.Engine.Numerical.SC import short_circuit_3p
 from GridCal.Engine.CalculationEngine import LINEWIDTH, MultiCircuit
 from GridCal.Engine.PowerFlowDriver import PowerFlowResults
 from GridCal.Engine.IoStructures import CalculationInputs, ResultTypes
-
+from GridCal.Engine.Devices import Branch, Bus
 
 ########################################################################################################################
 # Short circuit classes
@@ -33,14 +33,28 @@ from GridCal.Engine.IoStructures import CalculationInputs, ResultTypes
 
 class ShortCircuitOptions:
 
-    def __init__(self, bus_index, verbose=False):
+    def __init__(self, bus_index=[], branch_index=[], branch_fault_locations=[], branch_fault_impedance=[],
+                 verbose=False):
         """
 
         Args:
-            bus_index: indices of the short circuited buses
-            zf: fault impedance
+            bus_index:
+            branch_index:
+            branch_fault_locations:
+            branch_fault_impedance:
+            verbose:
         """
+
+        assert (len(branch_fault_locations) == len(branch_index))
+        assert (len(branch_fault_impedance) == len(branch_index))
+
         self.bus_index = bus_index
+
+        self.branch_index = branch_index
+
+        self.branch_fault_locations = branch_fault_locations
+
+        self.branch_fault_impedance = branch_fault_impedance
 
         self.verbose = verbose
 
@@ -243,21 +257,73 @@ class ShortCircuit(QRunnable):
         # Options to use
         self.options = options
 
-        # compile the buses short circuit impedance array
-        n = len(self.grid.buses)
-        self.Zf = zeros(n, dtype=complex)
-        for i in range(n):
-            self.Zf[i] = self.grid.buses[i].Zf
-
         self.results = None
 
         self.__cancel__ = False
 
-    def single_short_circuit(self, calculation_inputs: CalculationInputs, Vpf):
+    @staticmethod
+    def compile_zf(grid):
+
+        # compile the buses short circuit impedance array
+        n = len(grid.buses)
+        Zf = zeros(n, dtype=complex)
+        for i in range(n):
+            Zf[i] = grid.buses[i].Zf
+
+        return Zf
+
+    @staticmethod
+    def split_branch(branch: Branch, fault_position, r_fault, x_fault):
+        """
+        Split a branch by a given distance
+        :param branch: Branch of a circuit
+        :param fault_position: per unit distance measured from the "from" bus (0 ~ 1)
+        :param r_fault: Fault resistance in p.u.
+        :param x_fault: Fault reactance in p.u.
+        :return: the two new branches and the mid short circuited bus
+        """
+
+        assert(0.0 < fault_position < 1.0)
+
+        r = branch.R
+        x = branch.X
+        g = branch.G
+        b = branch.B
+
+        # deactivate the current branch
+        branch.active = False
+
+        # each of the branches will have the proportional impedances
+        # Bus_from------------Middle_bus------------Bus_To
+        #       |------x--------|   (x: distance measured in per unit (0~1)
+
+        middle_bus = Bus()
+
+        # set the bus fault impedance
+        middle_bus.Zf = complex(r_fault, x_fault)
+
+        br1 = Branch(bus_from=branch.bus_from,
+                     bus_to=middle_bus,
+                     r=r * fault_position,
+                     x=x * fault_position,
+                     g=g * fault_position,
+                     b=b * fault_position)
+
+        br2 = Branch(bus_from=middle_bus,
+                     bus_to=branch.bus_to,
+                     r=r * (1 - fault_position),
+                     x=x * (1 - fault_position),
+                     g=g * (1 - fault_position),
+                     b=b * (1 - fault_position))
+
+        return br1, br2, middle_bus
+
+    def single_short_circuit(self, calculation_inputs: CalculationInputs, Vpf, Zf):
         """
         Run a power flow simulation for a single circuit
         @param calculation_inputs:
-        @param Vpf: Power flow voltage
+        @param Vpf: Power flow voltage vector applicable to the island
+        @param Zf: Short circuit impedance vector applicable to the island
         @return: short circuit results
         """
         # compute Zbus
@@ -265,12 +331,12 @@ class ShortCircuit(QRunnable):
         if calculation_inputs.Ybus.shape[0] > 1:
             Zbus = inv(calculation_inputs.Ybus).toarray()
 
-
             # Compute the short circuit
             V, SCpower = short_circuit_3p(bus_idx=self.options.bus_index,
                                           Zbus=Zbus,
                                           Vbus=Vpf,
-                                          Zf=self.Zf, baseMVA=calculation_inputs.Sbase)
+                                          Zf=Zf,
+                                          baseMVA=calculation_inputs.Sbase)
 
             # Compute the branches power
             Sbranch, Ibranch, loading, losses = self.compute_branch_results(calculation_inputs=calculation_inputs, V=V)
@@ -334,17 +400,42 @@ class ShortCircuit(QRunnable):
         # print('Short circuit at ', self.grid.name)
         # self.progress_signal.emit(0.0)
 
-        n = len(self.grid.buses)
-        m = len(self.grid.branches)
+        if len(self.options.branch_index) > 0:
+
+            # if there are branch indices where to perform short circuits, modify the grid accordingly
+
+            grid = self.grid.copy()
+
+            sc_bus_index = list()
+
+            for k, br_idx in enumerate(self.options.branch_index):
+
+                # modify the grid by inserting a mid-line short circuit bus
+                br1, br2, middle_bus = self.split_branch(branch=br_idx,
+                                                         fault_position=self.options.branch_fault_locations[k],
+                                                         r_fault=self.options.branch_fault_impedance[k].real,
+                                                         x_fault=self.options.branch_fault_impedance[k].imag)
+
+                grid.add_branch(br1)
+                grid.add_branch(br2)
+                grid.add_bus(middle_bus)
+                sc_bus_index.append(len(grid.buses) - 1)
+
+        else:
+            grid = self.grid
+
+        n = len(grid.buses)
+        m = len(grid.branches)
         results = ShortCircuitResults()  # yes, reuse this class
         results.initialize(n, m)
-        k = 0
 
-        # print('Compiling...', end='')
+        # Compile the grid
         numerical_circuit = self.grid.compile()
         calculation_inputs = numerical_circuit.compute()
 
-        if len(calculation_inputs) > 1:
+        Zf = self.compile_zf(grid)
+
+        if len(calculation_inputs) > 1:  # multi-island
 
             for i, calculation_input in enumerate(calculation_inputs):
 
@@ -352,13 +443,17 @@ class ShortCircuit(QRunnable):
                 branch_original_idx = numerical_circuit.island_branches[i]
 
                 res = self.single_short_circuit(calculation_inputs=calculation_input,
-                                                Vpf=self.pf_results.voltage[bus_original_idx])
+                                                Vpf=self.pf_results.voltage[bus_original_idx],
+                                                Zf=Zf[bus_original_idx])
 
                 # merge results
                 results.apply_from_island(res, bus_original_idx, branch_original_idx)
-        else:
+
+        else:  # single island
+
             results = self.single_short_circuit(calculation_inputs=calculation_inputs[0],
-                                                Vpf=self.pf_results.voltage)
+                                                Vpf=self.pf_results.voltage,
+                                                Zf=Zf)
 
         self.results = results
         self.grid.short_circuit_results = results
