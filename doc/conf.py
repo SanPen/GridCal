cd6d2e46d105f9a1ac531f7b47fbc7191d983ae9
Michel Lavoie <lavoie.michel@gmail.com>
2019-01-31 17:30:11 -0500

    Hopefully fixed equations in RTD

--- doc/conf.py
+++ doc/conf.py
@@ -40,7 +40,7 @@ release = '3.1.5'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-#    'sphinx.ext.doctest',
+    'sphinx.ext.imgmath',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
