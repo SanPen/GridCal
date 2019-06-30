735be37de379bc90828a645c09a0ee92b3287b09
santi <santiago.penate.vera@gmail.com>
2019-06-30 19:54:33 +0200

    removed plt from the plot config file

--- src/GridCal/Engine/plot_config.py
+++ src/GridCal/Engine/plot_config.py
@@ -17,14 +17,14 @@ import PySide2  # this line is necessary so that Matplotlib recognises that PySi
 import matplotlib
 matplotlib.use('Qt5Agg')
 # matplotlib.rcParams['backend.qt5'] = 'PySide2'
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 
 
 ########################################################################################################################
 # Set Matplotlib global parameters
 ########################################################################################################################
-if 'fivethirtyeight' in plt.style.available:
-    plt.style.use('fivethirtyeight')
+# if 'fivethirtyeight' in plt.style.available:
+#     plt.style.use('fivethirtyeight')
 
 SMALL_SIZE = 8
 MEDIUM_SIZE = 10
@@ -35,10 +35,10 @@ LEFT = 0.12
 RIGHT = 0.98
 TOP = 0.8
 BOTTOM = 0.2
-plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
-plt.rc('axes', titlesize=SMALL_SIZE)  # font size of the axes title
-plt.rc('axes', labelsize=SMALL_SIZE)  # font size of the x and y labels
-plt.rc('xtick', labelsize=SMALL_SIZE)  # font size of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)  # font size of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)  # legend font size
-plt.rc('figure', titlesize=MEDIUM_SIZE)  # font size of the figure title
+matplotlib.rc('font', size=SMALL_SIZE)  # controls default text sizes
+matplotlib.rc('axes', titlesize=SMALL_SIZE)  # font size of the axes title
+matplotlib.rc('axes', labelsize=SMALL_SIZE)  # font size of the x and y labels
+matplotlib.rc('xtick', labelsize=SMALL_SIZE)  # font size of the tick labels
+matplotlib.rc('ytick', labelsize=SMALL_SIZE)  # font size of the tick labels
+matplotlib.rc('legend', fontsize=SMALL_SIZE)  # legend font size
+matplotlib.rc('figure', titlesize=MEDIUM_SIZE)  # font size of the figure title
