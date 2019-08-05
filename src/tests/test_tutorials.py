3e9c6af3ec4bc0db187c4f7f43b5a5d8316fd9fa
Bengt Lüers <bengt.lueers@gmail.com>
2019-08-05 18:32:57 +0200

    make tutorials importable

--- src/tests/test_tutorials.py
+++ src/tests/test_tutorials.py
@@ -1,8 +1,8 @@
 import os
 from sys import executable
 
-from Tutorials import defining_a_grid_from_scratch_without_profiles
-# from Tutorials import defining_a_grid_from_scratch_with_profiles
+from GridCal.Tutorials import defining_a_grid_from_scratch_without_profiles
+# from GridCal.Tutorials import defining_a_grid_from_scratch_with_profiles
 
 
 def test_define_grid_from_scratch_without_profiles():
