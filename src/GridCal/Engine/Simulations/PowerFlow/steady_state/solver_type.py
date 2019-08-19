a75d738cc7f5bc443ed65ece702ef07b45326d73
Bengt Lüers <bengt.lueers@gmail.com>
2019-08-19 15:15:33 +0200

    reorganise solver types

--- src/GridCal/Engine/Simulations/PowerFlow/steady_state/solver_type.py
+++ src/GridCal/Engine/Simulations/PowerFlow/steady_state/solver_type.py
@@ -7,29 +7,39 @@ class SolverType(Enum):
     algorithms supported by **GridCal**.
     """
 
-    NR = 'Newton Raphson'
-    NRFD_XB = 'Fast decoupled XB'
-    NRFD_BX = 'Fast decoupled BX'
+    # Power Flow Solvers
+    CONTINUATION_NR = 'Continuation-Newton-Raphson'
     GAUSS = 'Gauss-Seidel'
-    DC = 'Linear DC'
     HELM = 'Holomorphic Embedding'
-    HELM_Z_PV = 'HELM-Z-PV'
-    HELM_Z_PQ = 'HELM-Z-PQ'
     HELM_CHENGXI_VANILLA = 'HELM-Chengxi-Vanilla'
     HELM_CHENGXI_2 = 'HELM-Chengxi-2'
     HELM_CHENGXI_CORRECTED = 'HELM-Chengxi-Corrected'
     HELM_PQ = 'HELM-PQ'
     HELM_VECT_ASU = 'HELM-Vect-ASU'
     HELM_WALLACE = 'HELM-Wallace'
-    ZBUS = 'Z-Gauss-Seidel'
+    HELM_Z_PV = 'HELM-Z-PV'
+    HELM_Z_PQ = 'HELM-Z-PQ'
     IWAMOTO = 'Iwamoto-Newton-Raphson'
-    CONTINUATION_NR = 'Continuation-Newton-Raphson'
-    LM = 'Levenberg-Marquardt'
-    FASTDECOUPLED = 'Fast decoupled'
     LACPF = 'Linear AC'
-    DC_OPF = 'Linear DC OPF'
-    AC_OPF = 'Linear AC OPF'
+    LM = 'Levenberg-Marquardt'
+    NR = 'Newton Raphson'
     NRI = 'Newton-Raphson in current'
+    ZBUS = 'Z-Gauss-Seidel'
+
+    # Dealternated Current Power Flow
+
+    DC = 'Linear DC'
+
+    # Fast Decoupled
+
+    FASTDECOUPLED = 'Fast decoupled'
+    NRFD_XB = 'Fast decoupled XB'
+    NRFD_BX = 'Fast decoupled BX'
+
+    # Optimal Power Flow
+
+    AC_OPF = 'Linear AC OPF'
+    DC_OPF = 'Linear DC OPF'
     DYCORS_OPF = 'DYCORS OPF'
     GA_OPF = 'Genetic Algorithm OPF'
     NELDER_MEAD_OPF = 'Nelder Mead OPF'
