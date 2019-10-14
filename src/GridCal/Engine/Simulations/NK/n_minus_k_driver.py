Fixed n-k and added OTDF  matrix computation

src/GridCal/Engine/Simulations/NK/n_minus_k_driver.py
@@ -102,9 +102,12 @@ class NMinusK(QThread):
         :param states_number_limit: limit the amount of states
         :return: Nothing, saves a report
         """
-        name = 'N-' + str(k)
 
-        self.progress_text("Filtering elements by voltage")
+        # if there are valid profiles...
+        if self.grid.time_profile is None:
+            return None
+
+        self.progress_text.emit("Filtering elements by voltage")
 
         # filter branches
         branch_names = list()
@@ -127,7 +130,7 @@ class NMinusK(QThread):
         bus_index = np.array(bus_index)
 
         # get N-k states
-        self.progress_text("Enumerating states")
+        self.progress_text.emit("Enumerating states")
         states, failed_indices = enumerate_states_n_k(m=len(branch_names), k=k)
 
         # limit states for memory reasons
@@ -139,12 +142,12 @@ class NMinusK(QThread):
         profile_indices = np.tile(time_indices, len(states))
 
         # compile the multi-circuit
-        self.progress_text("Compiling assets...")
-        self.progress_signal(0)
+        self.progress_text.emit("Compiling assets...")
+        self.progress_signal.emit(0)
         numerical_circuit = self.grid.compile(use_opf_vals=False, opf_time_series_results=None)
 
         # re-index the profile (this is essential for time-compatibility)
-        self.progress_signal(1, 1, prefix=name, suffix='Re-indexing...')
+        self.progress_signal.emit(100)
         numerical_circuit.re_index_time(t_idx=profile_indices)
 
         # set the branch states
@@ -161,8 +164,8 @@ class NMinusK(QThread):
         n_k_results = NMinusKResults(n, m, nt, time_array=numerical_circuit.time_array)
 
         # do the topological computation
-        self.progress_text("Compiling topology...")
-        self.progress_signal(0.0)
+        self.progress_text.emit("Compiling topology...")
+        self.progress_signal.emit(0.0)
         calc_inputs_dict = numerical_circuit.compute_ts(ignore_single_node_islands=pf_options.ignore_single_node_islands)
 
         n_k_results.bus_types = numerical_circuit.bus_types
@@ -177,46 +180,40 @@ class NMinusK(QThread):
                 bus_original_idx = calculation_input.original_bus_idx
                 branch_original_idx = calculation_input.original_branch_idx
 
-                # if there are valid profiles...
-                if self.grid.time_profile is not None:
+                # declare a results object for the partition
+                # nt = calculation_input.ntime
+                nt = len(calculation_input.original_time_idx)
+                n = calculation_input.nbus
+                m = calculation_input.nbr
+                partial_results = NMinusKResults(n, m, nt)
+                last_voltage = calculation_input.Vbus
 
-                    # declare a results object for the partition
-                    # nt = calculation_input.ntime
-                    nt = len(calculation_input.original_time_idx)
-                    n = calculation_input.nbus
-                    m = calculation_input.nbr
-                    partial_results = NMinusKResults(n, m, nt)
-                    last_voltage = calculation_input.Vbus
+                # traverse the time profiles of the partition and simulate each time step
+                for it, t in enumerate(calculation_input.original_time_idx):
 
-                    # traverse the time profiles of the partition and simulate each time step
-                    for it, t in enumerate(calculation_input.original_time_idx):
+                    self.progress_signal.emit(t / nt * 100.0)
 
-                        self.progress_signal(it / nt * 100.0)
+                    # set the power values
+                    # if the storage dispatch option is active, the batteries power is not included
+                    # therefore, it shall be included after processing
+                    Ysh = calculation_input.Ysh_prof[:, it]
+                    I = calculation_input.Ibus_prof[:, it]
+                    S = calculation_input.Sbus_prof[:, it]
+                    branch_rates = calculation_input.branch_rates_prof[it, :]
 
-                        # set the power values
-                        # if the storage dispatch option is active, the batteries power is not included
-                        # therefore, it shall be included after processing
-                        Ysh = calculation_input.Ysh_prof[:, it]
-                        I = calculation_input.Ibus_prof[:, it]
-                        S = calculation_input.Sbus_prof[:, it]
-                        branch_rates = calculation_input.branch_rates_prof[it, :]
+                    # run power flow at the circuit
+                    res = single_island_pf(circuit=calculation_input, Vbus=last_voltage, Sbus=S, Ibus=I,
+                                           branch_rates=branch_rates, options=pf_options, logger=self.logger)
 
-                        # run power flow at the circuit
-                        res = single_island_pf(circuit=calculation_input, Vbus=last_voltage, Sbus=S, Ibus=I,
-                                               branch_rates=branch_rates, options=pf_options, logger=self.logger)
+                    # Recycle voltage solution
+                    last_voltage = res.voltage
 
-                        # Recycle voltage solution
-                        last_voltage = res.voltage
+                    # store circuit results at the time index 't'
+                    partial_results.set_at(it, res)
 
-                        # store circuit results at the time index 't'
-                        partial_results.set_at(it, res)
-
-                    # merge the circuit's results
-
-                    n_k_results.apply_from_island(partial_results, bus_original_idx, branch_original_idx,
-                                                          calculation_input.original_time_idx, 'TS')
-                else:
-                    print('There are no profiles')
+                # merge the circuit's results
+                n_k_results.apply_from_island(partial_results, bus_original_idx, branch_original_idx,
+                                              calculation_input.original_time_idx, 'TS')
 
         return n_k_results
 
@@ -229,9 +226,7 @@ class NMinusK(QThread):
         :param states_number_limit: limit the amount of states
         :return: Nothing, saves a report
         """
-        name = 'N-' + str(k)
-
-        self.progress_text("Filtering elements by voltage")
+        self.progress_text.emit("Filtering elements by voltage")
 
         # filter branches
         branch_names = list()
@@ -254,7 +249,7 @@ class NMinusK(QThread):
         bus_index = np.array(bus_index)
 
         # get N-k states
-        self.progress_text("Enumerating states")
+        self.progress_text.emit("Enumerating states")
         states, failed_indices = enumerate_states_n_k(m=len(branch_names), k=k)
 
         # limit states for memory reasons
@@ -266,12 +261,12 @@ class NMinusK(QThread):
         profile_indices = np.tile(time_indices, len(states))
 
         # compile the multi-circuit
-        self.progress_text("Compiling assets...")
+        self.progress_text.emit("Compiling assets...")
         self.progress_signal(0)
         numerical_circuit = self.grid.compile(use_opf_vals=False, opf_time_series_results=None)
 
         # re-index the profile (this is essential for time-compatibility)
-        self.progress_signal(1, 1, prefix=name, suffix='Re-indexing...')
+        self.progress_signal.emit(100)
         numerical_circuit.re_index_time(t_idx=profile_indices)
 
         # set the branch states
@@ -288,8 +283,8 @@ class NMinusK(QThread):
         n_k_results = NMinusKResults(n, m, nt, time_array=numerical_circuit.time_array, states=states)
 
         # do the topological computation
-        self.progress_text("Compiling topology...")
-        self.progress_signal(0.0)
+        self.progress_text.emit("Compiling topology...")
+        self.progress_signal.emit(0.0)
         calc_inputs_dict = numerical_circuit.compute_ts(ignore_single_node_islands=pf_options.ignore_single_node_islands)
 
         n_k_results.bus_types = numerical_circuit.bus_types
@@ -317,7 +312,7 @@ class NMinusK(QThread):
 
                     # traverse the time profiles of the partition and simulate each time step
                     for it, t in enumerate(calculation_input.original_time_idx):
-                        self.progress_signal(it / nt * 100.0)
+                        self.progress_signal.emit(it / nt * 100.0)
 
                         # set the power values
                         # if the storage dispatch option is active, the batteries power is not included
@@ -343,7 +338,7 @@ class NMinusK(QThread):
                                                   calculation_input.original_time_idx, 'TS')
                 else:
                     self.progress_text.emit('There are no profiles')
-                    self.log.append('There are no profiles')
+                    self.logger.append('There are no profiles')
 
         return n_k_results
 
@@ -366,5 +361,45 @@ class NMinusK(QThread):
         self.progress_text.emit('Done!')
         self.done_signal.emit()
 
+    def get_otdf(self):
+        """
+        Outage Transfer Distribution Factors (OTDF)
+        :return:
+        """
+        if self.results is None:
+            return None
+        else:
+            Pbr = self.results.Sbranch.real
+
+            # m = Pbr.shape[1]
+            # otdf = np.empty((m, m))
+            # for i in range(m):
+            #     otdf[i, :] = (Pbr[i + 1, :] - Pbr[0, :]) / Pbr[0, :]
+
+            otdf = (Pbr[1:, :] - Pbr[0, :]) / Pbr[0, :]
+
+            return otdf
+
     def cancel(self):
         self.__cancel__ = True
+
+
+if __name__ == '__main__':
+
+    from GridCal.Engine import FileOpen, SolverType
+
+    # fname = '/home/santi/Documentos/GitHub/GridCal/Grids_and_profiles/grids/Lynn 5 Bus pv.gridcal'
+    # fname = '/home/santi/Documentos/GitHub/GridCal/Grids_and_profiles/grids/IEEE39_1W.gridcal'
+    # fname = '/home/santi/Documentos/GitHub/GridCal/Grids_and_profiles/grids/grid_2_islands.xlsx'
+    fname = '/home/santi/Documentos/GitHub/GridCal/Grids_and_profiles/grids/2869 Pegase.gridcal'
+
+    main_circuit = FileOpen(fname).open()
+
+    pf_options = PowerFlowOptions(solver_type=SolverType.LACPF)
+    options = NMinusKOptions(use_multi_threading=False)
+    simulation = NMinusK(grid=main_circuit, options=options, pf_options=pf_options)
+    simulation.run()
+
+    otdf_ = simulation.get_otdf()
+
+    print(otdf_)