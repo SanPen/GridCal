Fixed n-k and added OTDF  matrix computation

src/GridCal/Engine/Core/numerical_circuit.py
@@ -13,10 +13,11 @@
 # You should have received a copy of the GNU General Public License
 # along with GridCal.  If not, see <http://www.gnu.org/licenses/>.
 import numpy as np
+import datetime
 import networkx as nx
 from typing import List, Dict
 import scipy.sparse as sp
-
+import pandas as pd
 from GridCal.Engine.Core.csc_graph import Graph
 from GridCal.Engine.basic_structures import BranchImpedanceMode
 from GridCal.Engine.Core.calculation_inputs import CalculationInputs
@@ -494,15 +495,60 @@ class NumericalCircuit:
         self.static_gen_power_profile = self.static_gen_power_profile[t_idx, :]  # np.zeros((n_time, n_sta_gen), dtype=complex)
 
         # controlled generator
-        self.generator_active_prof = self.generator_active_prof  # np.zeros((n_time, n_gen), dtype=bool)
+        self.generator_active_prof = self.generator_active_prof[t_idx, :]  # np.zeros((n_time, n_gen), dtype=bool)
         self.generator_cost_profile = self.generator_cost_profile[t_idx, :]  # np.zeros((n_time, n_gen), dtype=float)
         self.generator_power_profile = self.generator_power_profile[t_idx, :]  # np.zeros((n_time, n_gen), dtype=float)
         self.generator_power_factor_profile = self.generator_power_factor_profile[t_idx, :]  # np.zeros((n_time, n_gen), dtype=float)
         self.generator_voltage_profile = self.generator_voltage_profile[t_idx, :]  # np.zeros((n_time, n_gen), dtype=float)
 
         # shunt
-        self.shunt_active_prof = self.shunt_active_prof  # np.zeros((n_time, n_sh), dtype=bool)
+        self.shunt_active_prof = self.shunt_active_prof[t_idx, :]  # np.zeros((n_time, n_sh), dtype=bool)
         self.shunt_admittance_profile = self.shunt_admittance_profile[t_idx, :]  # np.zeros((n_time, n_sh), dtype=complex)
+
+    def set_base_profile(self):
+        """
+        Re-index all the time based profiles
+        :return: Nothing, this is done in-place
+        """
+        now = datetime.datetime.now()
+        dte = datetime.datetime(year=now.year, month=1, day=1, hour=0)
+        self.time_array = pd.to_datetime([dte])
+        self.ntime = len(self.time_array)
+
+        # branch
+        self.branch_active_prof = self.branch_active.reshape(1, -1)  # np.zeros((n_time, n_br), dtype=int)
+        self.temp_oper_prof = self.temp_oper.reshape(1, -1)  # np.zeros((n_time, n_br), dtype=float)
+        self.br_rate_profile = self.br_rates.reshape(1, -1)  # np.zeros((n_time, n_br), dtype=float)
+        self.branch_cost_profile = self.branch_cost.reshape(1, -1)  # np.zeros((n_time, n_br), dtype=float)
+
+        # load
+        self.load_active_prof = self.load_active.reshape(1, -1)   # np.zeros((n_time, n_ld), dtype=bool)
+        self.load_cost_prof = self.load_cost.reshape(1, -1)   # np.zeros((n_time, n_ld), dtype=float)
+
+        self.load_power_profile = self.load_power.reshape(1, -1)  # np.zeros((n_time, n_ld), dtype=complex)
+        self.load_current_profile = self.load_current.reshape(1, -1)  # np.zeros((n_time, n_ld), dtype=complex)
+        self.load_admittance_profile = self.load_admittance.reshape(1, -1)  # np.zeros((n_time, n_ld), dtype=complex)
+
+        # battery
+        self.battery_active_prof = self.battery_active.reshape(1, -1)  # np.zeros((n_time, n_batt), dtype=bool)
+        self.battery_power_profile = self.battery_power.reshape(1, -1)  # np.zeros((n_time, n_batt), dtype=float)
+        self.battery_voltage_profile = self.battery_voltage.reshape(1, -1)  # np.zeros((n_time, n_batt), dtype=float)
+        self.battery_cost_profile = self.battery_cost.reshape(1, -1)  # np.zeros((n_time, n_batt), dtype=float)
+
+        # static generator
+        self.static_gen_active_prof = self.static_gen_active.reshape(1, -1)  # np.zeros((n_time, n_sta_gen), dtype=bool)
+        self.static_gen_power_profile = self.static_gen_power.reshape(1, -1)  # np.zeros((n_time, n_sta_gen), dtype=complex)
+
+        # controlled generator
+        self.generator_active_prof = self.generator_active.reshape(1, -1)  # np.zeros((n_time, n_gen), dtype=bool)
+        self.generator_cost_profile = self.generator_cost.reshape(1, -1)  # np.zeros((n_time, n_gen), dtype=float)
+        self.generator_power_profile = self.generator_power.reshape(1, -1)  # np.zeros((n_time, n_gen), dtype=float)
+        self.generator_power_factor_profile = self.generator_power_factor.reshape(1, -1)  # np.zeros((n_time, n_gen), dtype=float)
+        self.generator_voltage_profile = self.generator_voltage.reshape(1, -1)  # np.zeros((n_time, n_gen), dtype=float)
+
+        # shunt
+        self.shunt_active_prof = self.shunt_active.reshape(1, -1)  # np.zeros((n_time, n_sh), dtype=bool)
+        self.shunt_admittance_profile = self.shunt_admittance.reshape(1, -1)  # np.zeros((n_time, n_sh), dtype=complex)
 
     def get_different_states(self):
         """

src/GridCal/Engine/Simulations/NK/n_minus_k_driver.py
@@ -93,19 +93,26 @@ class NMinusK(QThread):
 
         self.elapsed = 0.0
 
-    def n_minus_k(self, k=1, time_indices=np.array([0]), vmin=0, states_number_limit=None):
+        self.branch_names = list()
+
+    def get_steps(self):
+        """
+        Get variations list of strings
+        """
+        if self.results is not None:
+            return [v for v in self.branch_names]
+        else:
+            return list()
+
+    def n_minus_k(self, k=1, indices=None, vmin=0, states_number_limit=None):
         """
         Run N-K simulation in series
         :param k: Parameter level (1 for n-1, 2 for n-2, etc...)
-        :param time_indices: time indices
+        :param indices: time indices {np.array([0])}
         :param vmin: minimum nominal voltage to allow (filters out branches and buses below)
         :param states_number_limit: limit the amount of states
         :return: Nothing, saves a report
         """
-
-        # if there are valid profiles...
-        if self.grid.time_profile is None:
-            return None
 
         self.progress_text.emit("Filtering elements by voltage")
 
@@ -119,6 +126,8 @@ class NMinusK(QThread):
                 branch_index.append(i)
                 branches.append(branch)
         branch_index = np.array(branch_index)
+
+        self.branch_names = branch_names
 
         # filter buses
         bus_names = list()
@@ -138,9 +147,6 @@ class NMinusK(QThread):
             states = states[:states_number_limit, :]
             failed_indices = failed_indices[:states_number_limit]
 
-        # construct the profile indices
-        profile_indices = np.tile(time_indices, len(states))
-
         # compile the multi-circuit
         self.progress_text.emit("Compiling assets...")
         self.progress_signal.emit(0)
@@ -148,6 +154,16 @@ class NMinusK(QThread):
 
         # re-index the profile (this is essential for time-compatibility)
         self.progress_signal.emit(100)
+
+        # if no base profile time is given, pick the base values
+        if indices is None:
+            time_indices = np.array([0])
+            numerical_circuit.set_base_profile()
+        else:
+            time_indices = indices
+
+        # construct the profile indices
+        profile_indices = np.tile(time_indices, len(states))
         numerical_circuit.re_index_time(t_idx=profile_indices)
 
         # set the branch states
@@ -217,11 +233,11 @@ class NMinusK(QThread):
 
         return n_k_results
 
-    def n_minus_k_mt(self, k=1, time_indices=np.array([0]), vmin=200, states_number_limit=None):
+    def n_minus_k_mt(self, k=1, indices=None, vmin=200, states_number_limit=None):
         """
         Run N-K simulation in series
         :param k: Parameter level (1 for n-1, 2 for n-2, etc...)
-        :param time_indices: time indices
+        :param indices: time indices {np.array([0])}
         :param vmin: minimum nominal voltage to allow (filters out branches and buses below)
         :param states_number_limit: limit the amount of states
         :return: Nothing, saves a report
@@ -257,16 +273,22 @@ class NMinusK(QThread):
             states = states[:states_number_limit, :]
             failed_indices = failed_indices[:states_number_limit]
 
-        # construct the profile indices
-        profile_indices = np.tile(time_indices, len(states))
-
         # compile the multi-circuit
         self.progress_text.emit("Compiling assets...")
         self.progress_signal.emit(0)
         numerical_circuit = self.grid.compile(use_opf_vals=False, opf_time_series_results=None)
 
+        # if no base profile time is given, pick the base values
+        if indices is None:
+            time_indices = np.array([0])
+            numerical_circuit.set_base_profile()
+        else:
+            time_indices = indices
+
         # re-index the profile (this is essential for time-compatibility)
         self.progress_signal.emit(100)
+        # construct the profile indices
+        profile_indices = np.tile(time_indices, len(states))
         numerical_circuit.re_index_time(t_idx=profile_indices)
 
         # set the branch states
@@ -350,11 +372,16 @@ class NMinusK(QThread):
         start = time.time()
         if self.options.use_multi_threading:
 
-            self.results = self.n_minus_k_mt(k=1, time_indices=np.array([0]), vmin=0, states_number_limit=None)
+            self.results = self.n_minus_k_mt(k=1, indices=None, vmin=0, states_number_limit=None)
 
         else:
 
-            self.results = self.n_minus_k(k=1, time_indices=np.array([0]), vmin=0, states_number_limit=None)
+            self.results = self.n_minus_k(k=1, indices=None, vmin=0, states_number_limit=None)
+
+        self.progress_text.emit('Computing OTDF...')
+        if self.results is not None:
+            self.results.branch_names = np.array([b.name for b in self.grid.branches])
+            self.results.otdf = self.get_otdf(failure_flow_limit=0.0)
 
         end = time.time()
         self.elapsed = end - start

src/GridCal/Engine/Simulations/NK/n_minus_k_results.py
@@ -48,6 +48,8 @@ class NMinusKResults(PowerFlowResults):
 
         self.bus_types = np.zeros(n, dtype=int)
 
+        self.branch_names = None
+
         if nt > 0:
             self.voltage = np.zeros((nt, n), dtype=complex)
 
@@ -118,7 +120,10 @@ class NMinusKResults(PowerFlowResults):
 
             self.buses_useful_for_storage = None
 
-        self.available_results = [ResultTypes.BusVoltageModule,
+        self.otdf = np.zeros((m, m))
+
+        self.available_results = [ResultTypes.OTDF,
+                                  ResultTypes.BusVoltageModule,
                                   ResultTypes.BusVoltageAngle,
                                   ResultTypes.BusActivePower,
                                   ResultTypes.BusReactivePower,
@@ -128,7 +133,7 @@ class NMinusKResults(PowerFlowResults):
                                   ResultTypes.BranchLosses,
                                   ResultTypes.BranchVoltage,
                                   ResultTypes.BranchAngles,
-                                  ResultTypes.SimulationError]
+                                  ResultTypes.OTDFSimulationError]
 
     def set_at(self, t, results: PowerFlowResults):
         """
@@ -170,6 +175,9 @@ class NMinusKResults(PowerFlowResults):
         self.undervoltage_idx[t] = results.undervoltage_idx
 
         self.buses_useful_for_storage[t] = results.buses_useful_for_storage
+
+    def get_steps(self):
+        return
 
     @staticmethod
     def merge_if(df, arr, ind, cols):
@@ -299,73 +307,80 @@ class NMinusKResults(PowerFlowResults):
             labels = names[indices]
 
             if result_type == ResultTypes.BusVoltageModule:
-                data = np.abs(self.voltage[:, indices])
+                data = np.abs(self.voltage[indices + 1])
                 y_label = '(p.u.)'
                 title = 'Bus voltage '
 
             elif result_type == ResultTypes.BusVoltageAngle:
-                data = np.angle(self.voltage[:, indices], deg=True)
+                data = np.angle(self.voltage[indices + 1], deg=True)
                 y_label = '(Deg)'
                 title = 'Bus voltage '
 
             elif result_type == ResultTypes.BusActivePower:
-                data = self.S[:, indices].real
+                data = self.S[indices + 1].real
                 y_label = '(MW)'
                 title = 'Bus active power '
 
             elif result_type == ResultTypes.BusReactivePower:
-                data = self.S[:, indices].imag
+                data = self.S[indices + 1].imag
                 y_label = '(MVAr)'
                 title = 'Bus reactive power '
 
             elif result_type == ResultTypes.BranchPower:
-                data = self.Sbranch[:, indices]
+                data = self.Sbranch[indices + 1]
                 y_label = '(MVA)'
                 title = 'Branch power '
 
             elif result_type == ResultTypes.BranchCurrent:
-                data = self.Ibranch[:, indices]
+                data = self.Ibranch[indices + 1]
                 y_label = '(kA)'
                 title = 'Branch current '
 
             elif result_type == ResultTypes.BranchLoading:
-                data = self.loading[:, indices] * 100
+                data = self.loading[indices + 1] * 100
                 y_label = '(%)'
                 title = 'Branch loading '
 
             elif result_type == ResultTypes.BranchLosses:
-                data = self.losses[:, indices]
+                data = self.losses[indices + 1]
                 y_label = '(MVA)'
                 title = 'Branch losses'
 
             elif result_type == ResultTypes.BranchVoltage:
-                data = np.abs(self.Vbranch[:, indices])
+                data = np.abs(self.Vbranch[indices + 1])
                 y_label = '(p.u.)'
                 title = result_type.value[0]
 
             elif result_type == ResultTypes.BranchAngles:
-                data = np.angle(self.Vbranch[:, indices], deg=True)
+                data = np.angle(self.Vbranch[indices + 1], deg=True)
                 y_label = '(deg)'
                 title = result_type.value[0]
 
             elif result_type == ResultTypes.BatteryPower:
-                data = np.zeros_like(self.losses[:, indices])
+                data = np.zeros_like(self.losses[indices + 1])
                 y_label = '$\Delta$ (MVA)'
                 title = 'Battery power'
 
-            elif result_type == ResultTypes.SimulationError:
-                data = self.error
+            elif result_type == ResultTypes.OTDFSimulationError:
+                data = self.error[indices + 1]
                 y_label = 'Per unit power'
                 labels = [y_label]
                 title = 'Error'
 
+            elif result_type == ResultTypes.OTDF:
+                data = self.otdf[indices, :]
+                y_label = 'Per unit'
+                labels = [y_label]
+                title = 'OTDF'
+
+                # assemble model
+                mdl = ResultsModel(data=data, index=self.branch_names[indices],
+                                   columns=self.branch_names, title=title, ylabel=y_label)
+                return mdl
             else:
                 raise Exception('Result type not understood:' + str(result_type))
 
-            if self.time is not None:
-                index = self.time
-            else:
-                index = list(range(data.shape[0]))
+            index = self.branch_names[indices]
 
             # assemble model
             mdl = ResultsModel(data=data, index=index, columns=labels, title=title, ylabel=y_label)