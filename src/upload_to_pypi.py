e4b329da44f096d41e78d6c8054d562f4aa34b9c
santi <santiago.penate.vera@gmail.com>
2020-01-12 18:55:28 +0100

    fixed setup

--- src/upload_to_pypi.py
+++ src/upload_to_pypi.py
@@ -12,10 +12,8 @@ from subprocess import call
 from GridCal.__version__ import __GridCal_VERSION__
 
 import os
+import sys
 
-if os.name == 'nt':
-    call(['python', 'setup.py', 'sdist'])
-else:
-    call(['python3', 'setup.py', 'sdist'])
+call([sys.executable, 'setup.py', 'sdist'])
 
 call(['twine', 'upload', 'dist/GridCal-' + str(__GridCal_VERSION__) + '.tar.gz'])
