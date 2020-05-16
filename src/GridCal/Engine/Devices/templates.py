a8594c3bfb90246b1d4e35cc2918856c82c193f7
sanpen <krimen_1980>
2020-05-16 16:52:45 +0200

    worked on the catalogue

--- src/GridCal/Engine/Devices/templates.py
+++ src/GridCal/Engine/Devices/templates.py
@@ -15,7 +15,7 @@
 
 import os
 import pandas as pd
-from GridCal.Engine.Devices.line import LineTemplate
+from GridCal.Engine.Devices.line import SequenceLineType
 from GridCal.Engine.Devices.transformer import TransformerType
 from GridCal.Engine.Devices.wire import Wire
 
@@ -47,5 +47,76 @@ def get_transformer_catalogue():
     else:
         return list()
 
+
+def get_cables_catalogue():
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    fname = os.path.join(path, '..', '..', 'data', 'cables.csv')
+
+    if os.path.exists(fname):
+        df = pd.read_csv(fname)
+
+        lst = list()
+        for i, item in df.iterrows():
+            """
+            Name,
+            Rated voltage [kV],
+            Rated current [kA],
+            Nominal Frequency,
+            R [Ohm/km AC,20°C],
+            X [Ohm/km],
+            L [Ohm/km],
+            R0 (AC) [Ohm/km],
+            X0  [Ohm/km]
+            L0 [mH/km]
+            """
+            tpe = SequenceLineType(name=item['Name'],
+                                   rating=item['Rated current [kA]'],
+                                   R=item['R [Ohm/km AC@20°C]'],
+                                   X=item['X [Ohm/km]'],
+                                   G=0.0,
+                                   B=0.0,
+                                   R0=item['R0 (AC) [Ohm/km]'],
+                                   X0=item['X0  [Ohm/km]'],
+                                   G0=0.0,
+                                   B0=0.0)
+
+            lst.append(tpe)
+
+        return lst
+    else:
+        return list()
+
+
+def get_wires_catalogue():
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    fname = os.path.join(path, '..', '..', 'data', 'wires.csv')
+
+    if os.path.exists(fname):
+        df = pd.read_csv(fname)
+
+        lst = list()
+        for i, item in df.iterrows():
+
+            '''
+            Size,Stranding,Material,Diameter [cm],GMR [m],R [Ohm/km],Rating [kA]
+            '''
+            name = str(item['Stranding']) + '_' + str(item['Material']) + '_' + str(item['Diameter [cm]'])
+            tpe = Wire(name=name,
+                       gmr=item['GMR [m]'],
+                       r=item['R [Ohm/km]'],
+                       x=0.0,
+                       max_current=item['Rating [kA]'])
+            lst.append(tpe)
+
+        return lst
+    else:
+        return list()
+
+
 if __name__ == '__main__':
-    get_transformer_catalogue()
+    tr = get_transformer_catalogue()
+    cab = get_cables_catalogue()
+    wi = get_wires_catalogue()
+    print()
