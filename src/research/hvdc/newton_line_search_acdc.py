8fcbb745b6929438f3abd02b1759d820c3ec332c
santi <santiago.penate.vera@gmail.com>
2020-08-13 14:54:34 +0100

    oddly working, although the indices od 'm' seem to be wrong

--- src/research/hvdc/newton_line_search_acdc.py
+++ src/research/hvdc/newton_line_search_acdc.py
@@ -83,6 +83,9 @@ def determine_branch_indices(circuit):
     idx_vt = list()
     idx_qt = list()
 
+    idx_mvf = list()
+    idx_mvt = list()
+
     for k, tpe in enumerate(circuit.control_mode):
 
         f = circuit.F[k]
@@ -96,37 +99,51 @@ def determine_branch_indices(circuit):
 
         elif tpe == TransformerControlType.v_from:
             idx_vf.append(f)
+            idx_mvf.append(k)
 
         elif tpe == TransformerControlType.v_to:
             idx_vt.append(t)
+            idx_mvt.append(k)
 
         elif tpe == TransformerControlType.angle_v_from:
             idx_pf.append(k)
             idx_vf.append(f)
+            idx_mvf.append(k)
 
         elif tpe == TransformerControlType.angle_v_to:
             idx_pf.append(k)
             idx_vt.append(t)
+            idx_mvt.append(k)
 
-        elif tpe == ConverterControlType.theta_vac:
+        # VSC ----------------------------------------------------------------------------------------------------------
+        elif tpe == ConverterControlType.theta_vac:  # type 1
             idx_qz.append(k)
-            idx_vt.append(t)
 
-        elif tpe == ConverterControlType.pf_qac:
+            idx_vt.append(t)
+            idx_mvt.append(k)
+
+        elif tpe == ConverterControlType.pf_qac:  # type 2
             idx_qz.append(k)
             idx_pf.append(k)
             idx_qt.append(k)
 
-        elif tpe == ConverterControlType.pf_vac:
+        elif tpe == ConverterControlType.pf_vac:  # type 3
             idx_qz.append(k)
             idx_pf.append(k)
-            idx_vt.append(t)
 
-        elif tpe == ConverterControlType.vdc_qac:
+            idx_vt.append(t)
+            idx_mvt.append(k)
+
+        elif tpe == ConverterControlType.vdc_qac:  # type 4
+
+            idx_mvf.append(k)
             idx_vf.append(f)
             idx_qt.append(k)
 
-        elif tpe == ConverterControlType.vdc_vac:
+        elif tpe == ConverterControlType.vdc_vac:  # type 5
+
+            idx_mvf.append(k)
+            idx_mvt.append(k)
             idx_vf.append(f)
             idx_vt.append(t)
 
@@ -136,10 +153,10 @@ def determine_branch_indices(circuit):
         else:
             raise Exception('Unknown control type:' + str(tpe))
 
-    return idx_pf, idx_qz, idx_vf, idx_vt, idx_qt
+    return idx_pf, idx_qz, idx_vf, idx_vt, idx_qt, idx_mvf, idx_mvt
 
 
-def compile_y2(circuit, m, theta, Beq, If):
+def compile_y(circuit, m, theta, Beq, If):
     """
 
     :param circuit:
@@ -180,168 +197,7 @@ def compile_y2(circuit, m, theta, Beq, If):
     return Ybus, Yf, Yt
 
 
-# def compute_g(V, S0, m, theta, Beq, If, Pset, Qset,
-#               pqpv, pq, idx_pf, idx_qz, idx_vf, idx_vt, idx_qt):
-#     """
-#
-#     :param V:
-#     :param S0:
-#     :param m:
-#     :param theta:
-#     :param Beq:
-#     :param If:
-#     :param pqpv:
-#     :param pq:
-#     :param idx_pf:
-#     :param idx_qz:
-#     :param idx_vf:
-#     :param idx_vt:
-#     :param idx_qt:
-#     :return:
-#     """
-#
-#     Ybus, Yf, Yt = compile_y2(circuit=nc, m=m, theta=theta, Beq=Beq, If=If)
-#
-#     S_calc = V * np.conj(Ybus * V)
-#
-#     # equation (6)
-#     gs = S_calc - S0
-#
-#     If = Yf * V
-#     It = Yt * V
-#     Vf = nc.C_branch_bus_f * V
-#     Vt = nc.C_branch_bus_t * V
-#     Sf = Vf * np.conj(If)  # eq. (8)
-#     St = Vt * np.conj(It)  # eq. (9)
-#
-#     gp = gs.real[pqpv]  # eq. (12)
-#     gq = gs.imag[pq]  # eq. (13)
-#
-#     # controls that the specified power flow is met
-#     # Applicable to:
-#     # - VSC devices where the power flow is set, this is types 2 and 3
-#     # - Transformer devices where the power flow is set (is it?, apparently it only depends on the value of theta)
-#     gsh = Sf.real[idx_pf] - Pset[idx_pf]  # eq. (14)
-#
-#     # controls that 'Beq' absorbs the reactive power.
-#     # Applicable to:
-#     # - All the VSC converters (is it?)
-#     gqz = Sf.imag[idx_qz]  # eq. (15)
-#
-#     # Controls that 'ma' modulates to control the "voltage from" module.
-#     # Applicable to:
-#     # - Transformers that control the "from" voltage side
-#     # - VSC that control the "from" voltage side, this is types 4 and 5
-#     gvf = gs.imag[idx_vf]  # eq. (16)
-#
-#     # Controls that 'ma' modulates to control the "voltage to" module.
-#     # Applicable to:
-#     # - Transformers that control the "to" voltage side
-#     # - VSC that control the "from" voltage side, this is types 1, 3, 5 and 7
-#     gvt = gs.imag[idx_vt]  # eq. (17)
-#
-#     # controls that the specified reactive power flow is met, this is Qf=0
-#     # Applicable to:
-#     # - All VSC converters (is it?)  this is types 2, 4, 6
-#     gqt = St.imag[idx_qt] - Qset[idx_qt]  # eq. (18)
-#
-#     # return the complete mismatch function
-#     return np.r_[gp, gq, gsh, gqz, gvf, gvt, gqt]
-#
-
-def dSbus_dV(Ybus, V, I):
-    """
-    Computes partial derivatives of power injection w.r.t. voltage.
-    :param Ybus: Admittance matrix
-    :param V: Bus voltages array
-    :param I: Bus current injections array
-    :return:
-    """
-    '''
-    Computes partial derivatives of power injection w.r.t. voltage.
-
-    Returns two matrices containing partial derivatives of the complex bus
-    power injections w.r.t voltage magnitude and voltage angle respectively
-    (for all buses). If C{Ybus} is a sparse matrix, the return values will be
-    also. The following explains the expressions used to form the matrices::
-
-        Ibus = Ybus * V - I
-
-        S = diag(V) * conj(Ibus) = diag(conj(Ibus)) * V
-
-    Partials of V & Ibus w.r.t. voltage magnitudes::
-        dV/dVm = diag(V / abs(V))
-        dI/dVm = Ybus * dV/dVm = Ybus * diag(V / abs(V))
-
-    Partials of V & Ibus w.r.t. voltage angles::
-        dV/dVa = j * diag(V)
-        dI/dVa = Ybus * dV/dVa = Ybus * j * diag(V)
-
-    Partials of S w.r.t. voltage magnitudes::
-        dS/dVm = diag(V) * conj(dI/dVm) + diag(conj(Ibus)) * dV/dVm
-               = diag(V) * conj(Ybus * diag(V / abs(V)))
-                                        + conj(diag(Ibus)) * diag(V / abs(V))
-
-    Partials of S w.r.t. voltage angles::
-        dS/dVa = diag(V) * conj(dI/dVa) + diag(conj(Ibus)) * dV/dVa
-               = diag(V) * conj(Ybus * j * diag(V))
-                                        + conj(diag(Ibus)) * j * diag(V)
-               = -j * diag(V) * conj(Ybus * diag(V))
-                                        + conj(diag(Ibus)) * j * diag(V)
-               = j * diag(V) * conj(diag(Ibus) - Ybus * diag(V))
-
-    For more details on the derivations behind the derivative code used
-    in PYPOWER information, see:
-
-    [TN2]  R. D. Zimmerman, "AC Power Flows, Generalized OPF Costs and
-    their Derivatives using Complex Matrix Notation", MATPOWER
-    Technical Note 2, February 2010.
-    U{http://www.pserc.cornell.edu/matpower/TN2-OPF-Derivatives.pdf}
-
-    @author: Ray Zimmerman (PSERC Cornell)
-    '''
-
-    ib = range(len(V))
-
-    Ibus = Ybus * V - I
-
-    diagV = sparse((V, (ib, ib)))
-    diagIbus = sparse((Ibus, (ib, ib)))
-    diagVnorm = sparse((V / np.abs(V), (ib, ib)))
-
-    dS_dVm = diagV * conj(Ybus * diagVnorm) + conj(diagIbus) * diagVnorm
-    dS_dVa = 1.0j * diagV * conj(diagIbus - Ybus * diagV)
-
-    return dS_dVm, dS_dVa
-
-
-def Jacobian(Ybus, V, Ibus, pq, pvpq):
-    """
-    Computes the system Jacobian matrix
-    Args:
-        Ybus: Admittance matrix
-        V: Array of nodal voltages
-        Ibus: Array of nodal current injections
-        pq: Array with the indices of the PQ buses
-        pvpq: Array with the indices of the PV and PQ buses
-
-    Returns:
-        The system Jacobian matrix
-    """
-    dS_dVm, dS_dVa = dSbus_dV(Ybus, V, Ibus)  # compute the derivatives
-
-    J11 = dS_dVa[array([pvpq]).T, pvpq].real
-    J12 = dS_dVm[array([pvpq]).T, pq].real
-    J21 = dS_dVa[array([pq]).T, pvpq].imag
-    J22 = dS_dVm[array([pq]).T, pq].imag
-
-    J = vstack([hstack([J11, J12]),
-                hstack([J21, J22])], format="csr")
-
-    return J
-
-
-def split_x(x, pq, pvpq, idx_pf, idx_qz, idx_vf, idx_vt, idx_qt):
+def split_x(x, pq, pvpq, idx_pf, idx_qz, idx_mvf, idx_mvt, idx_qt):
     """
 
     :param x:
@@ -349,14 +205,14 @@ def split_x(x, pq, pvpq, idx_pf, idx_qz, idx_vf, idx_vt, idx_qt):
     :param pvpq:
     :param idx_pf:
     :param idx_qz:
-    :param idx_vf:
-    :param idx_vt:
+    :param idx_mvf:
+    :param idx_mvt:
     :param idx_qt:
     :return:
     """
     a = 0
     b = len(pvpq)
-    va= x[a:b]
+    va = x[a:b]
 
     a = b
     b += len(pq)
@@ -371,11 +227,11 @@ def split_x(x, pq, pvpq, idx_pf, idx_qz, idx_vf, idx_vt, idx_qt):
     Beq1 = x[a:b]  # Beq for Qflow = 0
 
     a = b
-    b += len(idx_vf)
+    b += len(idx_mvf)
     m1 = x[a:b]  # m controlling Vf
 
     a = b
-    b += len(idx_vt)
+    b += len(idx_mvt)
     m2 = x[a:b]  # m controlling Vt
 
     a = b
@@ -392,7 +248,16 @@ def gx_function(x, args):
     :param args:
     :return:
     """
-    nc, pq, pvpq, idx_pf, idx_qz, idx_vf, idx_vt, idx_qt, Yf, Yt, S0, Pset, Qset, If, va_, vm_, m_, theta_, Beq_ = args
+
+    '''
+    args=(nc, pq, pvpq, idx_pf, idx_qz, idx_mvf, idx_mvt, idx_vf, idx_vt, idx_qt,
+          Yf, Yt, S0, Pset, Qset, If,
+          va, vm, m, theta, Beq),
+    '''
+
+    nc, pq, pvpq, idx_pf, idx_qz, idx_mvf, idx_mvt, idx_vf, idx_vt, idx_qt, \
+    Yf, Yt, S0, Pset, Qset, If, \
+    va_, vm_, m_, theta_, Beq_ = args
 
     va = va_.copy()
     vm = vm_.copy()
@@ -400,8 +265,8 @@ def gx_function(x, args):
     theta = theta_.copy()
     Beq = Beq_.copy()
 
-    # update the variables
-    va1, vm1, theta1, Beq1, m1, m2, Beq2 = split_x(x, pq, pvpq, idx_pf, idx_qz, idx_vf, idx_vt, idx_qt)
+    # update the variables:                        x, pq, pvpq, idx_pf, idx_qz, idx_mvf, idx_mvt, idx_qt
+    va1, vm1, theta1, Beq1, m1, m2, Beq2 = split_x(x, pq, pvpq, idx_pf, idx_qz, idx_mvf, idx_mvt, idx_qt)
     va[pvpq] = va1
     vm[pq] = vm1
     theta[idx_pf] = theta1
@@ -422,7 +287,7 @@ def gx_function(x, args):
     St = Vt * np.conj(It)  # eq. (9)
 
     # compute admittances as a function of the branch variables
-    Ybus, Yf, Yt = compile_y2(circuit=nc, m=m, theta=theta, Beq=Beq, If=If)
+    Ybus, Yf, Yt = compile_y(circuit=nc, m=m, theta=theta, Beq=Beq, If=If)
 
     # compute the new mismatch (g)
     S_calc = V * np.conj(Ybus * V)  # compute Bus power injections
@@ -434,6 +299,7 @@ def gx_function(x, args):
     gvf = gs.imag[idx_vf]  # eq. (16) Controls that 'ma' modulates to control the "voltage from" module.
     gvt = gs.imag[idx_vt]  # eq. (17) Controls that 'ma' modulates to control the "voltage to" module.
     gqt = St.imag[idx_qt] - Qset[idx_qt]  # eq. (18) controls that the specified reactive power flow is met
+
     g = np.r_[gp, gq, gsh, gqz, gvf, gvt, gqt]  # complete mismatch function
 
     return g
@@ -453,29 +319,37 @@ def numerical_jacobian(f, x, args, dx=1e-8):
     jac = np.zeros((n, n))
     for j in range(n):  # through columns to allow for vector addition
         Dxj = (np.abs(x[j]) * dx if x[j] != 0 else dx)
-        x_plus = [(xi if k != j else xi + Dxj) for k, xi in enumerate(x)]
+        x_plus = np.array([(xi if k != j else xi + Dxj) for k, xi in enumerate(x)])
         f1 = f(x_plus, args)
         col = (f1 - f0) / Dxj
         jac[:, j] = col
     return jac
 
 
-def nr_acdc(nc: AcDcSnapshotCircuit):
+def nr_acdc(nc: AcDcSnapshotCircuit, tolerance=1e-6, max_iter=4):
     """
 
     :param nc:
+    :param tolerance:
+    :param max_iter:
     :return:
     """
     # compute the indices of the converter/transformer variables from their control strategies
-    idx_pf, idx_qz, idx_vf, idx_vt, idx_qt = determine_branch_indices(circuit=nc)
+    idx_pf, idx_qz, idx_vf, idx_vt, idx_qt, idx_mvf, idx_mvt = determine_branch_indices(circuit=nc)
 
     # initialize the variables
     V = nc.Vbus
     S0 = nc.Sbus
+    va = np.angle(V)
+    vm = np.abs(V)
+    m = nc.m.copy()
+    theta = nc.theta.copy() * 0
+    Beq = nc.Beq.copy() * 0
     Pset = nc.Pset / nc.Sbase
     Qset = nc.Qset / nc.Sbase
-    pq = nc.pq.copy()
-    pvpq_orig = np.r_[nc.pv, pq]
+    pq = nc.pq.copy().astype(int)
+    pvpq_orig = np.r_[nc.pv, pq].astype(int)
+    pvpq_orig.sort()
 
     # the elements of PQ that exist in the control indices Ivf and Ivt must be passed from the PQ to the PV list
     # otherwise those variables would be in two sets of equations
@@ -485,20 +359,13 @@ def nr_acdc(nc: AcDcSnapshotCircuit):
             pq = pq[pq != val]
 
     # compose the new pvpq indices à la NR
-    pv = np.unique(np.r_[i_ctrl_v, nc.pv])
+    pv = np.unique(np.r_[i_ctrl_v, nc.pv]).astype(int)
     pv.sort()
-    pvpq = np.r_[pv, pq]
-    npqpv = len(pvpq)
-    tolerance = 1e-6
-    max_iter = 4
+    pvpq = np.r_[pv, pq].astype(int)
+    pvpq.sort()
 
-    # initialize variables
-    va = np.angle(V)
-    vm = np.abs(V)
-    m = nc.m.copy()
-    theta = nc.theta.copy()
-    Beq = nc.Beq.copy() * 0
-    Ybus, Yf, Yt = compile_y2(circuit=nc, m=m, theta=theta, Beq=Beq, If=nc.Inom)
+    # compute initial admittances
+    Ybus, Yf, Yt = compile_y(circuit=nc, m=m, theta=theta, Beq=Beq, If=nc.Inom)
 
     # compute branch flows
     If = Yf * V
@@ -509,7 +376,7 @@ def nr_acdc(nc: AcDcSnapshotCircuit):
     St = Vt * np.conj(It)  # eq. (9)
 
     # compute admittances as a function of the branch variables
-    Ybus, Yf, Yt = compile_y2(circuit=nc, m=m, theta=theta, Beq=Beq, If=If)
+    Ybus, Yf, Yt = compile_y(circuit=nc, m=m, theta=theta, Beq=Beq, If=If)
 
     # compute the mismatch (g)
     S_calc = V * np.conj(Ybus * V)  # compute Bus power injections
@@ -524,7 +391,7 @@ def nr_acdc(nc: AcDcSnapshotCircuit):
     g = np.r_[gp, gq, gsh, gqz, gvf, gvt, gqt]  # return the complete mismatch function
 
     # compose the initial x value
-    x = np.r_[va[pvpq], vm[pq], theta[idx_pf], Beq[idx_qz], m[idx_vf], m[idx_vt], Beq[idx_qt]]
+    x = np.r_[va[pvpq], vm[pq], theta[idx_pf], Beq[idx_qz], m[idx_mvf], m[idx_mvt], Beq[idx_qt]]
 
     # compute the error
     ff = np.r_[gs[pvpq_orig].real, gs[pq].imag]  # concatenate to form the mismatch function
@@ -542,11 +409,27 @@ def nr_acdc(nc: AcDcSnapshotCircuit):
 
     var_names = ['va' + str(k) for k in pvpq] \
                 + ['vm' + str(k) for k in pq] \
-                + ['Ɵ' + str(k) for k in idx_pf] \
+                + ['Ɵsh' + str(k) for k in idx_pf] \
                 + ['Beq' + str(k) for k in idx_qz] \
-                + ['m' + str(k) for k in idx_vf] \
-                + ['m' + str(k) for k in idx_vt] \
+                + ['m' + str(k) for k in idx_mvf] \
+                + ['m' + str(k) for k in idx_mvt] \
                 + ['Beq' + str(k) for k in idx_qt]
+
+    type_names = [None] * len(V)
+    for i in pq:
+        type_names[i] = 'PQ'
+    for i in pv:
+        type_names[i] = 'PV'
+    for i in nc.vd:
+        type_names[i] = 'VD'
+
+    print(['Ish' + str(k) for k in idx_pf])
+    print(['IQz' + str(k) for k in idx_qz])
+    print(['Imvf' + str(k) for k in idx_mvf])
+    print(['Imvt' + str(k) for k in idx_mvt])
+    print(['Ivf' + str(k) for k in idx_vf])
+    print(['Ivt' + str(k) for k in idx_vt])
+    print(['IQt' + str(k) for k in idx_qt])
 
     while norm_f > tolerance and iterations < max_iter:
         print('-' * 200)
@@ -554,9 +437,10 @@ def nr_acdc(nc: AcDcSnapshotCircuit):
         # compute the numerical Jacobian
         J = numerical_jacobian(gx_function,
                                x,
-                               args=(nc, pq, pvpq, idx_pf, idx_qz, idx_vf, idx_vt, idx_qt,
+                               args=(nc, pq, pvpq, idx_pf, idx_qz, idx_mvf, idx_mvt, idx_vf, idx_vt, idx_qt,
                                      Yf, Yt, S0, Pset, Qset, If,
-                                     va, vm, m, theta, Beq))
+                                     va, vm, m, theta, Beq),
+                               dx=tolerance)
 
         print('J:')
         print(pd.DataFrame(data=J, columns=var_names, index=eq_names))
@@ -576,7 +460,7 @@ def nr_acdc(nc: AcDcSnapshotCircuit):
         x -= dx
 
         # update the variables
-        va1, vm1, theta1, Beq1, m1, m2, Beq2 = split_x(x, pq, pvpq, idx_pf, idx_qz, idx_vf, idx_vt, idx_qt)
+        va1, vm1, theta1, Beq1, m1, m2, Beq2 = split_x(x, pq, pvpq, idx_pf, idx_qz, idx_mvf, idx_mvt, idx_qt)
         va[pvpq] = va1
         vm[pq] = vm1
         theta[idx_pf] = theta1
@@ -597,7 +481,7 @@ def nr_acdc(nc: AcDcSnapshotCircuit):
         St = Vt * np.conj(It)  # eq. (9)
 
         # compute admittances as a function of the branch variables
-        Ybus, Yf, Yt = compile_y2(circuit=nc, m=m, theta=theta, Beq=Beq, If=If)
+        Ybus, Yf, Yt = compile_y(circuit=nc, m=m, theta=theta, Beq=Beq, If=If)
 
         # compute the new mismatch (g)
         S_calc = V * np.conj(Ybus * V)  # compute Bus power injections
@@ -619,15 +503,22 @@ def nr_acdc(nc: AcDcSnapshotCircuit):
         iterations += 1
 
     print('END', '-' * 200)
-    print('V:\n', V)
-    print('Sf:\n', Sf)
-    print('error:', norm_f)
+    print('Bus values')
+    print(pd.DataFrame(data=np.c_[type_names, S_calc.real, S_calc.imag, gs.real, gs.imag, vm, va],
+                       columns=['Type', 'P', 'Q', '∆P', '∆Q', 'Vm', 'Va'],
+                       index=nc.bus_names))
+    print('\nBranch values')
+    print(pd.DataFrame(data=np.c_[nc.F, nc.T, nc.control_mode, nc.Pset, nc.Qset, nc.vf_set, nc.vt_set,
+                                  vm[nc.F] - vm[nc.T], va[nc.F] - va[nc.T],
+                                  Sf.real, Sf.imag, Sf.real, Sf.imag, m, theta, Beq],
+                       columns=['from', 'to', 'Ctrl mode', 'Pset', 'Qset', 'Vfset', 'Vtset',
+                                '∆Vm', '∆Va', 'Pf', 'Qf', 'Pt', 'Qt', 'm', 'Ɵ', 'Beq'],
+                       index=nc.branch_names))
+    print('\nerror:', norm_f)
 
     return norm_f, V, m, theta, Beq
 
-########################################################################################################################
-#  MAIN
-########################################################################################################################
+
 if __name__ == "__main__":
 
     np.set_printoptions(linewidth=10000)
@@ -636,6 +527,8 @@ if __name__ == "__main__":
     pd.set_option('display.width', 1000)
 
     fname = '/home/santi/Documentos/GitHub/GridCal/Grids_and_profiles/grids/LineHVDCGrid.gridcal'
+    # fname = '/home/santi/Documentos/GitHub/GridCal/Grids_and_profiles/grids/IEEE57+IEEE14 DC grid.gridcal'
+    # fname = '/home/santi/Documentos/GitHub/GridCal/Grids_and_profiles/grids/ACDC_example_grid.gridcal'
     grid = FileOpen(fname).open()
 
     ####################################################################################################################
@@ -643,5 +536,5 @@ if __name__ == "__main__":
     ####################################################################################################################
     nc_ = compile_acdc_snapshot_circuit(grid)
 
-    res = nr_acdc(nc=nc_)
+    res = nr_acdc(nc=nc_, tolerance=1e-8, max_iter=2)
 
