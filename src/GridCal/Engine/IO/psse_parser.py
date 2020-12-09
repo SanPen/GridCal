854df0cfabf10a8adbf873a9bf3ca80051302403
penversa <santiago.penate@ree.es>
2020-12-09 15:29:19 +0100

    Adapted psse parser

--- src/GridCal/Engine/IO/psse_parser.py
+++ src/GridCal/Engine/IO/psse_parser.py
@@ -2276,14 +2276,16 @@ class PSSeFACTS:
             # only shunt device: STATCOM
             logger.append('FACTS mode ' + str(mode) + ' not implemented')
 
-        elif mode == 3 and abs(self.J) > 0:
+        elif mode == 3 and abs(self.J) > 0:  # const Z
             # series and shunt links operating with series link at constant series impedance
             sh = Shunt(name='FACTS:' + name1, B=self.SHMX)
             load_from = Load(name='FACTS:' + name1, P=-self.PDES, Q=-self.QDES)
-            load_to = Load(name='FACTS:' + name1, P=self.PDES, Q=self.QDES)
+            gen_to = Generator(name='FACTS:' + name1, active_power=self.PDES, voltage_module=self.VSET)
+            # branch = Line(bus_from=bus1, bus_to=bus2, name='FACTS:' + name1, x=self.LINX)
             circuit.add_shunt(bus1, sh)
             circuit.add_load(bus1, load_from)
-            circuit.add_load(bus2, load_to)
+            circuit.add_generator(bus2, gen_to)
+            # circuit.add_line(branch)
 
         elif mode == 4 and abs(self.J) > 0:
             # series and shunt links operating with series link at constant series voltage
