3db48b58eeac152f97724f21a0c9b24de8afe19e
RAMFERAN <aramiro@ree.es>
2022-04-25 10:29:18 +0200

    added contingency rates as profile

--- src/build/lib/GridCal/Engine/Simulations/Clustering/__init__.py
+++
@@ -1,1 +0,0 @@
-from GridCal.Engine.Simulations.Clustering.clustering import *
--- src/build/lib/GridCal/Engine/Simulations/ContingencyAnalysis/__init__.py
+++
@@ -1,4 +0,0 @@
-from GridCal.Engine.Simulations.ContingencyAnalysis.contingency_analysis_driver import *
-from GridCal.Engine.Simulations.ContingencyAnalysis.contingency_analysis_results import *
-from GridCal.Engine.Simulations.ContingencyAnalysis.contingency_analysis_ts_driver import *
-from GridCal.Engine.Simulations.ContingencyAnalysis.contingency_analysis_ts_results import *
