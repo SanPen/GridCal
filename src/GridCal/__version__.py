f124fb65cb73b7406269e8b6f5085b5377f64fc4
Santi <santiago@gridcal.com>
2023-05-13 14:38:21 +0100

    4.7.6

--- src/GridCal/__version__.py
+++ src/GridCal/__version__.py
@@ -2,7 +2,7 @@ import datetime
 _current_year_ = datetime.datetime.now().year
 
 # do not forget to keep a three-number version!!!
-__GridCal_VERSION__ = "4.7.5"
+__GridCal_VERSION__ = "4.7.6"
 
 url = 'https://github.com/SanPen/GridCal'
 
