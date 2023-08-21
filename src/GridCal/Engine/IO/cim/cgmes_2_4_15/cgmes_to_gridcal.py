f40ae3322e18c65af495e1983fc20bc625947d0e
santi <santiago.penate.vera@gmail.com>
2023-08-21 12:47:37 +0100

    added CGMES trasnsformer and line conversions

--- src/GridCal/Engine/IO/cim/cgmes_2_4_15/cgmes_to_gridcal.py
+++ src/GridCal/Engine/IO/cim/cgmes_2_4_15/cgmes_to_gridcal.py
@@ -20,6 +20,7 @@ import GridCal.Engine.IO.cim.cgmes_2_4_15.cim_enums as cgmes_enums
 from GridCal.Engine.IO.cim.cgmes_2_4_15.devices.terminal import Terminal
 from GridCal.Engine.IO.cim.cgmes_2_4_15.devices.identified_object import IdentifiedObject
 from GridCal.Engine.IO.cim.cgmes_2_4_15.cgmes_circuit import CgmesCircuit
+from GridCal.Engine.IO.cim.cgmes_2_4_15.devices.branches.transformer.power_transformer_end import PowerTransformerEnd
 from GridCal.Engine.Core.Devices.multi_circuit import MultiCircuit
 import GridCal.Engine.Core.Devices as gcdev
 from GridCal.Engine.basic_structures import Logger
@@ -380,7 +381,8 @@ def get_gcdev_ac_lines(cgmes_model: CgmesCircuit,
                        calc_node_dict: Dict[str, gcdev.Bus],
                        cn_dict: Dict[str, gcdev.ConnectivityNode],
                        device_to_terminal_dict: Dict[str, List[Terminal]],
-                       logger: Logger) -> None:
+                       logger: Logger,
+                       Sbase: float) -> None:
     """
     Convert the CGMES ac lines to gcdev
     :param cgmes_model: CgmesCircuit
@@ -389,6 +391,7 @@ def get_gcdev_ac_lines(cgmes_model: CgmesCircuit,
     :param cn_dict: Dict[str, gcdev.ConnectivityNode]
     :param device_to_terminal_dict: Dict[str, Terminal]
     :param logger:
+    :param Sbase: 100 MVA
     :return:
     """
     # convert ac lines
@@ -406,6 +409,16 @@ def get_gcdev_ac_lines(cgmes_model: CgmesCircuit,
                 cn_f = cns[0]
                 cn_t = cns[1]
 
+                z_base = cgmes_elm.BaseVoltage.nominalVoltage ** 2 / Sbase
+                r = cgmes_elm.r / z_base
+                x = cgmes_elm.x / z_base
+                g = cgmes_elm.gch * z_base
+                b = cgmes_elm.bch * z_base
+                r0 = cgmes_elm.r0 / z_base
+                x0 = cgmes_elm.x0 / z_base
+                g0 = cgmes_elm.gch0 * z_base
+                b0 = cgmes_elm.bch0 * z_base
+
                 gcdev_elm = gcdev.Line(idtag=cgmes_elm.uuid,
                                        code=cgmes_elm.description,
                                        name=cgmes_elm.name,
@@ -414,13 +427,14 @@ def get_gcdev_ac_lines(cgmes_model: CgmesCircuit,
                                        # cn_to=cn_t,
                                        bus_from=calc_node_f,
                                        bus_to=calc_node_t,
-                                       r=0.0,
-                                       x=0.0,
-                                       b=0.0,
-                                       r0=0.0,
-                                       x0=0.0,
-                                       b0=0.0,
-                                       length=0.0)
+                                       r=r,
+                                       x=x,
+                                       b=b,
+                                       r0=r0,
+                                       x0=x0,
+                                       b0=b0,
+                                       rate=1e-20,
+                                       length=cgmes_elm.length)
                 gcdev_model.add_line(gcdev_elm, logger=logger)
             else:
                 logger.add_error(msg='Not exactly two terminals',
@@ -431,228 +445,103 @@ def get_gcdev_ac_lines(cgmes_model: CgmesCircuit,
                                  expected_value=2)
 
 
-# def parse_bus_bars(cim: CgmesCircuit, circuit: MultiCircuit, logger: Logger):
-#     """
-#
-#     :param cim:
-#     :param circuit:
-#     :param logger:
-#     :return:
-#     """
-#
-#     busbar_dict = dict()
-#
-#     if 'BusbarSection' in cim.elements_by_type.keys():
-#         for elm in cim.elements_by_type['BusbarSection']:
-#             obj = gcdev.Bus(name=str(elm.name),
-#                             idtag=elm.uuid)
-#
-#             circuit.add_bus(obj)
-#
-#             busbar_dict[elm] = obj
-#     else:
-#         logger.add_error("No BusbarSections: There is no chance to reduce the grid")
-#
-#     return busbar_dict
-#
-#
-# def parse_ac_line_segment(cim: CgmesCircuit, circuit: MultiCircuit, busbar_dict, logger: Logger):
-#     """
-#
-#     :param cim:
-#     :param circuit:
-#     :param busbar_dict:
-#     :return:
-#     """
-#
-#     if 'ACLineSegment' in cim.elements_by_type.keys():
-#         for elm in cim.elements_by_type['ACLineSegment']:
-#
-#             b1, b2 = elm.get_buses()
-#
-#             B1, B2 = try_buses(b1, b2, busbar_dict)
-#
-#             if B1 is not None and B2 is not None:
-#                 R, X, G, B = elm.get_pu_values()
-#                 rate = elm.get_rate()
-#
-#                 # create AcLineSegment (Line)
-#                 line = gcdev.Line(idtag=elm.uuid,
-#                                   bus_from=B1,
-#                                   bus_to=B2,
-#                                   name=str(elm.name),
-#                                   r=R,
-#                                   x=X,
-#                                   b=B,
-#                                   rate=rate,
-#                                   active=True,
-#                                   mttf=0,
-#                                   mttr=0)
-#
-#                 circuit.add_line(line)
-#             else:
-#                 logger.add_error('Bus not found', elm.rdfid)
-#
-#
-# def parse_power_transformer(cim: CgmesCircuit, circuit: MultiCircuit, busbar_dict, logger: Logger):
-#     """
-#
-#     :param cim:
-#     :param circuit:
-#     :param busbar_dict:
-#     :return:
-#     """
-#     if 'PowerTransformer' in cim.elements_by_type.keys():
-#         for elm in cim.elements_by_type['PowerTransformer']:
-#             b1, b2 = elm.get_buses()
-#             B1, B2 = try_buses(b1, b2, busbar_dict)
-#
-#             if B1 is not None and B2 is not None:
-#                 R, X, G, B = elm.get_pu_values()
-#                 rate = elm.get_rate()
-#
-#                 voltages = elm.get_voltages()
-#                 voltages.sort()
-#
-#                 if len(voltages) == 2:
-#                     lv, hv = voltages
-#                 else:
-#                     lv = 1
-#                     hv = 1
-#                     logger.add_error('Could not parse transformer nominal voltages', elm.name)
-#
-#                 line = gcdev.Transformer2W(idtag=cimdev.rfid2uuid(elm.rdfid),
-#                                            bus_from=B1,
-#                                            bus_to=B2,
-#                                            name=str(elm.name),
-#                                            r=R,
-#                                            x=X,
-#                                            g=G,
-#                                            b=B,
-#                                            rate=rate,
-#                                            tap=1.0,
-#                                            shift_angle=0,
-#                                            active=True,
-#                                            HV=hv,
-#                                            LV=lv)
-#
-#                 circuit.add_branch(line)
-#             else:
-#                 logger.add_error('Bus not found', elm.rdfid)
-#
-#
-# def parse_switches(cim: CgmesCircuit, circuit: MultiCircuit, busbar_dict, logger: Logger):
-#     """
-#
-#     :param cim:
-#     :param circuit:
-#     :param busbar_dict:
-#     :return:
-#     """
-#     EPS = 1e-20
-#     cim_switches = ['Switch', 'Disconnector', 'Breaker', 'LoadBreakSwitch']
-#     if any_in_dict(cim.elements_by_type, cim_switches):
-#         for elm in get_elements(cim.elements_by_type, cim_switches):
-#             b1, b2 = elm.get_buses()
-#             B1, B2 = try_buses(b1, b2, busbar_dict)
-#
-#             if B1 is not None and B2 is not None:
-#                 state = True
-#                 line = gcdev.Switch(idtag=elm.uuid,
-#                                     bus_from=B1,
-#                                     bus_to=B2,
-#                                     name=str(elm.name),
-#                                     r=EPS,
-#                                     x=EPS,
-#                                     rate=EPS,
-#                                     active=state)
-#
-#                 circuit.add_switch(line)
-#             else:
-#                 logger.add_error('Bus not found', elm.rdfid)
-#
-#
-# def parse_loads(cim: CgmesCircuit, circuit: MultiCircuit, busbar_dict, logger: Logger):
-#     """
-#
-#     :param cim:
-#     :param circuit:
-#     :param busbar_dict:
-#     :return:
-#     """
-#     cim_loads = ['ConformLoad', 'EnergyConsumer', 'NonConformLoad']
-#     if any_in_dict(cim.elements_by_type, cim_loads):
-#         for elm in get_elements(cim.elements_by_type, cim_loads):
-#
-#             b1 = elm.get_bus()
-#             B1 = try_bus(b1, busbar_dict)
-#
-#             if B1 is not None:
-#
-#                 p, q = elm.get_pq()
-#
-#                 load = gcdev.Load(idtag=elm.uuid,
-#                                   name=str(elm.name),
-#                                   G=0,
-#                                   B=0,
-#                                   Ir=0,
-#                                   Ii=0,
-#                                   P=p if p is not None else 0,
-#                                   Q=q if q is not None else 0)
-#                 circuit.add_load(B1, load)
-#             else:
-#                 logger.add_error('Bus not found', elm.rdfid)
-#
-#
-# def parse_shunts(cim: CgmesCircuit, circuit: MultiCircuit, busbar_dict, logger: Logger):
-#     """
-#
-#     :param cim:
-#     :param circuit:
-#     :param busbar_dict:
-#     :return:
-#     """
-#     if 'ShuntCompensator' in cim.elements_by_type.keys():
-#         for elm in cim.elements_by_type['ShuntCompensator']:
-#             b1 = elm.get_bus()
-#             B1 = try_bus(b1, busbar_dict)
-#
-#             if B1 is not None:
-#                 g = 0
-#                 b = 0
-#                 sh = gcdev.Shunt(idtag=elm.uuid,
-#                                  name=str(elm.name),
-#                                  G=g,
-#                                  B=b)
-#                 circuit.add_shunt(B1, sh)
-#             else:
-#                 logger.add_error('Bus not found', elm.rdfid)
-#
-#
-# def parse_generators(cim: CgmesCircuit, circuit: MultiCircuit, busbar_dict, logger: Logger):
-#     """
-#
-#     :param cim:
-#     :param circuit:
-#     :param busbar_dict:
-#     :return:
-#     """
-#     if 'SynchronousMachine' in cim.elements_by_type.keys():
-#         for elm in cim.elements_by_type['SynchronousMachine']:
-#             b1 = elm.get_bus()
-#             B1 = try_bus(b1, busbar_dict)
-#
-#             if B1 is not None:
-#
-#                 gen = gcdev.Generator(idtag=elm.uuid,
-#                                       name=str(elm.name),
-#                                       active_power=-elm.p,
-#                                       # CGMES defines the generator P as negative to indicate a positive injection
-#                                       voltage_module=1.0)
-#                 circuit.add_generator(B1, gen)
-#
-#             else:
-#                 logger.add_error('Bus not found', elm.rdfid)
+def get_gcdev_ac_transformers(cgmes_model: CgmesCircuit,
+                              gcdev_model: MultiCircuit,
+                              calc_node_dict: Dict[str, gcdev.Bus],
+                              cn_dict: Dict[str, gcdev.ConnectivityNode],
+                              device_to_terminal_dict: Dict[str, List[Terminal]],
+                              logger: Logger) -> None:
+    """
+    Convert the CGMES ac lines to gcdev
+    :param cgmes_model: CgmesCircuit
+    :param gcdev_model: gcdevCircuit
+    :param calc_node_dict: Dict[str, gcdev.Bus]
+    :param cn_dict: Dict[str, gcdev.ConnectivityNode]
+    :param device_to_terminal_dict: Dict[str, Terminal]
+    :param logger:
+    :return:
+    """
+
+    # convert ac lines
+    for device_list in [cgmes_model.PowerTransformer_list]:
+
+        for cgmes_elm in device_list:
+
+            windings: List[PowerTransformerEnd] = list(cgmes_elm.references_to_me['PowerTransformerEnd'])
+
+            if len(windings) == 2:
+                calc_nodes, cns = find_connections(cgmes_elm=cgmes_elm,
+                                                   device_to_terminal_dict=device_to_terminal_dict,
+                                                   calc_node_dict=calc_node_dict,
+                                                   cn_dict=cn_dict,
+                                                   logger=logger)
+
+                if len(calc_nodes) == 2:
+                    calc_node_f = calc_nodes[0]
+                    calc_node_t = calc_nodes[1]
+                    cn_f = cns[0]
+                    cn_t = cns[1]
+
+                    r = 0.0
+                    x = 0.0
+                    g = 0.0
+                    b = 0.0
+                    r0 = 0.0
+                    x0 = 0.0
+                    g0 = 0.0
+                    b0 = 0.0
+                    v1 = windings[0].BaseVoltage.nominalVoltage
+                    v2 = windings[1].BaseVoltage.nominalVoltage
+                    HV = max(v1, v2)
+                    LV = min(v1, v2)
+                    for winding in windings:
+                        z_base = winding.BaseVoltage.nominalVoltage**2 / winding.ratedS
+                        r += winding.r / z_base
+                        x += winding.x / z_base
+                        g += winding.g * z_base
+                        b += winding.b * z_base
+                        r0 += winding.r0 / z_base
+                        x0 += winding.x0 / z_base
+                        g0 += winding.g0 * z_base
+                        b0 += winding.b0 * z_base
+
+                    gcdev_elm = gcdev.Transformer2W(idtag=cgmes_elm.uuid,
+                                                    code=cgmes_elm.description,
+                                                    name=cgmes_elm.name,
+                                                    active=True,
+                                                    # cn_from=cn_f,
+                                                    # cn_to=cn_t,
+                                                    bus_from=calc_node_f,
+                                                    bus_to=calc_node_t,
+                                                    HV=HV,
+                                                    LV=LV,
+                                                    r=r,
+                                                    x=x,
+                                                    g=g,
+                                                    b=b,
+                                                    r0=r0,
+                                                    x0=x0,
+                                                    g0=g0,
+                                                    b0=b0,
+                                                    tap=1.0,
+                                                    shift_angle=0.0)
+
+                    gcdev_model.add_transformer2w(gcdev_elm)
+                else:
+                    logger.add_error(msg='Not exactly two terminals',
+                                     device=cgmes_elm.rdfid,
+                                     device_class=cgmes_elm.tpe,
+                                     device_property="number of associated terminals",
+                                     value=len(calc_nodes),
+                                     expected_value=2)
+
+            else:
+                logger.add_error(msg='Transformers with {} windings not supported yet'.format(len(windings)),
+                                 device=cgmes_elm.rdfid,
+                                 device_class=cgmes_elm.tpe,
+                                 device_property="windings",
+                                 value=len(windings),
+                                 expected_value=2)
 
 
 def cgmes_to_gridcal(cgmes_model: CgmesCircuit, logger: Logger) -> MultiCircuit:
@@ -663,6 +552,7 @@ def cgmes_to_gridcal(cgmes_model: CgmesCircuit, logger: Logger) -> MultiCircuit:
     :return: MultiCircuit
     """
     gc_model = MultiCircuit()
+    Sbase = gc_model.Sbase
 
     # busbar_dict = parse_bus_bars(cgmes_model, circuit, logger)
     # parse_ac_line_segment(cgmes_model, circuit, busbar_dict, logger)
@@ -689,6 +579,7 @@ def cgmes_to_gridcal(cgmes_model: CgmesCircuit, logger: Logger) -> MultiCircuit:
     get_gcdev_loads(cgmes_model, gc_model, calc_node_dict, cn_dict, device_to_terminal_dict, logger)
     get_gcdev_generators(cgmes_model, gc_model, calc_node_dict, cn_dict, device_to_terminal_dict, logger)
 
-    get_gcdev_ac_lines(cgmes_model, gc_model, calc_node_dict, cn_dict, device_to_terminal_dict, logger)
+    get_gcdev_ac_lines(cgmes_model, gc_model, calc_node_dict, cn_dict, device_to_terminal_dict, logger, Sbase)
+    get_gcdev_ac_transformers(cgmes_model, gc_model, calc_node_dict, cn_dict, device_to_terminal_dict, logger)
 
     return gc_model
