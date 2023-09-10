2befabf31fcf16fbe8f7088bea1fec33f87c9214
Santiago Peñate Vera <SanPen@users.noreply.github.com>
2023-09-10 13:22:05 +0100

    split the setups and building the wheels correctly apparently

--- src/GridCalEngine/setup.py
+++ src/GridCalEngine/setup.py
@@ -1,10 +1,63 @@
+"""
+A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+import distutils.command.build
+import os
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+
+from GridCalEngine.__version__ import __GridCalEngine_VERSION__
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 
-pkgs_to_exclude = ['tests', 'Tutorials', 'GridCal']
+# Override build command
+class BuildCommand(distutils.command.build.build):
+    def initialize_options(self):
+        distutils.command.build.build.initialize_options(self)
+        self.build_base = 'build_gridcalengine'
 
-# find_packages will do a terrible job and it will find everything regardless of what you tell it to exclude
-packages = find_packages(where='.', exclude=pkgs_to_exclude)
+
+long_description = '''# GridCal
+
+This software aims to be a complete platform for power systems research and simulation.
+
+[Watch the video https](https://youtu.be/SY66WgLGo54)
+
+[Check out the documentation](https://gridcal.readthedocs.io)
+
+
+## Installation
+
+pip install GridCal
+
+For more options (including a standalone setup one), follow the
+[installation instructions]( https://gridcal.readthedocs.io/en/latest/getting_started/install.html)
+from the project's [documentation](https://gridcal.readthedocs.io)
+'''
+
+# if os.path.exists(os.path.join(here, 'about.txt')):
+#     with open(os.path.join(here, 'about.txt'), encoding='utf-8') as f:
+#         description = f.read()
+#         print(description)
+# else:
+#     description = ''
+#     print('Unable to read the description file')
+description = 'GridCal is a Power Systems simulation program intended for professional use and research'
+
+pkgs_to_exclude = ['docs', 'research', 'research.*', 'tests', 'tests.*', 'Tutorials', 'GridCal']
+
+packages = find_packages(exclude=pkgs_to_exclude)
+
 
 # ... so we have to do the filtering ourselves
 packages2 = list()
@@ -18,14 +71,63 @@ for package in packages:
     if not excluded:
         packages2.append(package)
 
+
+package_data = {'GridCalEngine': ['LICENSE.txt', ], }
+
+
+dependencies = ['setuptools>=41.0.1',
+                'wheel>=0.37.2',
+                "numpy>=1.19.0",
+                "scipy>=1.0.0",
+                "networkx>=2.1",
+                "pandas>=1.0",
+                "ortools>=9.0.0",
+                "xlwt>=1.3.0",
+                "xlrd>=1.1.0",
+                "matplotlib>=2.1.1",
+                "qtconsole>=4.5.4",
+                "openpyxl>=2.4.9",
+                "chardet>=3.0.4",  # for the psse files character detection
+                "scikit-learn>=0.18",
+                "geopy>=1.16",
+                "pytest>=3.8",
+                "h5py>=2.9.0",
+                "numba>=0.46",  # to compile routines natively
+                'pyproj',
+                'pyarrow',
+                'ortools',
+                "nptyping",
+                "windpowerlib",
+                "pvlib",
+                "hyperopt",
+                ]
+
+extras_require = {
+        'gch5 files':  ["tables"]  # this is for h5 compatibility
+    }
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
 setup(
-    name='GridCalEngine',
-    version='1.0.0',  # Update with your version number
-    description='Your backend description',
-    author='Your Name',
-    author_email='your.email@example.com',
-    packages=packages2,  # Automatically discover packages in the GridCalEngine directory
-    install_requires=[
-        # List any dependencies required for the backend package here
+    name='GridCalEngine',  # Required
+    version=__GridCalEngine_VERSION__,  # Required
+    license='LGPL',
+    description=description,  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/SanPen/GridCal',  # Optional
+    author='Santiago Peñate Vera et. Al.',  # Optional
+    author_email='santiago@gridcal.org',  # Optional
+    classifiers=[ 
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Programming Language :: Python :: 3.10',
     ],
+    keywords='power systems planning',  # Optional
+    packages=packages2,  # Required
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires=dependencies,
+    extras_require=extras_require,
+    package_data=package_data,
+    cmdclass={"build": BuildCommand},
 )
--- src/upload_to_pypi.py
+++ src/upload_to_pypi.py
@@ -7,21 +7,43 @@ python3 setup.py sdist
 twine upload dist/GridCal-2.30.tar.gz
 
 """
-
-from subprocess import call
-from GridCal.__version__ import __GridCal_VERSION__
-
 import os
 import sys
+from subprocess import call
+from GridCalEngine.__version__ import __GridCalEngine_VERSION__
+from GridCal.__version__ import __GridCal_VERSION__
 
-call([sys.executable, 'setup_gridcal.py', 'sdist'])
 
-call([sys.executable, 'setup_gridcal.py', 'bdist_wheel'])
+def publish(pkg_name='GridCal', setup_path='setup.py', version='0.0.1'):
+    """
+    Publish package to Pypi using twine
+    :param pkg_name:
+    :param setup_path:
+    :param version:
+    """
+    call([sys.executable, setup_path, 'sdist'])
+    call([sys.executable, setup_path, 'bdist_wheel'])
 
-name = 'GridCal-' + str(__GridCal_VERSION__) + '.tar.gz'
-fpath = os.path.join('dist', name)
+    name = pkg_name + '-' + str(version) + '.tar.gz'
+    fpath = os.path.join('dist', name)
 
-call([sys.executable, '-m', 'twine', 'check', fpath])
+    call([sys.executable, '-m', 'twine', 'check', fpath])
 
-call([sys.executable, '-m', 'twine', 'upload', '--repository', 'pypi', fpath])
+    call([sys.executable, '-m', 'twine', 'upload', '--repository', 'pypi', fpath])
 
+
+if __GridCalEngine_VERSION__ == __GridCal_VERSION__:
+
+    # both packages' versions must be exactly the same
+
+    publish(pkg_name='GridCalEngine',
+            setup_path=os.path.join('GridCalEngine', 'setup.py'),
+            version=__GridCalEngine_VERSION__)
+
+    publish(pkg_name='GridCal',
+            setup_path=os.path.join('GridCal', 'setup.py'),
+            version=__GridCal_VERSION__)
+
+else:
+
+    print(__GridCalEngine_VERSION__, 'and', __GridCal_VERSION__, "are different :(")
