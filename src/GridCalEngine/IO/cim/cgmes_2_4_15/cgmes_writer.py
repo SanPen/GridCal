4318cd4effb19d73885d4ae1c6de0b829cf20c92
santi <santiago.penate.vera@gmail.com>
2023-11-22 07:55:17 +0000

    Fixed bug when setting the file label for when loading many CIM files
    Minor refactoring

--- src/GridCalEngine/IO/cim/cgmes_2_4_15/cgmes_writer.py
+++ src/GridCalEngine/IO/cim/cgmes_2_4_15/cgmes_writer.py
@@ -15,24 +15,43 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import zipfile
-from io import StringIO, TextIOWrapper, BytesIO
-from typing import List
+from typing import List, Union, Callable
 from GridCalEngine.IO.cim.cgmes_2_4_15.cgmes_enums import cgmesProfile
 from GridCalEngine.IO.cim.cgmes_2_4_15.cgmes_circuit import CgmesCircuit
 
 
-def write_cgmes(filename_zip: str, model: CgmesCircuit, profiles: List[cgmesProfile]):
-
+def write_cgmes(filename_zip: str, model: CgmesCircuit, profiles: List[cgmesProfile],
+                text_func: Union[Callable, None] = None,
+                progress_func: Union[Callable, None] = None):
+    """
+    Write a CGMES model to a zip file
+    :param filename_zip: name of the zip file
+    :param model: CgmesCircuit
+    :param profiles: list of profiles to export
+    :param text_func: text callback function (optional)
+    :param progress_func: progress callback function (optional)
+    """
     # get the cgmes XML per profile
     data = model.get_model_xml(profiles)
+
+    n = len(data)
 
     # open zip file for writing
     with zipfile.ZipFile(filename_zip, 'w', zipfile.ZIP_DEFLATED) as f_zip_ptr:
 
         # for each CGMES profile of the list
+        i = 0
         for profile, txt in data.items():
             filename = 'model_{}.xml'.format(profile.value)
-            print('Saving', filename)
+
+            if text_func is not None:
+                text_func('Saving {}'.format(filename))
+
             f_zip_ptr.writestr(filename, txt)  # save the buffer to the zip file
 
-    print("done! see", filename_zip)
+            if progress_func is not None:
+                progress_func((i + 1) / n * 100)
+            i += 1
+
+    if text_func is not None:
+        text_func('done! see: {}'.format(filename_zip))
