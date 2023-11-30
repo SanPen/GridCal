739f7366bde0d576bdea3e073543981a64aa61d1
Carlos-Alegre <calegre@eroots.tech>
2023-11-30 13:11:39 +0100

    Test remove imports

--- examples/acopf_generalize.py
+++ examples/acopf_generalize.py
@@ -1,5 +1,3 @@
-# from GridCalEngine.IO import FileOpen
-# from GridCalEngine.Core.DataStructures.numerical_circuit import compile_numerical_circuit_at
 import GridCalEngine.api as gce
 
 # grid = gce.FileOpen('../Grids_and_profiles/grids/IEEE39.xlsx').open()
