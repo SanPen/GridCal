Corrected bug with Bbus computation Implemented PTDF and LODF definition tests using power flows vs analytical PTDF and LODF tests passing

src/GridCalEngine/Core/admittance_matrices.py
@@ -411,7 +411,6 @@ def compute_fast_decoupled_admittances(X: Vec,
 def compute_linear_admittances(nbr: int,
                                X: Vec,
                                R: Vec,
-                               tap_modules: Vec,
                                active: IntVec,
                                Cf: sp.csc_matrix,
                                Ct: sp.csc_matrix,
@@ -422,30 +421,27 @@ def compute_linear_admittances(nbr: int,
     :param nbr: Number of Branches
     :param X: array of branch reactance (p.u.)
     :param R: array of branch resistance (p.u.)
-    :param tap_modules: array of tap modules (for all Branches, regardless of their type)
     :param active: array of branch active (bool)
     :param Cf: Connectivity branch-bus "from" with the branch states computed
     :param Ct: Connectivity branch-bus "to" with the branch states computed
     :param ac: array of ac Branches indices
     :param dc: array of dc Branches indices
-    :return: Bbus, Bf, Btheta
+    :return: Bbus, Bf, Btau
     """
-
-    m_abs = np.abs(tap_modules)
     if len(dc):
         # compose the vector for AC-DC grids where the R is needed for this matrix
         # even if conceptually we only want the susceptance
         b = np.zeros(nbr)
-        b[ac] = 1.0 / (m_abs[ac] * X[ac] * active[ac] + 1e-20)  # for ac Branches
-        b[dc] = 1.0 / (m_abs[dc] * R[dc] * active[dc] + 1e-20)  # for dc Branches
+        b[ac] = 1.0 / (X[ac] * active[ac] + 1e-20)  # for ac Branches
+        b[dc] = 1.0 / (R[dc] * active[dc] + 1e-20)  # for dc Branches
     else:
-        b = 1.0 / (m_abs * X * active + 1e-20)  # for ac Branches
+        b = 1.0 / (X * active + 1e-20)  # for ac Branches
 
     b_tt = sp.diags(b)  # This is Bd from the
     Bf = b_tt * Cf - b_tt * Ct
     Bt = -b_tt * Cf + b_tt * Ct
     Bbus = Cf.T * Bf + Ct.T * Bt
-    Btheta = (b_tt * (Cf + Ct)).T
+    Btau = (b_tt * (Cf + Ct)).T
 
     """
     According to the KULeuven document "DC power flow in unit commitment models"
@@ -455,11 +451,11 @@ def compute_linear_admittances(nbr: int,
     
     Identifying the already computed matrices, it becomes:
     
-    Pbus = Bbus x bus_angles + Btheta x branch_angles
+    Pbus = Bbus x bus_angles + Btau x branch_angles
     
     If we solve for bus angles:
     
-    bus_angles = Bbus^-1 x (Pbus - Btheta x branch_angles)
+    bus_angles = Bbus^-1 x (Pbus - Btau x branch_angles)
     """
 
-    return Bbus, Bf, Btheta
+    return Bbus, Bf, Btau

src/GridCalEngine/Simulations/PowerFlow/NumericalMethods/linearized_power_flow.py
@@ -28,19 +28,21 @@ linear_solver = get_linear_solver()
 sparse = get_sparse_type()
 
 
-def dcpf(Ybus, Bpqpv, Bref, Btheta, S0: CxVec, I0: CxVec, V0: CxVec, theta: Vec,
-         ref: IntVec, pvpq: IntVec, pq: IntVec, pv: IntVec) -> NumericPowerFlowResults:
+def dcpf(Ybus: sp.csc_matrix, Bpqpv: sp.csc_matrix, Bref: sp.csc_matrix, Btau: sp.csc_matrix,
+         S0: CxVec, I0: CxVec, Y0: CxVec, V0: CxVec, tau: Vec,
+         vd: IntVec, pvpq: IntVec, pq: IntVec, pv: IntVec) -> NumericPowerFlowResults:
     """
     Solves a linear-DC power flow.
     :param Ybus: Normal circuit admittance matrix
     :param Bpqpv: Susceptance matrix reduced
     :param Bref: Susceptane matrix sliced for the slack node
-    :param Btheta: Susceptance matrix of the Branches to nodes (used to include the phase shifters)
+    :param Btau: Susceptance matrix of the Branches to nodes (used to include the phase shifters)
     :param S0: Complex power Injections at all the nodes
     :param I0: Complex current Injections at all the nodes
+    :param Y0: Complex admittance Injections at all the nodes
     :param V0: Array of complex seed voltage (it contains the ref voltages)
-    :param theta: Array of branch angles
-    :param ref: array of the indices of the slack nodes
+    :param tau: Array of branch angles
+    :param vd: array of the indices of the slack nodes
     :param pvpq: array of the indices of the non-slack nodes
     :param pq: array of the indices of the pq nodes
     :param pv: array of the indices of the pv nodes
@@ -52,27 +54,30 @@ def dcpf(Ybus, Bpqpv, Bref, Btheta, S0: CxVec, I0: CxVec, V0: CxVec, theta: Vec,
     npv = len(pv)
     if (npq + npv) > 0:
         # Decompose the voltage in angle and magnitude
-        Va_ref = np.angle(V0[ref])  # we only need the angles at the slack nodes
+        Va_ref = np.angle(V0[vd])  # we only need the angles at the slack nodes
         Vm = np.abs(V0)
 
         # initialize result vector
         Va = np.empty(len(V0))
 
+        # compute the power injection
+        Sbus = cf.compute_zip_power(S0, I0, Y0, Vm)
+
         # compose the reduced power Injections
         # Since we have removed the slack nodes, we must account their influence as Injections Bref * Va_ref
         # We also need to account for the effect of the phase shifters
-        Pps = Btheta * theta
-        Pinj = S0[pvpq].real + (- Bref * Va_ref + I0[pvpq].real) * Vm[pvpq] - Pps[pvpq]
+        Pps = Btau @ tau
+        Pinj = Sbus[pvpq].real - (Bref @ Va_ref) * Vm[pvpq] - Pps[pvpq]
 
         # update angles for non-reference buses
         Va[pvpq] = linear_solver(Bpqpv, Pinj)
-        Va[ref] = Va_ref
+        Va[vd] = Va_ref
 
         # re assemble the voltage
         V = cf.polar_to_rect(Vm, Va)
 
         # compute the calculated power injection and the error of the voltage solution
-        Scalc = V * np.conj(Ybus * V - I0)
+        Scalc = cf.compute_power(Ybus, V)
 
         # compute the power mismatch between the specified power Sbus and the calculated power Scalc
         mismatch = cf.compute_fx(Scalc, S0, pvpq, pq)
@@ -82,7 +87,7 @@ def dcpf(Ybus, Bpqpv, Bref, Btheta, S0: CxVec, I0: CxVec, V0: CxVec, theta: Vec,
     else:
         norm_f = 0.0
         V = V0
-        Scalc = V * np.conj(Ybus * V - I0)
+        Scalc = cf.compute_power(Ybus, V)
 
     end = time.time()
     elapsed = end - start

src/tests/test_ptdf.py
@@ -1,6 +1,7 @@
 import os
 from GridCalEngine.api import *
 from GridCalEngine.Simulations.ContingencyAnalysis.contingency_plan import add_n1_contingencies
+from GridCalEngine.Simulations.PowerFlow.power_flow_worker import multi_island_pf_nc
 
 
 def test_ptdf():
@@ -46,6 +47,86 @@ def test_ptdf():
     return True
 
 
+def test_ptdf_ieee14_definition():
+    """
+    Compare the PSSE LODF and the GridCal LODF for the IEEE14
+    """
+    fname = os.path.join('data', 'grids', 'RAW', 'IEEE 14 bus.raw')
+    # fname = os.path.join('data', 'grids', 'RAW', 'IEEE 30 bus.raw')
+    main_circuit = FileOpen(fname).open()
+
+    # add all branch contingencies
+    add_n1_contingencies(branches=main_circuit.get_branches(),
+                         vmax=1e20, vmin=0,
+                         filter_branches_by_voltage=False,
+                         branch_types=[DeviceType.LineDevice, DeviceType.Transformer2WDevice])
+
+    # run the linear analysis
+    options = LinearAnalysisOptions(distribute_slack=False, correct_values=False)
+    simulation = LinearAnalysisDriver(grid=main_circuit, options=options)
+    simulation.run()
+
+    # compute the PTDF by the definition: PTDF(i, j) = (flow base(i) - modified flow(i)) / bus power increase(j)
+    nc = compile_numerical_circuit_at(main_circuit, t_idx=None)
+    options = PowerFlowOptions(solver_type=SolverType.DC)
+    base_res = multi_island_pf_nc(nc=nc, options=options)
+    S = nc.Sbus.copy()
+    ptdf = np.zeros((nc.nbr, nc.nbus))
+
+    for i in range(nc.nbus):
+        dS = np.zeros(nc.nbus)
+        dS[i] += 0.01  # 1 MW in p.u.
+        res = multi_island_pf_nc(nc=nc, options=options, Sbus_input=S + dS)
+        ptdf[:, i] = (res.Sf.real - base_res.Sf.real) / (dS[i] * nc.Sbase)
+
+    # diff = simulation.results.PTDF - ptdf
+    # print(diff)
+
+    assert (np.isclose(simulation.results.PTDF, ptdf).all())
+
+
+def test_lodf_ieee14_definition():
+    """
+    Compare the PSSE LODF and the GridCal LODF for the IEEE14
+    """
+    fname = os.path.join('data', 'grids', 'RAW', 'IEEE 14 bus.raw')
+    # fname = os.path.join('data', 'grids', 'RAW', 'IEEE 30 bus.raw')
+    main_circuit = FileOpen(fname).open()
+
+    # add all branch contingencies
+    add_n1_contingencies(branches=main_circuit.get_branches(),
+                         vmax=1e20, vmin=0,
+                         filter_branches_by_voltage=False,
+                         branch_types=[DeviceType.LineDevice, DeviceType.Transformer2WDevice])
+
+    # run the linear analysis
+    options = LinearAnalysisOptions(distribute_slack=False, correct_values=False)
+    simulation = LinearAnalysisDriver(grid=main_circuit, options=options)
+    simulation.run()
+
+    # compute the LODF by the definition: PTDF(i, j) = (flow base(i) - modified flow(i)) / flow base(i)
+    nc = compile_numerical_circuit_at(main_circuit, t_idx=None)
+    options = PowerFlowOptions(solver_type=SolverType.DC)
+    base_res = multi_island_pf_nc(nc=nc, options=options)
+
+    lodf = np.zeros((nc.nbr, nc.nbr))
+
+    for i in range(nc.nbr):
+        nc.branch_data.active[i] = 0  # fail branch
+        res = multi_island_pf_nc(nc=nc, options=options)
+        lodf[:, i] = (res.Sf.real - base_res.Sf.real) / base_res.Sf.real[i]
+        nc.branch_data.active[i] = 1  # revert back
+
+    # force zeros o the branch 10 because it is a feeder
+    lodf[:, 10] = 0
+    simulation.results.LODF[:, 10] = 0
+
+    # diff = simulation.results.LODF - lodf
+    # print(diff)
+
+    assert (np.isclose(simulation.results.LODF, lodf).all())
+
+
 def test_lodf_ieee14_psse():
     """
     Compare the PSSE LODF and the GridCal LODF for the IEEE14
@@ -78,8 +159,15 @@ def test_lodf_ieee14_psse():
             j_psse = psse_names_dict[name_j]
             lodf[i, j] = lodf_df.values[i_psse, j_psse]
 
+    # force zeros o the branch 10 because it is a feeder
+    lodf[:, 10] = 0
+    simulation.results.LODF[:, 10] = 0
+
     # print differences greater than 0.01
-    print(np.abs(lodf - simulation.results.LODF) > 0.01)
+    # diff = lodf - simulation.results.LODF
+    # print(diff)
+
+    assert(np.isclose(lodf, simulation.results.LODF, atol=1e-5).all())
 
 
 if __name__ == '__main__':