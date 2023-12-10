43bf5ec547fc07e751feacb9c0eab8f2ed638292
Carlos-Alegre <calegre@eroots.tech>
2023-12-10 20:22:59 +0100

    Quadr constrained test WIP

--- examples/ACOPF_MIPS_replica.py
+++ examples/ACOPF_MIPS_replica.py
@@ -26,23 +26,25 @@ def solver():
     gamma = 1
     error = 100
     #YK = INIT_STATE()
-    NV = 2
+    NV = 3
     NE = 0
-    NI = 0
+    NI = 2
 
-    X = [0, 0]
+    X = [0, 0, 0]
     PI = sparse.csc_matrix([0] * NE)
     LAMBDA = sparse.csc_matrix([0] * NI)
+    LAMBDA_MAT = sparse.dia_matrix(([0] * NI, 0), shape = (NI, NI))
     T = sparse.csc_matrix([0] * NI)
-
+    T_MAT = sparse.dia_matrix(([1] * NI, 0), shape = (NI, NI))
+    E = sparse.csc_matrix([1] * NV)
     while (error > gamma):
 
 
         #f, G, H, fx, Gx, Hx, fxx, Gxx, Hxx = feval(N, L, LINES, V_U, V_L, P_U, P_L, Q_U, Q_L, PD, QD, SMAX, DELTA_MAX, YK)
-        f, G, H, fx, Gx, Hx, fxx, Gxx, Hxx = brock_eval(X)
+        f, G, H, fx, Gx, Hx, fxx, Gxx, Hxx = NLP_test(X, LAMBDA, PI)
 
-        M = fxx + Gxx + Hxx  #+ Hx.transpose() @ 1/T @ LAMBDA @ Hx
-        N =  fx#.transpose() #+ Gx.transpose() @ PI + Hx.transpose() @ LAMBDA + Hx.transpose() @ 1/T @ (gamma * E - LAMBDA @ H)
+        M = fxx + Gxx + Hxx  + Hx @ sparse.linalg.inv(T_MAT) @ LAMBDA_MAT @ Hx.transpose()
+        N =  fx + Hx @ LAMBDA + Hx @ sparse.linalg.inv(T_MAT) @ (gamma * E - LAMBDA_MAT @ H) #+ Gx.transpose() @ PI
 
         J1 = sparse.hstack([M, Gx.transpose()])
         J2 = sparse.hstack([Gx, sparse.csc_matrix((NE,NE))])
@@ -54,29 +56,62 @@ def solver():
         dXP = sparse.linalg.spsolve(J, r)
 
         dX = dXP[0 : NV]
-        #dP = dXP[NV : NE + NV]
-        #dT = - H - T - Hx @ dX
-        #dL = - LAMBDA + 1/T @ (gamma * E - LAMBDA_MAT @ dP)
+        dP = dXP[NV : NE + NV]
+        dT = - H - T - Hx.transpose() @ dX
+        dL = - LAMBDA + sparse.linalg.inv(T_MAT) @ (gamma * E - LAMBDA_MAT @ dP)
 
         #alfap = step_calculation(T, dT)
         #alfad = step_calculation(L, dL)
+        TAU = 0.9995
+        alphap =min(min(TAU * T / abs(dT)), 1)
+        alphad = min(min(TAU * LAMBDA / abs(dL)), 1)
+
+
         alpha = 0.3
-        X += dX * alpha
-        error = max(abs(dX))
+        X += dX * alphap
+        T += dT * alphap
+        LAMBDA += dL * alphad
+        error = max(max(abs(dX)), max(abs(dL)), max(abs(dT)))
         newgamma = gamma * 0.1
         gamma = max(newgamma, 1e-8)
         print(X, error)
-    print('SOLUTION: ',X, brock_eval(X)[0])
+    print('SOLUTION: ',X, NLP_test(X, LAMBDA, PI)[0])
     return
+
+
+
+def NLP_test(x, LAMBDA, PI):
+
+    NV = 3
+    NE = 0
+    NI = 2
+
+    f = -x[0] * x[1] - x[1] * x[2]
+    fx = sparse.csc_matrix([[-x[1]],[-x[0] - x[2]],[-x[1]]])
+    fxx = sparse.csc_matrix([[0, -1, 0],[-1, 0, -1],[0, -1, 0]])
+
+    G = sparse.csc_matrix((0,0))
+    Gx = sparse.csc_matrix((0,0))
+    Gxx = sparse.csc_matrix((3,3))
+
+    H = sparse.csc_matrix([[x[0] ** 2 - x[1] ** 2 + x[2] ** 2 - 2], [x[0] ** 2 + x[1] ** 2 + x[2] ** 2 - 10]])
+    Hx = sparse.csc_matrix([[2 * x[0], 2 * x[0]],[-2 * x[1], 2* x[1]],[2 * x[2], 2 * x[2]]])
+    Hxx = LAMBDA.toarray()[0][0] * sparse.csc_matrix([[2, 0, 0], [0, -2, 0], [0, 0, 2]])
+    Hxx += LAMBDA.toarray()[0][1] * sparse.csc_matrix([[2, 0, 0], [0, 2, 0], [0, 0, 2]])
+
+
+
+    return f, G, H, fx, Gx, Hx, fxx, Gxx, Hxx
+
 
 
 
 
 def brock_eval(x):
 
-    NV = 2
+    NV = 3
     NE = 0
-    NI = 0
+    NI = 2
 
     f = 100 * (x[1] - x[0] ** 2) ** 2 + (1 - x[0]) ** 2
     fx = sparse.csc_matrix([[400 * (x[1] ** 3 - x[0] * x[1]) + 2 * x[0] -  2],[ 200 * (x[1] - x[0]**2 )]])
