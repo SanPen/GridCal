5c64f8273edfb49b30de0f66c90b332d7d4d7eb7
santi <santiago.penate.vera@gmail.com>
2024-01-12 10:08:38 +0000

    Improved PSSe compatibility with faulty raw files

--- src/GridCalEngine/IO/raw/devices/transformer.py
+++ src/GridCalEngine/IO/raw/devices/transformer.py
@@ -1199,9 +1199,13 @@ class RawTransformer(RawObject):
             xsh = x_ohm / z_base_sys
 
             # convert shunt impedance to shunt admittance
-            ysh = 1 / (rsh + 1j * xsh)
-            g = ysh.real
-            b = ysh.imag
+            if rsh != 0.0 and xsh != 0.0:
+                ysh = 1 / (rsh + 1j * xsh)
+                g = ysh.real
+                b = ysh.imag
+            else:
+                g = 1e-20
+                b = 1e-20
         else:
             raise Exception("Invalid value of CM")
 
