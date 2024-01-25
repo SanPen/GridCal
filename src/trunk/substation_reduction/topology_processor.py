1f626be88498c5428ff5700cb069f3585a85bcd3
rcarmona_ingelectus <rcarmona@ingelectus>
2024-01-25 10:23:22 +0100

    Scripts for topology processor

--- src/trunk/substation_reduction/topology_processor.py
+++ src/trunk/substation_reduction/topology_processor.py
@@ -1,5 +1,8 @@
+import os
+from GridCalEngine.api import *
 import GridCalEngine.Core.Devices as dev
 from GridCalEngine.Core.Devices.multi_circuit import MultiCircuit
+
 def createGrid():
     """
     Función para crear un Multicircuit a partir de la red del diagrama 1
@@ -17,8 +20,16 @@ def createGrid():
         grid.connectivity_nodes.append(t)
         # Indicar al CN cuál es su busbar
     # Añadir líneas
+    #for i in range(4):
+    #    l = dev.Line()
+    #    grid.lines
 
     # Añadir los switches
+    for i in range(7):
+        s = dev.Switch()
+
+    print('')
+    return grid
 
 
 def topology_proc(grid: MultiCircuit):
@@ -27,3 +38,6 @@ def topology_proc(grid: MultiCircuit):
 
     ## Crear matrices de conectividad a partir de MultiCircuit. Replicar
     grid.get_branches() # Devuelve una lista de todas las ramas en el orden de cálculo
+
+if __name__ == '__main__':
+    createGrid()
