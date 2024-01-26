94e92e0e92abed9cc4980197653a56bbfeff92fa
benceszirbik <bence.szirbik@griddigit.eu>
2024-01-26 11:45:04 +0100

    new cgmes 2.4.15 folder added. old version with _old

--- src/GridCalEngine/IO/cim/cgmes_2_4_15/devices/branches/line/__init__.py
+++
@@ -1,4 +0,0 @@
-
-
-# from GridCalEngine.IO.cim.cgmes_2_4_15.devices.branches.line.ac_line_segment import ACLineSegment
-# from GridCalEngine.IO.cim.cgmes_2_4_15.devices.branches.line.line import Line
---
+++ src/GridCalEngine/IO/cim/cgmes_2_4_15_old/__init__.py
@@ -0,0 +1,5 @@
+
+
+# import GridCalEngine.IO.cim.cgmes_2_4_15_old.cgmes_circuit
+# import GridCalEngine.IO.cim.cgmes_2_4_15_old.cgmes_to_roseta
+# import GridCalEngine.IO.cim.cgmes_2_4_15_old.devices as devices
