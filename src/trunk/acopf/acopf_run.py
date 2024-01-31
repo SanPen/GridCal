33c8d2a5335afb6ec4b8ed2c8f1aa0bcc8cc262d
Josep Fanals i Batllori <jfanals@eroots.tech>
2024-01-31 20:40:30 +0100

    Taps fixed, pegase89 works!

--- src/trunk/acopf/acopf_run.py
+++ src/trunk/acopf/acopf_run.py
@@ -212,7 +212,7 @@ def case_pegase89():
     file_path = os.path.join(new_directory, 'Grids_and_profiles', 'grids', 'case89pegase.m')
 
     grid = gce.FileOpen(file_path).open()
-    pf_options = gce.PowerFlowOptions(solver_type=gce.SolverType.NR, verbose=1, max_iter=500, trust=1.0)
+    pf_options = gce.PowerFlowOptions(solver_type=gce.SolverType.NR, verbose=1, max_iter=30, trust=1.0)
     run_nonlinear_opf(grid=grid, pf_options=pf_options, plot_error=True)
 
 
