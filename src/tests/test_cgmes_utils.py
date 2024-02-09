63672c4451da99ed0122a746b6c95bb60f0064d7
Jozsef Gorcs <jozsef.gorcs@navitasoft.com>
2024-02-09 14:38:10 +0100

    Add more unit tests for cgmes_utils.py

--- src/tests/test_cgmes_utils.py
+++ src/tests/test_cgmes_utils.py
@@ -1,9 +1,12 @@
 import pytest
 from GridCalEngine.IO.cim.cgmes.cgmes_utils import get_windings_number, get_windings, get_voltages, get_rate, \
     get_voltage_power_transformer_end, get_pu_values_power_transformer_end, get_voltage_ac_line_segment, \
-    get_pu_values_ac_line_segment, get_rate_ac_line_segment, get_voltage_terminal
+    get_pu_values_ac_line_segment, get_rate_ac_line_segment, get_voltage_terminal, get_topological_nodes_bus_bar, \
+    get_topological_node_bus_bar, get_topological_nodes_dipole
 from GridCalEngine.IO.cim.cgmes.cgmes_v2_4_15.devices.ac_line_segment import ACLineSegment
 from GridCalEngine.IO.cim.cgmes.cgmes_v2_4_15.devices.base_voltage import BaseVoltage
+from GridCalEngine.IO.cim.cgmes.cgmes_v2_4_15.devices.busbar_section import BusbarSection
+from GridCalEngine.IO.cim.cgmes.cgmes_v2_4_15.devices.identified_object import IdentifiedObject
 from GridCalEngine.IO.cim.cgmes.cgmes_v2_4_15.devices.power_transformer import PowerTransformer
 from GridCalEngine.IO.cim.cgmes.cgmes_v2_4_15.devices.power_transformer_end import PowerTransformerEnd
 from GridCalEngine.IO.cim.cgmes.cgmes_v2_4_15.devices.terminal import Terminal
@@ -227,6 +230,7 @@ def test_get_pu_values_ac_line_segment_BaseVoltage_is_filled_returns_correct_val
 def test_get_rate_ac_line_segment_returns_constant():
     assert get_rate_ac_line_segment() == 1e-20
 
+
 def test_get_voltage_terminal_topologicalnode_nomivalvoltage_set_retuns_value():
     t = Terminal()
     t.TopologicalNode = TopologicalNode()
@@ -238,4 +242,51 @@ def test_get_voltage_terminal_topologicalnode_nomivalvoltage_set_retuns_value():
 def test_get_voltage_terminal_no_topologicalnode_retuns_None():
     t = Terminal()
     t.TopologicalNode = None
-    assert get_voltage_terminal(t, None) == None
+    assert get_voltage_terminal(t, None) is None
+
+
+def test_get_topological_nodes_bus_bar_setup_terminals_return_topologicalnodelist():
+    bbs = BusbarSection()
+    bbs.references_to_me["Terminal"] = [Terminal()]
+    result = get_topological_nodes_bus_bar(bbs)
+    assert len(result) == 1  # TODO
+
+
+def test_get_topological_node_bus_bar_setup_terminals_return_first_topologicalnode():
+    bbs = BusbarSection()
+    bbs.references_to_me["Terminal"] = [Terminal()]
+    result = get_topological_node_bus_bar(bbs)
+    assert result is not None  # TODO
+
+
+def test_get_topological_nodes_dipole_with_valid_terminals():
+    t1 = Terminal()
+    t2 = Terminal()
+    t1.TopologicalNode = TopologicalNode()
+    t2.TopologicalNode = TopologicalNode()
+    i = IdentifiedObject("a", "b")
+    i.references_to_me["Terminal"] = [t1, t2]
+    node1, node2 = get_topological_nodes_dipole(i)
+    assert isinstance(node1, TopologicalNode)
+    assert isinstance(node2, TopologicalNode)
+
+
+def test_get_topological_nodes_dipole_with_valid_terminals2():
+    t1 = Terminal()
+    t2 = Terminal()
+    t1.TopologicalNode = TopologicalNode()
+    t2.TopologicalNode = TopologicalNode()
+    i = IdentifiedObject("a", "b")
+    i.references_to_me["aaa"] = [t1, t2]
+    node1, node2 = get_topological_nodes_dipole(i)
+    assert node1 is None
+    assert node2 is None
+
+def test_get_topological_nodes_dipole_with_only_one_terminal_returns_None():
+    t1 = Terminal()
+    t1.TopologicalNode = TopologicalNode()
+    i = IdentifiedObject("a", "b")
+    i.references_to_me["Terminal"] = [t1]
+    node1, node2 = get_topological_nodes_dipole(i)
+    assert node1 is None
+    assert node2 is None
