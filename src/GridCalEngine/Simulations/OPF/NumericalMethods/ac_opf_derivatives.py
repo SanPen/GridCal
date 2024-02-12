08b93253324551aec0ceaddfdad2e6fee8bb8684
Carlos-Alegre <calegre@eroots.tech>
2024-02-12 10:57:49 +0100

    Tap variable addition checkpoint. Tests ok and working as taps weren't included, but the structures are already in.

--- src/GridCalEngine/Simulations/OPF/NumericalMethods/ac_opf_derivatives.py
+++ src/GridCalEngine/Simulations/OPF/NumericalMethods/ac_opf_derivatives.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import pandas as pd
-from scipy import sparse
+from scipy import sparse as sp
 from scipy.sparse import csc_matrix as csc
 from scipy.sparse import csr_matrix as csr
 from dataclasses import dataclass
@@ -11,9 +11,11 @@ from GridCalEngine.Utils.Sparse.csc import diags
 import GridCalEngine.Utils.NumericalMethods.autodiff as ad
 from typing import Tuple, Union
 from GridCalEngine.basic_structures import Vec, CxVec, IntVec
+from GridCalEngine.Core.DataStructures.numerical_circuit import compile_numerical_circuit_at, NumericalCircuit
 
 
-def x2var(x: Vec, nVa: int, nVm: int, nPg: int, nQg: int) -> Tuple[Vec, Vec, Vec, Vec]:
+def x2var(x: Vec, nVa: int, nVm: int, nPg: int,
+          nQg: int, ntapm: int, ntapt: int) -> Tuple[Vec, Vec, Vec, Vec, Vec, Vec]:
     """
     Convert the x solution vector to its composing variables
     :param x: solution vector
@@ -39,11 +41,19 @@ def x2var(x: Vec, nVa: int, nVm: int, nPg: int, nQg: int) -> Tuple[Vec, Vec, Vec
     b += nQg
 
     Qg = x[a: b]
+    a = b
+    b += ntapm
 
-    return Va, Vm, Pg, Qg
+    tapm = x[a: b]
+    a = b
+    b += ntapt
+
+    tapt = x[a: b]
+
+    return Va, Vm, Pg, Qg, tapm, tapt
 
 
-def var2x(Va: Vec, Vm: Vec, Pg: Vec, Qg: Vec) -> Vec:
+def var2x(Va: Vec, Vm: Vec, Pg: Vec, Qg: Vec, tapm: Vec, tapt: Vec) -> Vec:
     """
     Compose the x vector from its componenets
     :param Va: Voltage angles
@@ -52,30 +62,23 @@ def var2x(Va: Vec, Vm: Vec, Pg: Vec, Qg: Vec) -> Vec:
     :param Qg: Generator reactive powers
     :return: [Vm, Va, Pg, Qg]
     """
-    return np.r_[Va, Vm, Pg, Qg]
+    return np.r_[Va, Vm, Pg, Qg, tapm, tapt]
 
 
-def compute_analytic_admittances(nc):
-    k_m = nc.k_m
-    k_tau = nc.k_tau
-    k_mtau = nc.k_mtau
+def compute_analytic_admittances(alltapm, alltapt, k_m, k_tau, k_mtau, Cf, Ct, R, X):
 
-    tapm = nc.branch_data.tap_module
-    tapt = nc.branch_data.tap_angle
-
-    Cf = nc.Cf
-    Ct = nc.Ct
-    ys = 1.0 / (nc.branch_data.R + 1.0j * nc.branch_data.X + 1e-20)
+    ys = 1.0 / (R + 1.0j * X + 1e-20)
 
     # First partial derivative with respect to tap module
-    mp = tapm[k_m]
-    tau = tapt[k_m]
+    mp = alltapm[k_m]
+    tau = alltapt[k_m]
     ylin = ys[k_m]
+    N = len(alltapm)
 
-    dYffdm = np.zeros(len(tapm), dtype=complex)
-    dYftdm = np.zeros(len(tapm), dtype=complex)
-    dYtfdm = np.zeros(len(tapm), dtype=complex)
-    dYttdm = np.zeros(len(tapm), dtype=complex)
+    dYffdm = np.zeros(N, dtype=complex)
+    dYftdm = np.zeros(N, dtype=complex)
+    dYtfdm = np.zeros(N, dtype=complex)
+    dYttdm = np.zeros(N, dtype=complex)
 
     dYffdm[k_m] = -2 * ylin / (mp * mp * mp)
     dYftdm[k_m] = ylin / (mp * mp * np.exp(-1.0j * tau))
@@ -87,14 +90,14 @@ def compute_analytic_admittances(nc):
     dYbusdm = Cf.T * dYfdm + Ct.T * dYtdm  # Cf_m.T and Ct_m.T included earlier
 
     # First partial derivative with respect to tap angle
-    mp = tapm[k_tau]
-    tau = tapt[k_tau]
+    mp = alltapm[k_tau]
+    tau = alltapt[k_tau]
     ylin = ys[k_tau]
 
-    dYffdt = np.zeros(len(tapm), dtype=complex)
-    dYftdt = np.zeros(len(tapm), dtype=complex)
-    dYtfdt = np.zeros(len(tapm), dtype=complex)
-    dYttdt = np.zeros(len(tapm), dtype=complex)
+    dYffdt = np.zeros(N, dtype=complex)
+    dYftdt = np.zeros(N, dtype=complex)
+    dYtfdt = np.zeros(N, dtype=complex)
+    dYttdt = np.zeros(N, dtype=complex)
 
     dYftdt[k_tau] = -1j * ylin / (mp * np.exp(-1.0j * tau))
     dYtfdt[k_tau] = 1j * ylin / (mp * np.exp(1.0j * tau))
@@ -137,27 +140,20 @@ def compute_finitediff_admittances(nc, tol=1e-6):
     return dYbusdm, dYfdm, dYtdm, dYbusdt, dYfdt, dYtdt
 
 
-def compute_analytic_admittances_2dev(nc):
-    k_m = np.r_[nc.k_m, nc.k_mtau]
-    k_tau = np.r_[nc.k_tau, nc.k_mtau]
-    k_mtau = nc.k_mtau
+def compute_analytic_admittances_2dev(alltapm, alltapt, k_m, k_tau, k_mtau, Cf, Ct, R, X):
 
-    tapm = nc.branch_data.tap_module
-    tapt = nc.branch_data.tap_angle
-
-    Cf = nc.Cf
-    Ct = nc.Ct
-    ys = 1.0 / (nc.branch_data.R + 1.0j * nc.branch_data.X + 1e-20)
+    ys = 1.0 / (R + 1.0j * X + 1e-20)
+    N = len(alltapm)
 
     # Second partial derivative with respect to tap module
-    mp = tapm[k_m]
-    tau = tapt[k_m]
+    mp = alltapm[k_m]
+    tau = alltapt[k_m]
     ylin = ys[k_m]
 
-    dYffdmdm = np.zeros(len(tapm), dtype=complex)
-    dYftdmdm = np.zeros(len(tapm), dtype=complex)
-    dYtfdmdm = np.zeros(len(tapm), dtype=complex)
-    dYttdmdm = np.zeros(len(tapm), dtype=complex)
+    dYffdmdm = np.zeros(N, dtype=complex)
+    dYftdmdm = np.zeros(N, dtype=complex)
+    dYtfdmdm = np.zeros(N, dtype=complex)
+    dYttdmdm = np.zeros(N, dtype=complex)
 
     dYffdmdm[k_m] = 6 * ylin / (mp * mp * mp * mp)
     dYftdmdm[k_m] = -2 * ylin / (mp * mp * mp * np.exp(-1.0j * tau))
@@ -169,14 +165,14 @@ def compute_analytic_admittances_2dev(nc):
     dYbusdmdm = (Cf.T * dYfdmdm + Ct.T * dYtdmdm)
 
     # Second partial derivative with respect to tap angle
-    mp = tapm[k_tau]
-    tau = tapt[k_tau]
+    mp = alltapm[k_tau]
+    tau = alltapt[k_tau]
     ylin = ys[k_tau]
 
-    dYffdtdt = np.zeros(len(tapm), dtype=complex)
-    dYftdtdt = np.zeros(len(tapm), dtype=complex)
-    dYtfdtdt = np.zeros(len(tapm), dtype=complex)
-    dYttdtdt = np.zeros(len(tapm), dtype=complex)
+    dYffdtdt = np.zeros(N, dtype=complex)
+    dYftdtdt = np.zeros(N, dtype=complex)
+    dYtfdtdt = np.zeros(N, dtype=complex)
+    dYttdtdt = np.zeros(N, dtype=complex)
 
     dYftdtdt[k_tau] = ylin / (mp * np.exp(-1.0j * tau))
     dYtfdtdt[k_tau] = ylin / (mp * np.exp(1.0j * tau))
@@ -187,14 +183,14 @@ def compute_analytic_admittances_2dev(nc):
     dYbusdtdt = Cf.T * dYfdtdt + Ct.T * dYtdtdt
 
     # Second partial derivative with respect to both tap module and angle
-    mp = tapm[k_mtau]
-    tau = tapt[k_mtau]
+    mp = alltapm[k_mtau]
+    tau = alltapt[k_mtau]
     ylin = ys[k_mtau]
 
-    dYffdmdt = np.zeros(len(tapm), dtype=complex)
-    dYftdmdt = np.zeros(len(tapm), dtype=complex)
-    dYtfdmdt = np.zeros(len(tapm), dtype=complex)
-    dYttdmdt = np.zeros(len(tapm), dtype=complex)
+    dYffdmdt = np.zeros(N, dtype=complex)
+    dYftdmdt = np.zeros(N, dtype=complex)
+    dYtfdmdt = np.zeros(N, dtype=complex)
+    dYttdmdt = np.zeros(N, dtype=complex)
 
     dYftdmdt[k_mtau] = 1j * ylin / (mp * mp * np.exp(-1.0j * tau))
     dYtfdmdt[k_mtau] = -1j * ylin / (mp * mp * np.exp(1.0j * tau))
@@ -253,7 +249,7 @@ def compute_finitediff_admittances_2dev(nc, tol=1e-6):
             dYbusdtdm, dYfdtdm, dYtdtdm, dYbusdtdt, dYfdtdt, dYtdtdt)
 
 
-def eval_f(x: Vec, Cg, c0: Vec, c1: Vec, c2: Vec, ig: Vec, Sbase: float) -> Vec:
+def eval_f(x: Vec, Cg, k_m: Vec, k_tau: Vec, c0: Vec, c1: Vec, c2: Vec, ig: Vec, Sbase: float) -> Vec:
     """
 
     :param x:
@@ -267,15 +263,17 @@ def eval_f(x: Vec, Cg, c0: Vec, c1: Vec, c2: Vec, ig: Vec, Sbase: float) -> Vec:
     """
     N, _ = Cg.shape  # Check
     Ng = len(ig)
+    ntapm = len(k_m)
+    ntapt = len(k_tau)
 
-    _, _, Pg, Qg = x2var(x, nVa=N, nVm=N, nPg=Ng, nQg=Ng)
+    _, _, Pg, Qg, _, _ = x2var(x, nVa=N, nVm=N, nPg=Ng, nQg=Ng, ntapm=ntapm, ntapt=ntapt)
 
     fval = np.sum((c0 + c1 * Pg * Sbase + c2 * np.power(Pg * Sbase, 2))) * 1e-4
 
     return fval
 
 
-def eval_g(x, Ybus, Yf, Cg, Sd, ig, nig, pv, Vm_max, Sg_undis, slack) -> Vec:
+def eval_g(x, Ybus, Yf, Cg, Sd, ig, nig, pv, k_m, k_tau, Vm_max, Sg_undis, slack) -> Vec:
     """
 
     :param x:
@@ -291,12 +289,13 @@ def eval_g(x, Ybus, Yf, Cg, Sd, ig, nig, pv, Vm_max, Sg_undis, slack) -> Vec:
     """
     M, N = Yf.shape
     Ng = len(ig)
+    ntapm = len(k_m)
+    ntapt = len(k_tau)
 
-    va, vm, Pg_dis, Qg_dis = x2var(x, nVa=N, nVm=N, nPg=Ng, nQg=Ng)
+    va, vm, Pg_dis, Qg_dis, _, _ = x2var(x, nVa=N, nVm=N, nPg=Ng, nQg=Ng, ntapm=ntapm, ntapt=ntapt)
 
     V = vm * np.exp(1j * va)
     S = V * np.conj(Ybus @ V)
-
     S_dispatch = Cg[:, ig] @ (Pg_dis + 1j * Qg_dis)
     S_undispatch = Cg[:, nig] @ Sg_undis
     dS = S + Sd - S_dispatch - S_undispatch
@@ -331,8 +330,10 @@ def eval_h(x, Yf, Yt, from_idx, to_idx, pq, no_slack, k_m, k_tau, k_mtau, Va_max
     """
     M, N = Yf.shape
     Ng = len(ig)
+    ntapm = len(k_m)
+    ntapt = len(k_tau)
 
-    va, vm, Pg, Qg = x2var(x, nVa=N, nVm=N, nPg=Ng, nQg=Ng)
+    va, vm, Pg, Qg, tapm, tapt = x2var(x, nVa=N, nVm=N, nPg=Ng, nQg=Ng, ntapm=ntapm, ntapt=ntapt)
 
     V = vm * np.exp(1j * va)
     Sf = V[from_idx[il]] * np.conj(Yf[il, :] @ V)
@@ -358,7 +359,11 @@ def eval_h(x, Yf, Yt, from_idx, to_idx, pq, no_slack, k_m, k_tau, k_mtau, Va_max
                  Qg - Qg_max[ig],  # generator Q upper limits
                  Vm_min[pq] - vm[pq],  # voltage module lower limit
                  Pg_min[ig] - Pg,  # generator P lower limits
-                 Qg_min[ig] - Qg  # generation Q lower limits
+                 Qg_min[ig] - Qg,  # generation Q lower limits
+                 tapm - tapm_max,
+                 tapm_min - tapm,
+                 tapt - tapt_max,
+                 tapt_min - tapt
     ]
 
     # Sftot = V[from_idx[il]] * np.conj(Yf[il, :] @ V)
@@ -368,8 +373,9 @@ def eval_h(x, Yf, Yt, from_idx, to_idx, pq, no_slack, k_m, k_tau, k_mtau, Va_max
     return hval, Sf, St
 
 
-def jacobians_and_hessians(x, c1, c2, Cg, Cf, Ct, Yf, Yt, Ybus, Sbase, il, ig, nig, slack, no_slack, pq, pv, k_m,
-                           k_tau, k_mtau, mu, lmbda, from_idx, to_idx, compute_jac: bool, compute_hess: bool):
+def jacobians_and_hessians(x, c1, c2, Cg, Cf, Ct, Yf, Yt, Ybus, Sbase, il, ig, nig, slack, no_slack, pq, pv, alltapm,
+                           alltapt, k_m, k_tau, k_mtau, mu, lmbda, from_idx, to_idx, compute_jac: bool,
+                           compute_hess: bool):
     """
 
     :param x:
@@ -395,14 +401,18 @@ def jacobians_and_hessians(x, c1, c2, Cg, Cf, Ct, Yf, Yt, Ybus, Sbase, il, ig, n
     M = len(il)
     Ng = len(ig)
     NV = len(x)
+    ntapm = len(k_m)
+    ntapt = len(k_tau)
 
-    va, vm, Pg, Qg = x2var(x, nVa=N, nVm=N, nPg=Ng, nQg=Ng)
+    va, vm, Pg, Qg, tapm, tapt = x2var(x, nVa=N, nVm=N, nPg=Ng, nQg=Ng, ntapm=ntapm, ntapt=ntapt)
     V = vm * np.exp(1j * va)
     Vmat = diags(V)
     vm_inv = diags(1 / vm)
     E = Vmat @ vm_inv
     Ibus = Ybus @ V
     IbusCJmat = diags(np.conj(Ibus))
+    alltapm[k_m] = tapm
+    alltapt[k_tau] = tapt
 
     if compute_jac:
         fx = np.zeros(NV)
@@ -424,8 +434,19 @@ def jacobians_and_hessians(x, c1, c2, Cg, Cf, Ct, Yf, Yt, Ybus, Sbase, il, ig, n
         for i, ss in enumerate(pv):
             Gvm[i, N + ss] = 1.
 
-        GS = sparse.hstack([GSva, GSvm, GSpg, GSqg])
-        Gx = sparse.vstack([GS.real, GS.imag, GTH, Gvm]).T.tocsc()
+        if ntapm + ntapt != 0:  # Check if there are tap variables that can affect the admittances
+
+            (dYbusdm, dYfdm, dYtdm,
+             dYbusdt, dYfdt, dYtdt) = compute_analytic_admittances(alltapm, alltapt, k_m, k_tau, k_mtau, Cf, Ct, R, X)
+
+
+
+
+
+
+
+        GS = sp.hstack([GSva, GSvm, GSpg, GSqg])
+        Gx = sp.vstack([GS.real, GS.imag, GTH, Gvm]).T.tocsc()
 
         #############
         # Old flow derivatives
@@ -440,8 +461,8 @@ def jacobians_and_hessians(x, c1, c2, Cg, Cf, Ct, Yf, Yt, Ybus, Sbase, il, ig, n
         Sfva = (1j * (IfCJmat @ Cf[il, :] @ Vmat - diags(Cf[il, :] @ V) @ np.conj(Yf[il, :]) @ np.conj(Vmat)))
         Stva = (1j * (ItCJmat @ Ct[il, :] @ Vmat - diags(Ct[il, :] @ V) @ np.conj(Yt[il, :]) @ np.conj(Vmat)))
 
-        SfX = sparse.hstack([Sfva, Sfvm, lil_matrix((M, 2 * Ng))])
-        StX = sparse.hstack([Stva, Stvm, lil_matrix((M, 2 * Ng))])
+        SfX = sp.hstack([Sfva, Sfvm, lil_matrix((M, 2 * Ng))])
+        StX = sp.hstack([Stva, Stvm, lil_matrix((M, 2 * Ng))])
 
         HSf = 2 * (Sfmat.real @ SfX.real + Sfmat.imag @ SfX.imag)
         HSt = 2 * (Stmat.real @ StX.real + Stmat.imag @ StX.imag)
@@ -499,8 +520,8 @@ def jacobians_and_hessians(x, c1, c2, Cg, Cf, Ct, Yf, Yt, Ybus, Sbase, il, ig, n
         # dAf_dVm = dAf_dPf * dSf_dVm.real + dAf_dQf * dSf_dVm.imag
         # dAt_dVm = dAt_dPt * dSt_dVm.real + dAt_dQt * dSt_dVm.imag
         #
-        # HSf = sparse.hstack([dAf_dVa, dAf_dVm, lil_matrix((nl, 2 * Ng))])
-        # HSt = sparse.hstack([dAt_dVa, dAt_dVm, lil_matrix((nl, 2 * Ng))])
+        # HSf = sp.hstack([dAf_dVa, dAf_dVm, lil_matrix((nl, 2 * Ng))])
+        # HSt = sp.hstack([dAt_dVa, dAt_dVm, lil_matrix((nl, 2 * Ng))])
 
         #############
 
@@ -519,20 +540,20 @@ def jacobians_and_hessians(x, c1, c2, Cg, Cf, Ct, Yf, Yt, Ybus, Sbase, il, ig, n
         Hqu[0: Ng] = 1
         Hql[0: Ng] = -1
 
-        Hvu = sparse.hstack([lil_matrix((len(pq), N)), Hvmu_, lil_matrix((len(pq), 2 * Ng))])
-        Hvl = sparse.hstack([lil_matrix((len(pq), N)), Hvml_, lil_matrix((len(pq), 2 * Ng))])
+        Hvu = sp.hstack([lil_matrix((len(pq), N)), Hvmu_, lil_matrix((len(pq), 2 * Ng))])
+        Hvl = sp.hstack([lil_matrix((len(pq), N)), Hvml_, lil_matrix((len(pq), 2 * Ng))])
 
-        Hvau = sparse.hstack([Hvau_, lil_matrix((N - len(slack), N + 2 * Ng))])
-        Hval = sparse.hstack([Hval_, lil_matrix((N - len(slack), N + 2 * Ng))])
+        Hvau = sp.hstack([Hvau_, lil_matrix((N - len(slack), N + 2 * Ng))])
+        Hval = sp.hstack([Hval_, lil_matrix((N - len(slack), N + 2 * Ng))])
 
-        Hpu = sparse.hstack([lil_matrix((Ng, 2 * N)), diags(Hpu), lil_matrix((Ng, Ng))])
-        Hpl = sparse.hstack([lil_matrix((Ng, 2 * N)), diags(Hpl), lil_matrix((Ng, Ng))])
-        Hqu = sparse.hstack([lil_matrix((Ng, 2 * N + Ng)), diags(Hqu)])
-        Hql = sparse.hstack([lil_matrix((Ng, 2 * N + Ng)), diags(Hql)])
+        Hpu = sp.hstack([lil_matrix((Ng, 2 * N)), diags(Hpu), lil_matrix((Ng, Ng))])
+        Hpl = sp.hstack([lil_matrix((Ng, 2 * N)), diags(Hpl), lil_matrix((Ng, Ng))])
+        Hqu = sp.hstack([lil_matrix((Ng, 2 * N + Ng)), diags(Hqu)])
+        Hql = sp.hstack([lil_matrix((Ng, 2 * N + Ng)), diags(Hql)])
 
-        # Hx = sparse.vstack([HSf, HSt, Hvu, Hvl, Hpu, Hpl, Hqu, Hql]).T.tocsc()
-        Hx = sparse.vstack([HSf, HSt, Hvu, Hpu, Hqu, Hvl, Hpl, Hql]).T.tocsc()
-        # Hx = sparse.vstack([HSf, HSt, Hvu, Hvl, Hvau, Hval, Hpu, Hpl, Hqu, Hql]).T.tocsc()
+        # Hx = sp.vstack([HSf, HSt, Hvu, Hvl, Hpu, Hpl, Hqu, Hql]).T.tocsc()
+        Hx = sp.vstack([HSf, HSt, Hvu, Hpu, Hqu, Hvl, Hpl, Hql]).T.tocsc()
+        # Hx = sp.vstack([HSf, HSt, Hvu, Hvl, Hvau, Hval, Hpu, Hpl, Hqu, Hql]).T.tocsc()
     else:
         fx = None
         Gx = None
@@ -580,13 +601,22 @@ def jacobians_and_hessians(x, c1, c2, Cg, Cf, Ct, Yf, Yt, Ybus, Sbase, il, ig, n
         Gav_q = Gva_q.T
         Gvv_q = vm_inv @ (C_q + C_q.T) @ vm_inv
 
-        # Add all
-        # G1 = sparse.hstack([Gvv_p.real + Gvv_q.imag, Gva_p.real + Gva_q.imag, lil_matrix((N, 2 * Ng))])
-        # G2 = sparse.hstack([Gav_p.real + Gav_q.imag, Gaa_p.real + Gaa_q.imag, lil_matrix((N, 2 * Ng))])
+        if ntapm + ntapt != 0:
 
-        G1 = sparse.hstack([Gaa_p.real + Gaa_q.imag, Gav_p.real + Gav_q.imag, lil_matrix((N, 2 * Ng))])
-        G2 = sparse.hstack([Gva_p.real + Gva_q.imag, Gvv_p.real + Gvv_q.imag, lil_matrix((N, 2 * Ng))])
-        Gxx = sparse.vstack([G1, G2, lil_matrix((2 * Ng, NV))]).tocsc()
+            (dYbusdmdm, dYfdmdm, dYtdmdm,
+             dYbusdmdt, dYfdmdt, dYtdmdt,
+             dYbusdtdm, dYfdtdm, dYtdtdm,
+             dYbusdtdt, dYfdtdt, dYtdtdt) = compute_analytic_admittances_2dev(alltapm, alltapt, k_m, k_tau,
+                                                                              k_mtau, Cf, Ct, R, X)
+
+
+        # Add all
+        # G1 = sp.hstack([Gvv_p.real + Gvv_q.imag, Gva_p.real + Gva_q.imag, lil_matrix((N, 2 * Ng))])
+        # G2 = sp.hstack([Gav_p.real + Gav_q.imag, Gaa_p.real + Gaa_q.imag, lil_matrix((N, 2 * Ng))])
+
+        G1 = sp.hstack([Gaa_p.real + Gaa_q.imag, Gav_p.real + Gav_q.imag, lil_matrix((N, 2 * Ng))])
+        G2 = sp.hstack([Gva_p.real + Gva_q.imag, Gvv_p.real + Gvv_q.imag, lil_matrix((N, 2 * Ng))])
+        Gxx = sp.vstack([G1, G2, lil_matrix((2 * Ng, NV))]).tocsc()
 
         #########
 
@@ -624,12 +654,12 @@ def jacobians_and_hessians(x, c1, c2, Cg, Cf, Ct, Yf, Yt, Ybus, Sbase, il, ig, n
         Htvavm = 2 * (Stvavm + Stva.T @ mu_mat @ np.conj(Stvm)).real
         Htvmvm = 2 * (Stvmvm + Stvm.T @ mu_mat @ np.conj(Stvm)).real
 
-        # H1 = sparse.hstack([Hfvmvm + Htvmvm, Hfvavm + Htvmva, lil_matrix((N, 2 * Ng))])
-        # H2 = sparse.hstack([Hfvavm + Htvavm, Hfvava + Htvava, lil_matrix((N, 2 * Ng))])
+        # H1 = sp.hstack([Hfvmvm + Htvmvm, Hfvavm + Htvmva, lil_matrix((N, 2 * Ng))])
+        # H2 = sp.hstack([Hfvavm + Htvavm, Hfvava + Htvava, lil_matrix((N, 2 * Ng))])
 
-        H1 = sparse.hstack([Hfvava + Htvava, Hfvavm + Htvavm, lil_matrix((N, 2 * Ng))])
-        H2 = sparse.hstack([Hfvmva + Htvmva, Hfvmvm + Htvmvm, lil_matrix((N, 2 * Ng))])
-        Hxx = sparse.vstack([H1, H2, lil_matrix((2 * Ng, NV))]).tocsc()
+        H1 = sp.hstack([Hfvava + Htvava, Hfvavm + Htvavm, lil_matrix((N, 2 * Ng))])
+        H2 = sp.hstack([Hfvmva + Htvmva, Hfvmvm + Htvmvm, lil_matrix((N, 2 * Ng))])
+        Hxx = sp.vstack([H1, H2, lil_matrix((2 * Ng, NV))]).tocsc()
     else:
         fxx = None
         Gxx = None
