04791a7751e751eea2dcb525a3bdb896b1d72ff9
jag0nzalez <jagonzalez@ingelectus.com>
2024-03-01 08:47:19 +0100

    compensated ptdf goodness-of-fit test

--- src/tests/test_ptdf.py
+++ src/tests/test_ptdf.py
@@ -483,8 +483,8 @@ def test_ptdf_generation_contingencies():
         os.path.join('data', 'grids', 'IEEE14-gen80.gridcal'),
         os.path.join('data', 'grids', 'IEEE30-gen80.gridcal'),
         os.path.join('data', 'grids', 'IEEE30-gen120.gridcal'),
-        os.path.join('data', 'grids', 'IEEE118-gen80-one.gridcal'),
-        os.path.join('data', 'grids', 'IEEE118-gen120-one.gridcal'),
+        os.path.join('data', 'grids', 'IEEE118-gen80.gridcal'),
+        os.path.join('data', 'grids', 'IEEE118-gen120.gridcal'),
 
     ]:
         main_circuit = FileOpen(fname).open()
@@ -561,7 +561,7 @@ def test_lodf_single_contingencies():
 
 def test_generation_contingencies_powerflow():
     """
-    Compare power flow results when using ContingencyAnalysisDriver wight generator contingencies
+    Compare power flow results when using ContingencyAnalysisDriver with generator contingencies
 
     The test consists in performing the contingencies with a Power flow driver (with linear power flow)
     later perform a power flow again applying the contingencies manually without using ContingencyAnalysisDriver
@@ -579,7 +579,7 @@ def test_generation_contingencies_powerflow():
         },
         {
             'orig': os.path.join('data', 'grids', 'RAW', 'IEEE 118 Bus v2.raw'),
-            'conti': os.path.join('data', 'grids', 'IEEE118-gen120-one.gridcal'),
+            'conti': os.path.join('data', 'grids', 'IEEE118-gen120.gridcal'),
             'cont_index_change': {44: 1.2}
         }
     ]:
@@ -653,6 +653,66 @@ def test_compensated_ptdf():
         linear_analysis_raw.run()
 
         ok = np.allclose(linear_multi_contingency.multi_contingencies[0].compensated_ptdf_factors.todense(), linear_analysis_raw.results.PTDF, atol=1e-1)
+        assert ok
+
+
+def test_ptdf_contingencies_powerflow():
+    """
+    Compare power flow results when using ContingencyAnalysisDriver with both generator and branch contingencies
+
+    The test consists in performing the contingencies with a Power flow driver (with linear power flow)
+    later perform a power flow again applying the contingencies manually without using ContingencyAnalysisDriver
+    """
+    for case in [
+        {
+            'orig': os.path.join('data', 'grids', 'RAW', 'IEEE 14 bus.raw'),
+            'conti': os.path.join('data', 'grids', 'IEEE14-gen120-1_2.gridcal'),
+            'cont_index_change': {1: 1.2}  # Generator contingency dict: {index: %change}
+        },
+        {
+            'orig': os.path.join('data', 'grids', 'RAW', 'IEEE 30 bus.raw'),
+            'conti': os.path.join('data', 'grids', 'IEEE30-gen80-1_2.gridcal'),
+            'cont_index_change': {1: 0.8}
+        },
+        {
+            'orig': os.path.join('data', 'grids', 'RAW', 'IEEE 118 Bus v2.raw'),
+            'conti': os.path.join('data', 'grids', 'IEEE118-gen120-12_16.gridcal'),
+            'cont_index_change': {44: 1.2}
+        }
+    ]:
+        main_circuit = FileOpen(case['conti']).open()
+
+        # DC power flow method with ContingencyAnalysisDriver
+        pf_options = PowerFlowOptions(SolverType.DC,
+                                      verbose=False,
+                                      initialize_with_existing_solution=False,
+                                      dispatch_storage=True,
+                                      control_q=ReactivePowerControlMode.NoControl,
+                                      control_p=False)
+        options1 = ContingencyAnalysisOptions(pf_options=pf_options, engine=ContingencyMethod.PowerFlow)
+        cont_analysis_driver1 = ContingencyAnalysisDriver(grid=main_circuit, options=options1,
+                                                          linear_multiple_contingencies=None)
+        cont_analysis_driver1.run()
+
+        line_cnt = ''
+        for cnt in main_circuit.contingencies:
+            if cnt.value == 0.0:  # Generator contingencies have values in this test have values <0 or >0
+                line_cnt = cnt.code
+
+        # DC power flow
+        main_circuit_raw = FileOpen(case['orig']).open()
+
+        for index, change in case['cont_index_change'].items():
+            main_circuit_raw.generators[index].P *= change
+
+        for l in main_circuit.lines:
+            if l.code == line_cnt:
+                l.active = False
+
+        power_flow = PowerFlowDriver(main_circuit, pf_options)
+        power_flow.run()
+
+        ok = np.allclose(cont_analysis_driver1.results.Sf, power_flow.results.Sf)
         assert ok
 
 
