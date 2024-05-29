74181a791e91f44ded9a439707634f7b442ad6f5
Mate Zsebehazi <mate.zsebehazi@griddigit.eu>
2024-05-29 16:42:43 +0200

    Cgmes import/export test power flow added

--- src/trunk/cgmes_import_check/cgmes_imp_exp_test.py
+++ src/trunk/cgmes_import_check/cgmes_imp_exp_test.py
@@ -5,7 +5,10 @@ import GridCalEngine.api as gc
 from GridCalEngine.IO import FileSave
 from GridCalEngine.IO.cim.cgmes.cgmes_enums import cgmesProfile
 from GridCalEngine.IO.file_handler import FileSavingOptions
-from GridCalEngine.enumerations import CGMESVersions
+from GridCalEngine.Simulations import PowerFlowOptions
+from GridCalEngine.Simulations.results_template import DriverToSave
+from GridCalEngine.enumerations import CGMESVersions, SolverType, SimulationTypes
+from GridCalEngine.basic_structures import Logger
 
 
 def compare_inputs(circuit_1, circuit_2, tol=1e-6):
@@ -159,12 +162,23 @@ def create_file_save_options(boundary_zip_path: str) -> FileSavingOptions:
 
 
 def run_import_export_test(import_path: str | list[str], export_name: str, boundary_zip_path: str):
+    logger = Logger()
     # CGMES model import to MultiCircuit
     circuit = gc.open_file(import_path)
+    # run power flow
+    pf_options = PowerFlowOptions()
+    pf_results = gc.power_flow(circuit, pf_options)
+
+    pf_session_data = DriverToSave(name="powerflow results",
+                                   tpe=SimulationTypes.PowerFlow_run,
+                                   results=pf_results,
+                                   logger=logger)
     # Export
     # export_dir = os.path.join(os.path.curdir, "/export_result")
     # export_name = os.path.join(export_dir, export_name)
     options = create_file_save_options(boundary_zip_path)
+    options.sessions_data.append(pf_session_data)
+
     cgmes_export = FileSave(circuit=circuit,
                             file_name=export_name,
                             options=options)
@@ -177,7 +191,6 @@ export_name = r"C:\WorkProjects\PycharmProjects\GridCal\src\trunk\cgmes_import_c
 run_import_export_test(cgmes_path, export_name, boundary_path)
 # nc_o = gc.compile_numerical_circuit_at(circuit_o)
 
-# run power flow
 
 # export to CGMES
 # crate FileSave
