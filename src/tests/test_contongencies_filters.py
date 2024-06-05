587e1e8309e352c1f88d1fa73837cf7bdf5c1758
santi <santiago.penate.vera@gmail.com>
2024-06-05 11:22:07 +0100

    Finalized the contingency filtering test

--- src/tests/test_contongencies_filters.py
+++ src/tests/test_contongencies_filters.py
@@ -6,25 +6,27 @@ import numpy as np
 import os
 
 
-def test_contingency_filtes():
+def test_contingency_filtes() -> None:
+    """
+    Tests that the contingency filtering works
+    :return: 
+    """
     # First stage of tests
     print('Loading grical circuit... ', sep=' ')
 
     fname = os.path.join('data', 'grids', 'Test_filter_conting_v1.gridcal')
     grid = FileOpen(fname).open()
 
-
     # test 1
     print("Running contingency analysis...")
     con_options = ContingencyAnalysisOptions()
-
     con_options.use_srap = False
     con_options.contingency_method = ContingencyMethod.PTDF
 
+    cg_idx_dict = {a: i for i, a in enumerate(grid.get_contingency_groups())}
+
+    # ------------------------------------------------------------------------------------------------------------------
     # NO filter
-
-    #con_options.contingency_groups= ["05463aa28ce148a580b9f73e0b9005dc::Contingency 0","0f3185956e2446b9843100b14fafbf8e::Contingency 1","51e3cfa5edb94488a17e47cc1e43167d::Contingency 2","4a020a6c6f9d44a193fc80097e67093b::Contingency 3", "08912a5f7f834e2db74a5fde460e84f0::Contingency 4", "53a1d22d1b724a8db4f8ba42de6922f9::Contingency 5", "9d6cdd8ec8b2450e9e9f49fdff3adfb6::Contingency 6", "ea776a35fbd94d5bb9e0850a7c872247::Contingency 7"]
-
     con_drv = ContingencyAnalysisDriver(grid=grid,
                                         options=con_options,
                                         engine=EngineType.GridCal)
@@ -32,26 +34,14 @@ def test_contingency_filtes():
     con_drv.run()
     all_conting_loading = con_drv.results.loading
 
-    #Filter area 1
+    # ------------------------------------------------------------------------------------------------------------------
+    # Filter area 1
 
-    area1=[0,1,2,3,7] # contingencies that should be considered for area 1
-
-    #Falta condición de opciones de filtro de contingencias
-
-    con_drv = ContingencyAnalysisDriver(grid=grid,
-                                        options=con_options,
-                                        engine=EngineType.GridCal)
-
-    con_drv.run()
-    conting_loading = con_drv.results.loading
-
-    test1_result = np.all(all_conting_loading[area1,:] == conting_loading)
-
-
-    #Filter area 2
-    area2 = [0, 2, 3, 4, 5, 6, 7]
-
-    #Falta condición de opciones de filtro de contingencias
+    # contingencies that should be considered for area 1
+    area1 = grid.get_areas()[0]
+    con_options.contingency_groups = grid.get_contingency_groups_in(grouping_elements=[area1])
+    area1_con_groups_idx = np.array([cg_idx_dict[cg] for cg in con_options.contingency_groups])
+    assert np.all(area1_con_groups_idx == np.array([0, 1, 2, 3, 7]))
 
     con_drv = ContingencyAnalysisDriver(grid=grid,
                                         options=con_options,
@@ -60,12 +50,24 @@ def test_contingency_filtes():
     con_drv.run()
     conting_loading = con_drv.results.loading
 
-    test2_result = np.all(all_conting_loading[area2,:] == conting_loading)
+    test1_result = np.all(all_conting_loading[area1_con_groups_idx, :] == conting_loading)
+    assert test1_result  # The filter should be ok for area 1
 
+    # ------------------------------------------------------------------------------------------------------------------
+    # Filter area 2
+    area2 = grid.get_areas()[1]
+    con_options.contingency_groups = grid.get_contingency_groups_in(grouping_elements=[area2])
+    area2_con_groups_idx = np.array([cg_idx_dict[cg] for cg in con_options.contingency_groups])
+    assert np.all(area2_con_groups_idx == np.array([0, 2, 3, 4, 5, 6, 7]))
 
-    assert test1_result # The filter should be ok for area 1
+    # Falta condición de opciones de filtro de contingencias
 
+    con_drv = ContingencyAnalysisDriver(grid=grid,
+                                        options=con_options,
+                                        engine=EngineType.GridCal)
+
+    con_drv.run()
+    conting_loading = con_drv.results.loading
+
+    test2_result = np.all(all_conting_loading[area2_con_groups_idx, :] == conting_loading)
     assert test2_result  # The filter should be ok for area 1
-
-
-
