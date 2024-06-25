9e9ef345897dd223ad873277dbd3b88369355cae
Carlos-Alegre <calegre@eroots.tech>
2024-06-25 11:36:55 +0200

    Added shortcut funciton for ACOPF in GridCalEngine.api

--- src/GridCalEngine/api.py
+++ src/GridCalEngine/api.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+from GridCalEngine.Simulations.OPF.NumericalMethods.ac_opf import run_nonlinear_opf, NonlinearOPFResults
 from GridCalEngine.basic_structures import *
 from GridCalEngine.Simulations import *
 from GridCalEngine.IO import *
@@ -82,3 +83,16 @@ def power_flow_ts(grid: MultiCircuit,
     driver.run()
 
     return driver.results
+
+def acopf(grid: MultiCircuit,
+          pf_options: PowerFlowOptions = PowerFlowOptions(),
+          opf_options: OptimalPowerFlowOptions = OptimalPowerFlowOptions()) -> NonlinearOPFResults:
+
+    acopf_res = run_nonlinear_opf(grid=grid,
+                                  pf_options=pf_options,
+                                  opf_options=opf_options,
+                                  plot_error=False,
+                                  pf_init=True)
+
+    return acopf_res
+
