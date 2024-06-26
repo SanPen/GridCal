1d9fabe041512523fa78d3a8bd9b16aa529ba131
JosepFanals <jfanals13@gmail.com>
2024-06-26 12:18:26 +0200

    Revert "First refactoring, still much to be done"
    This reverts commit e2f5e1755afd16b7bef998c1084c796f7148cc47.

--- src/GridCalEngine/Simulations/InvestmentsEvaluation/NumericalMethods/NSGA_3.py
+++ src/GridCalEngine/Simulations/InvestmentsEvaluation/NumericalMethods/NSGA_3.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import numpy as np
-# PYMOO:
+#PYMOO:
 from pymoo.core.problem import ElementwiseProblem
 from pymoo.util.ref_dirs import get_reference_directions
 from pymoo.optimize import minimize
@@ -25,12 +25,19 @@ from pymoo.operators.repair.rounding import RoundingRepair
 # from pymoo.operators.mutation.bitflip import BitflipMutation
 from pymoo.core.sampling import Sampling
 from pymoo.core.mutation import Mutation as Mutation_pymoo
+#PLATYPUS:
+from platypus.algorithms import NSGAII, NSGAIII
+#from platypus.core import Mutation as MutationPtp
+from platypus.problems import Problem
+from platypus.types import Real, Integer
+import matplotlib.pyplot as plt
 from platypus import *
 from platypus.indicators import Hypervolume
 
 
-class UniformBinarySampling(Sampling):
+class UniformBinarySampling(Sampling): #pymoo library
     def _do(self, problem, n_samples, **kwargs):
+
         num_ones = np.linspace(0, problem.n_var, n_samples, dtype=int)
         num_ones[-1] = problem.n_var
         ones_into_array = np.zeros((n_samples, problem.n_var), dtype=int)
@@ -38,49 +45,76 @@ class UniformBinarySampling(Sampling):
         for i, num in enumerate(num_ones):
             ones_into_array[i, :num] = 1
             np.random.shuffle(ones_into_array[i])
+        print("testing")
         return ones_into_array
 
+def UniformBinaryPopulation(problem_ptp,pop_size):
+        """
+        BASED ON PLATYPUS LIBRARY ONLY
+        Defines the initial population matrix as an array and then transforms into platypus solution object
+        to be used for InjectedPopulation Generator method. It is based on UniformBinarySampling method for pymoo
 
-def generate_uniform_binary_pop(problem_ptp, pop_size):
-    """
-    Based on the Platypus library
-    Defines the initial population matrix as an array and then transforms into platypus solution object
-    to be used for InjectedPopulation Generator method. It is based on UniformBinarySampling method for pymoo
+        """
+        #inputs:
+        n_samples=pop_size #78
+        #creating an array with the number of ones we want in each individual:
+        num_ones = np.linspace(0, problem_ptp.nvars, n_samples, dtype=int)
+        num_ones[-1] = problem_ptp.nvars
+        # creating an array for each individual, with the number of ones defined in "num_ones"
+        ones_into_array = np.zeros((n_samples, problem_ptp.nvars), dtype=int)
+        # Fill ones_into_array randomly
+        ones_into_array_bin=[]
+        for i, num in enumerate(num_ones):
+            ones_into_array[i, :num] = 1
+            np.random.shuffle(ones_into_array[i])
+            #changing into platypus format:
+            aux=[]
+            for j in range(problem_ptp.nvars):
+                aux.append([bool(ones_into_array[i,j])])
+            ones_into_array_bin.append(aux)
 
-    :param problem_ptp: problem object from platypus library
-    :param pop_size: number of individuals in the population
-    :return: initial_population: list of platypus solution objects
-    """
-    # inputs:
-    n_samples = pop_size
-    # creating an array with the number of ones we want in each individual:
-    num_ones = np.linspace(0, problem_ptp.nvars, n_samples, dtype=int)
-    num_ones[-1] = problem_ptp.nvars
-    # creating an array for each individual, with the number of ones defined in "num_ones"
-    ones_into_array = np.zeros((n_samples, problem_ptp.nvars), dtype=int)
-    # Fill ones_into_array randomly
-    ones_into_array_bin = []
-    for i, num in enumerate(num_ones):
-        ones_into_array[i, :num] = 1
-        np.random.shuffle(ones_into_array[i])
-        aux = []
-        for j in range(problem_ptp.nvars):
-            aux.append(bool(ones_into_array[i, j]))
-        ones_into_array_bin.append(aux)
+        predefined_population =np.array(ones_into_array_bin) #array instead of list
+        # #to check number of ones per individual:
+        # for i in range(78):
+        #     unos = 0
+        #     suma=0
+        #     #print(i)
+        #     for j in range(390):
+        #         #print(j)
+        #         if ones_into_array_bin[i][j][0] == True: unos = unos + 1
+        #     suma=np.sum(ones_into_array[i])
+        #     print("individuo {} unos: {}".format(i, suma))
+        #         #print(unos)
+        #     print("individuo {} trues: {}".format(i, unos))
 
-    predefined_population = np.array(ones_into_array_bin)
+        #defining the initial population as a solution object
+        ind=0
+        initial_population=[]
+        for individual in predefined_population:
+            solution=Solution(problem_ptp)
+            solution.variables=individual.tolist()
+            #problem_ptp.evaluate(solution)
+            initial_population.append(solution)
+            ind=ind+1
+            #print('individual: {}'.format(ind))
 
-    # defining the initial population as a solution object
-    ind = 0
-    initial_population = []
-    for individual in predefined_population:
+        return initial_population
+
+class UniformBinaryGenerator(Generator): #platypus library, not used now, see UniformBinaryGenerator function above
+    def __init__(self):
+        super().__init__()
+
+    def generate(self, problem_ptp):
         solution = Solution(problem_ptp)
-        solution.variables = individual.tolist()
-        initial_population.append(solution)
-        ind = ind + 1
-
-    return initial_population
-
+        random_population=np.random.randint(0, 2, size=problem_ptp.nvars, dtype=int) #return random integers from the discrte uniform distribution (numpy array)
+        random_population2=np.random.choice([0,1], p=[0.7,0.3],size=390)
+        random_population=np.zeros(390) #initialize vector to zeros
+        initial_population=[]
+        for i in range(390):
+            boolean_random_population=bool(random_population[i])
+            initial_population.append([boolean_random_population])
+        solution.variables = initial_population
+        return solution
 
 class SkewedBinarySampling(Sampling):
     def _do(self, problem, n_samples, **kwargs):
@@ -93,6 +127,13 @@ class SkewedBinarySampling(Sampling):
         for i, num in enumerate(num_ones):
             ones_into_array[i, :num] = 1
             np.random.shuffle(ones_into_array[i])
+
+        # # Add rows with only one '1'
+        # additional_rows = 100
+        # for _ in range(additional_rows):
+        #     row = np.zeros(problem.n_var, dtype=int)
+        #     row[np.random.randint(0, problem.n_var)] = 1
+        #     ones_into_array = np.vstack([ones_into_array, row])
 
         return ones_into_array
 
@@ -152,7 +193,7 @@ class GridNsga(ElementwiseProblem):
         :param kwargs:
         :return:
         """
-        out["F"] = self.obj_func(x)  # F is array of 2 values (ndarray)
+        out["F"] = self.obj_func(x) # F is array of 2 values (ndarray)
 
 
 def NSGA_3(obj_func,
@@ -182,7 +223,7 @@ def NSGA_3(obj_func,
     ref_dirs = get_reference_directions("reduction", n_obj, n_partitions, seed=1)
 
     algorithm = NSGA3(pop_size=pop_size,
-                      sampling=UniformBinarySampling(),  # UniformBinarySampling() for ideal grid #SkewedBinarySampling
+                      sampling=UniformBinarySampling(),  #UniformBinarySampling() for ideal grid #SkewedBinarySampling
                       crossover=SBX_pymoo(prob=crossover_prob, eta=eta, vtype=float, repair=RoundingRepair()),
                       mutation=BitflipMutation(prob=mutation_probability, prob_var=0.4, repair=RoundingRepair()),
                       # selection=TournamentSelection(pressure=2),
@@ -202,17 +243,16 @@ def NSGA_3(obj_func,
 
     return res.X, res.F
 
-
 def NSGA_3_platypus(obj_func,
-                    n_partitions: int = 100,
-                    n_var: int = 1,
-                    n_obj: int = 2,
-                    n_const: int = 0,  # 1 for platypus test problem, 0 for PF problem
-                    max_evals: int = 30,
-                    pop_size: int = 1,
-                    crossover_prob: float = 0.05,
-                    mutation_probability=0.5,
-                    eta: float = 3.0):
+           n_partitions: int = 100,
+           n_var: int = 1,
+           n_obj: int = 2,
+           n_const: int = 0, # 1 for platypus test problem, 0 for PF problem
+           max_evals: int = 30,
+           pop_size: int = 1,
+           crossover_prob: float = 0.05,
+           mutation_probability=0.5,
+           eta: float = 3.0):
     """
 
     :param obj_func:
@@ -228,61 +268,114 @@ def NSGA_3_platypus(obj_func,
     """
 
     # Powerflow problem:
-    # problem_ptp=GridNsga_platypus(n_var, n_obj,n_const) #obj_func
-    problem_ptp = Problem(n_var, n_obj, n_const)
-    problem_ptp.types[:] = Integer(0, 1)
-    problem_ptp.function = obj_func
+    #problem_ptp=GridNsga_platypus(n_var, n_obj,n_const) #obj_func
+    problem_ptp=Problem(n_var, n_obj,n_const)
+    problem_ptp.types[:]=Integer(0,1)
+    problem_ptp.function=obj_func
 
-    init_pop_obj = generate_uniform_binary_pop(problem_ptp, pop_size)  # initialising population matrix
+    init_pop_obj=UniformBinaryPopulation(problem_ptp,pop_size) #initialising population matrix
 
-    # calling NSGA3 algorithm from Platypus library:
+    #calling NSGA3 algorithm from Platypus library:
     algorithm = NSGAIII(
-        problem_ptp,
-        divisions_outer=n_partitions,  # outer divisions for reference points
-        divisions_inner=0,  # optional, used for reference points too
-        generator=InjectedPopulation(init_pop_obj),
-        variator=CompoundOperator(SBX(probability=crossover_prob, distribution_index=eta),
-                                  BitFlip(probability=mutation_probability))  # for Powerflow objective function
+                        problem_ptp,
+                        divisions_outer=n_partitions,                   # outer divisions for reference points
+                        divisions_inner=0,                              # optional, used for reference points too
+                        generator=InjectedPopulation(init_pop_obj),     # init_pop_obj is the initial population as solution object from platypus
+                        #generator=RandomGenerator(),                   #generates initial population
+                        #generator=UniformBinaryGenerator(),            # generator class not good distribution of initial population
+                        #selector=TournamentSelector(2),                # selects parents during recombination
+                        variator=CompoundOperator(SBX(probability=crossover_prob, distribution_index=eta),
+                                                  BitFlip(probability=mutation_probability)) # for Powerflow objective function
 
-        # variator=CompoundOperator(SBX(), PMX(), PM(), UM())    # CompoundOperator for mixed variables: int and float
-    )
+                        #variator=CompoundOperator(SBX(), PMX(), PM(), UM())    # CompoundOperator for mixed variables: int and float
+                        )
     # running algorithm:
-    algorithm.population_size = pop_size  # optional - fixing population size, if not specified, it estimates it from "divisions_outer".
-    # algorithm.population = initial_population      # n/a
-    # algorithm.reference_points=[[1,1],[..],...]    #optional - specific reference points specified by user (list of lists)
+    algorithm.population_size=pop_size              #optional - fixing population size, if not specified, it estimates it from "divisions_outer".
+    #algorithm.population = initial_population      # n/a
+    #algorithm.reference_points=[[1,1],[..],...]    #optional - specific reference points specified by user (list of lists)
 
-    # =======================================STEP METHOD (EXECUTE + SAVE RESULTS)=========================================
-    output_per_iteration = []
-    iterations = int(max_evals / pop_size)
+    #=======================================STEP METHOD (EXECUTE + SAVE RESULTS)=========================================
+    output_per_iteration=[]
+    iterations=int(max_evals/pop_size)
     all_solutions = []
-    all_variables = []
-    all_objectives = []
+    all_variables=[]
+    all_objectives=[]
     all_variables_decoded = []
 
     for it in range(iterations):
         algorithm.step()
-        outputs = [solution.variables[:] for solution in algorithm.result]
+        outputs=[solution.variables[:] for solution in algorithm.result]
 
         for s in range(len(algorithm.result)):
             all_solutions.append(algorithm.result[s])
             all_variables.append(algorithm.result[s].variables)
-            all_variables_to_decode = algorithm.result[s].variables
+            all_variables_to_decode=algorithm.result[s].variables
             all_objectives.append(algorithm.result[s].objectives)
-            # all_variables_decoded = []
-            variables_decoded2 = [problem_ptp.types[i].decode(all_variables_to_decode[i]) for i in
-                                  range(len(algorithm.result[s].variables))]
-            all_variables_decoded.append(variables_decoded2)  # for each solution, stores all the values o
+            #all_variables_decoded = []
+            variables_decoded2 = [problem_ptp.types[i].decode(all_variables_to_decode[i]) for i in range(len(algorithm.result[s].variables))]
+            all_variables_decoded.append(variables_decoded2)              #for each solution, stores all the values o
         output_per_iteration.append(outputs)
-    res_objective_all = np.array(all_objectives[:])
+    res_objective_all=np.array(all_objectives[:])
+
+
+    #=================================================RUN METHOD=========================================================
+    #this method does not store all the solutions evaluated. It stores non-
+    #dominated solutions and all the solutions evaluated in the last generation
+
+    #algorithm.run(max_evals)                     # number of evaluations = Termination condition
+
+    #=======================================STORING RESULTS FOR RUN METHOD===============================================
+    # #platypus returns Integer(0,1) variables as True/false --> decode method to get 0/1
+    # res_objective_nd = []
+    # #res_norm_objective=[]                                          #normalised objective --> only for method algorithm.run()
+    # res_variables = []
+    # res_variables_decoded_nd=[]
+    # for solution in unique(nondominated(algorithm.result)):
+    #     res_objective_nd.append([solution.objectives[0], solution.objectives[1]])
+    #     #res_norm_objective.append([solution.normalized_objectives[0], solution.normalized_objectives[1]])      #only for method algorithm.run()
+    #     res_variables = solution.variables[:]                       # integer variables, returns true false (not decoded)
+    #     variables_decoded = []
+    #     variables_decoded2 = [problem_ptp.types[i].decode(res_variables[i]) for i in range(len(solution.variables))]
+    #     res_variables_decoded_nd.append(variables_decoded2)              #for each solution, stores all the values of the investments decoded (0,1)
+
+    #store last generation evaluation results --> N/A
+    # res_objective_lastgen=[]
+    # for solution in algorithm.result:
+    #     res_objective_lastgen.append(solution.objectives)
+
+    #=======================================EXPORT TO EXCEL SOLUTIONS OBJ VALUES========================================
+    # import pandas as pd
+    # df_nd = pd.DataFrame(res_objective_nd)              #only non-dominated solutions
+    # df_nd.to_excel('nsga_ptp_uf_nd.xlsx')               #only non-dominated solutions, UF= uniform sampling, ptp=platypus
+    # df_lasteval=pd.DataFrame(res_objective_lastgen)     # save all the solutions in last generation
+    # df_lasteval.to_excel('nsga_ptp_uf_lastgen.xlsx')    # Save all the solutions in last generation ,   UF= uniform sampling
+    # df_all = pd.DataFrame(res_objective_all)            # save all the solutions
+    # df_all.to_excel('nsga_ptp_uf_all.xlsx')             # Save all the solutions,  UF= uniform sampling
 
     hyp = Hypervolume(minimum=[0, 0], maximum=[1, 1])
     print("Hypervolume: {}".format(hyp(algorithm.result)))
 
-    print("pop_size: {}, pop_size internal: {}".format(algorithm.population_size, len(algorithm.population)))
+    print("pop_size: {}, pop_size internal: {}".format(algorithm.population_size,len(algorithm.population)))
     print("max_evals: {}".format(max_evals))
     print("divisions_outer: {}".format(n_partitions))
     print("n_vars: {}".format(n_var))
     print("n_const: {}".format(n_const))
     print("n_obj: {}".format(n_obj))
+    #print("Num de soluciones variables nondominated:{}".format(len(res_variables)))
+    #print("Num de soluciones objetivo nondominated:{}".format(len(res_objective_nd)))
+    #print("Num de soluciones objetivo last gen:{}".format(len(res_objective_lastgen)))
+    #print("Num de soluciones objetivo (all):{}".format(len(res_objective_all)))
+    #print("Num total de soluciones {}".format(len(algorithm.result)))
 
-    return all_variables_decoded, res_objective_all
+    """
+    outputs:
+    res_objective_all: result values of the objective functions (f1, f2) all solutions (non dominated and dominated) from method algorithm.step
+    res_objective_nd: result values of the objective functions (f1, f2) non-dominated solutions
+    res_objective_lastgen: algorithm.run() method gives all the solutions only from last generation
+    res_variables_decoded_nd: includes all the non dominated solutions
+    all_variables_decoded: includes all the solutions (all solutions, non dominated and dominated) from method algorithm.step
+    
+    """
+    return all_variables_decoded, res_objective_all #res_objective_nd, res_objective_lastgen, #res_variables_decoded_nd
+
+
--- src/trunk/investments/try_multiobj_nsga.py
+++ src/trunk/investments/try_multiobj_nsga.py
@@ -4,29 +4,69 @@ import matplotlib.pyplot as plt
 import pandas
 import pandas as pd
 from GridCalEngine.IO.file_handler import FileOpen
-import GridCalEngine.Simulations as Sim
+import GridCalEngine.Simulations as sim
 from GridCalEngine.enumerations import InvestmentEvaluationMethod, ResultTypes
 import time
 import cProfile
 import cProfile
 import pstats
 from pymoo.config import Config
-
 Config.warnings['not_compiled'] = False
 
 if __name__ == "__main__":
+    # absolute_path = os.path.abspath(
+    #   os.path.join(os.getcwd(), 'Grids_and_profiles', 'grids', 'ding0_test_network_2_mvlv.gridcal'))
+
     fname = os.path.join('..', '..', '..', 'Grids_and_profiles', 'grids', 'ding0_test_network_2_mvlv.gridcal')
-    # fname = os.path.join('/Users/CristinaFray/PycharmProjects/GridCal/src/trunk/investments/edited_IEEE 118 Bus - investments.gridcal')
+    #fname = os.path.join('/Users/CristinaFray/PycharmProjects/GridCal/src/trunk/investments/edited_IEEE 118 Bus - investments.gridcal')
     grid = FileOpen(fname).open()
 
     st_time = time.time()
 
-    pf_options = Sim.PowerFlowOptions()
-    options = Sim.InvestmentsEvaluationOptions(solver=InvestmentEvaluationMethod.NSGA3_platypus,
-                                               max_eval=4 * len(grid.investments),
-                                               pf_options=pf_options)
+    pf_options = sim.PowerFlowOptions()
+    #platypus:
+    options = sim.InvestmentsEvaluationOptions(solver=InvestmentEvaluationMethod.NSGA3_platypus,
+                                            max_eval=4 * len(grid.investments),
+                                            pf_options=pf_options)
 
-    inv = Sim.InvestmentsEvaluationDriver(grid, options=options)
+    #print("max_evals inicializadas: {}".format(4 * len(grid.investments)))
+    inv = sim.InvestmentsEvaluationDriver(grid, options=options)
+    #st_time = time.time()
     inv.run()
     e_time = time.time()
-    print(f"Simulation time: {e_time - st_time} sec")
+    print("Simulation time: {} sec".format(e_time - st_time))
+    print("Simulation time: {} min".format((e_time - st_time)/60))
+
+    # #============================================================
+    # #Results pymoo:
+    # # ============================================================
+    # output_f=inv.results._f_obj
+    # #combinations=inv.results._combinations
+    # output_f1=inv.results._financial
+    # output_f2=inv.results._electrical
+    # #save to excel all solutions (not only non-dominated)
+    # data=np.column_stack((output_f1,output_f2))
+    # dff = pd.DataFrame(data)
+    # dff.to_excel('nsga_PYMOO_all.xlsx')
+    #
+    # #plot all the results in scatter - not only pareto front - PYMOO
+    # import matplotlib.pyplot as plt
+    # import matplotlib
+    # import pandas as pd
+    # matplotlib.use("Qt5Agg")
+    # plt.scatter(data[0],data[1])
+    # plt.show()
+
+    # ============================================================
+    # Results PLATYPUS:
+    # ============================================================
+    #..................PLOTTING PLATYPUS................................
+    import matplotlib.pyplot as plt
+    import matplotlib
+    import pandas as pd
+    matplotlib.use("Qt5Agg")
+    data=pd.read_excel(r"C:\Users\cmach\PycharmProjects\GridCal2\src\trunk\investments\nsga_ptp_uf_all.xlsx")
+    plt.scatter(data[1],data[0], c='r', edgecolors='r')
+    plt.show()
+
+
