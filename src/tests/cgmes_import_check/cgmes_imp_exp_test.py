1ee377479d552e89943cca262045ec453a143049
benceszirbik <bence.szirbik@griddigit.eu>
2024-06-27 10:06:22 +0200

    soring and print names for debugging comparison

--- src/tests/cgmes_import_check/cgmes_imp_exp_test.py
+++ src/tests/cgmes_import_check/cgmes_imp_exp_test.py
@@ -44,7 +44,7 @@ def run_import_export_test(import_path: str | list[str], export_fname: str, boun
     logger = Logger()
     # CGMES model import to MultiCircuit
     circuit_1 = gc.open_file(import_path)
-    # circuit_1.buses.sort(key=lambda obj: obj.name)      # SORTING
+    circuit_1.buses.sort(key=lambda obj: obj.name)      # SORTING
     nc_1 = gc.compile_numerical_circuit_at(circuit_1)
     # run power flow
     pf_options = PowerFlowOptions()
@@ -66,7 +66,7 @@ def run_import_export_test(import_path: str | list[str], export_fname: str, boun
     cgmes_export.save_cgmes()
 
     circuit_2 = gc.open_file([export_fname, boundary_zip_path])
-    # circuit_2.buses.sort(key=lambda obj: obj.name)      # SORTING
+    circuit_2.buses.sort(key=lambda obj: obj.name)      # SORTING
     nc_2 = gc.compile_numerical_circuit_at(circuit_2)
 
     # COMPARING
@@ -82,10 +82,28 @@ def run_import_export_test(import_path: str | list[str], export_fname: str, boun
     else:
         logger.print()
 
-    S_diff = nc_2.Sbus - nc_1.Sbus
-    print(S_diff)
-    Y_diff = nc_2.Ybus.A - nc_1.Ybus.A
-    print(Y_diff)
+    # FOR DEBUG
+    print('Buses')
+    print(nc_1.bus_names)
+    print(nc_2.bus_names)
+    print('Loads')
+    print(nc_1.load_names)
+    print(nc_2.load_names)
+    print('Gens')
+    print(nc_1.generator_names)
+    print(nc_2.generator_names)
+    print('Sbus1')
+    print(nc_1.Sbus)
+    print('Sbus2')
+    print(nc_2.Sbus)
+    print('S_diff')
+    print(nc_2.Sbus - nc_1.Sbus)
+    print('Y1')
+    print(nc_1.Ybus.A)
+    print('Y2')
+    print(nc_2.Ybus.A)
+    print('Y_diff')
+    print(nc_2.Ybus.A - nc_1.Ybus.A)
 
     assert ok
 
