d69eeed28d9d8b6aff66549f098435ccd4988632
Santi <santiago@gridcal.com>
2024-07-08 17:32:07 +0100

    modified tuc17 for entsow IOP

--- src/trunk/entsoe/tuc17.py
+++ src/trunk/entsoe/tuc17.py
@@ -19,92 +19,60 @@ import numpy as np
 import GridCalEngine.api as gce
 
 
-def tuc_17_1() -> None:
+def tuc_17() -> None:
     """
-    This test load two supposedly equivalent grids and compared their internal structures
+    This test load two supposedly equivalent grids and compared their internal structures and their power flow
     """
-    base_folder = os.path.join("..", "..", "tests", "data", "grids", "CGMES_2_4_15",
-                               "TestConfigurations_packageCASv2.0")
 
-    logger = gce.Logger()
+    # path to the boundary set
+    bd_set = os.path.join("..", "..", "tests", "data", "grids", "CGMES_2_4_15",
+                          "TestConfigurations_packageCASv2.0", 'MicroGrid', 'BaseCase_BC',
+                          'CGMES_v2.4.15_MicroGridTestConfiguration_BD_v2.zip')
 
-    lst = [
-        os.path.join(base_folder, 'MicroGrid', 'BaseCase_BC', 'CGMES_v2.4.15_MicroGridTestConfiguration_BD_v2.zip'),
-        os.path.join(base_folder, 'MicroGrid', 'BaseCase_BC', 'CGMES_v2.4.15_MicroGridTestConfiguration_BC_BE_v2.zip'),
-    ]
-
-    grid1 = gce.open_file(filename=lst)
-
-    grid2 = gce.open_file(filename=os.path.join(base_folder, 'MicroGrid', 'BaseCase_BC', 'BE_psse33_cimconverter.raw'))
-
-    # does not make sense because ID?s are not respected
-    # ok, logger = grid1.compare_circuits(grid2)
-    #
-    # if not ok:
-    #     logger.print()
-
+    # load any model, IEEE14 in this case
+    grid1 = gce.open_file(filename=os.path.join("..", "..", "tests", "data", "grids", "RAW", "IEEE 14 bus.raw"))
     nc1 = gce.compile_numerical_circuit_at(grid1)
-    nc2 = gce.compile_numerical_circuit_at(grid2)
+    pf_res1 = gce.power_flow(grid1)
 
+    # we need to hack it a bit: add substations and voltage levels
+    gce.detect_substations(grid=grid1)
+
+    # save the model in CGMES
+    gce.save_cgmes_file(grid=grid1,
+                        filename="IEEE14.zip",
+                        cgmes_boundary_set_path=bd_set,
+                        cgmes_version=gce.CGMESVersions.v3_0_0,
+                        pf_results=pf_res1)
+
+    # load again from CGMES into a new grid object
+    grid2 = gce.open_file(filename=["IEEE14.zip", bd_set])
+    nc2 = gce.compile_numerical_circuit_at(grid2)
+    pf_res2 = gce.power_flow(grid2)
+
+    # TUC 17.1 compare structures (Ybus, Sbus, etc...)
     ok_nc, logger_nc = nc1.compare(nc2)
 
     if not ok_nc:
         logger_nc.print()
-        # logger_nc.to_xlsx("tuc_17_1.xlsx")
+        logger_nc.to_xlsx("tuc_17_1_ieee14.xlsx")
     else:
         print("TUC 17.1 ok")
 
-
-def tuc_17_2() -> None:
-    """
-    This test load two supposedly equivalent grids and compared their internal structures
-    """
-    base_folder = os.path.join("..", "..", "tests", "data", "grids", "CGMES_2_4_15",
-                               "TestConfigurations_packageCASv2.0")
-
-    logger = gce.Logger()
-
-    lst = [
-        os.path.join(base_folder, 'MicroGrid', 'BaseCase_BC', 'CGMES_v2.4.15_MicroGridTestConfiguration_BD_v2.zip'),
-        os.path.join(base_folder, 'MicroGrid', 'BaseCase_BC', 'CGMES_v2.4.15_MicroGridTestConfiguration_BC_BE_v2.zip'),
-    ]
-
-    grid1 = gce.open_file(filename=lst)
-
-    grid2 = gce.open_file(filename=os.path.join(base_folder, 'MicroGrid', 'BaseCase_BC', 'BE_psse33_cimconverter.raw'))
-
-    # does not make sense because ID?s are not respected
-    # ok, logger = grid1.compare_circuits(grid2)
-    #
-    # if not ok:
-    #     logger.print()
-
-    res1 = gce.power_flow(grid1)
-    res2 = gce.power_flow(grid2)
-
-    df1 = res1.get_bus_df()
-    df2 = res2.get_bus_df()
+    # TUC 17.2 compare power flows
+    df1 = pf_res1.get_bus_df()
+    df2 = pf_res2.get_bus_df()
 
     diff = df1 - df2
 
     print("Grid 1")
-    print(res1.get_bus_df())
+    print(pf_res1.get_bus_df())
 
     print("Grid 2")
-    print(res2.get_bus_df())
+    print(pf_res2.get_bus_df())
 
     print("Difference")
     print(diff)
 
-    # if not ok:
-    #     print("Grid 1")
-    #     print(res1.get_bus_df())
-    #     print("Grid 2")
-    #     print(res2.get_bus_df())
-    # else:
-    #     print("TUC 17.2 ok")
-
 
 if __name__ == '__main__':
-    tuc_17_1()
-    tuc_17_2()
+    tuc_17()
