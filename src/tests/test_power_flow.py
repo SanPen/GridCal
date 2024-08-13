f1b7ac6e18db8fe25dc982465ba53fc90e28a67c
santi <santiago.penate.vera@gmail.com>
2024-08-13 17:56:33 +0100

    Added Qf and Qt control tests All pf tests passing

--- src/tests/test_power_flow.py
+++ src/tests/test_power_flow.py
@@ -262,7 +262,52 @@ def test_voltage_control_with_ltc() -> None:
         assert np.isclose(vm[ctrl_idx], grid.transformers2w[0].vset, atol=options.tolerance)
 
 
-def test_power_flow_control_with_pst() -> None:
+def test_qf_control_with_ltc() -> None:
+    """
+    Check that a transformer can regulate the voltage at a bus
+    """
+    fname = os.path.join('data', 'grids', '5Bus_PST_FACTS_Fig4.10(Qf).gridcal')
+
+    grid = gce.open_file(fname)
+
+    for solver_type in [SolverType.NR]:
+        options = PowerFlowOptions(solver_type,
+                                   verbose=0,
+                                   control_q=False,
+                                   retry_with_other_methods=False)
+
+        results = gce.power_flow(grid, options)
+
+        assert results.converged
+
+        # check that the bus voltage module is the the transformer voltage set point
+        assert np.isclose(results.Sf[7].imag, grid.transformers2w[0].Qset, atol=options.tolerance)
+
+
+def test_qt_control_with_ltc() -> None:
+    """
+    Check that a transformer can regulate the voltage at a bus
+    """
+    fname = os.path.join('data', 'grids', '5Bus_PST_FACTS_Fig4.10(Qf).gridcal')
+
+    grid = gce.open_file(fname)
+    grid.transformers2w[0].tap_module_control_mode = gce.TapModuleControl.Qt
+
+    for solver_type in [SolverType.NR]:
+        options = PowerFlowOptions(solver_type,
+                                   verbose=0,
+                                   control_q=False,
+                                   retry_with_other_methods=False)
+
+        results = gce.power_flow(grid, options)
+
+        assert results.converged
+
+        # check that the bus voltage module is the the transformer voltage set point
+        assert np.isclose(results.St[7].imag, grid.transformers2w[0].Qset, atol=options.tolerance)
+
+
+def test_power_flow_control_with_pst_pf() -> None:
     """
     Check that a transformer can regulate the voltage at a bus
     """
