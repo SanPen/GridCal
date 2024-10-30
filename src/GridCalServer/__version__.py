d036612a64f54a117e5e51b887b348ada58de940
santi <santiago.penate.vera@gmail.com>
2024-10-30 18:08:26 +0000

    5.1.59

--- src/GridCalServer/__version__.py
+++ src/GridCalServer/__version__.py
@@ -16,7 +16,7 @@ import datetime
 _current_year_ = datetime.datetime.now().year
 
 # do not forget to keep a three-number version!!!
-__GridCalServer_VERSION__ = "5.1.58"
+__GridCalServer_VERSION__ = "5.1.59"
 
 url = 'https://github.com/SanPen/GridCal'
 
