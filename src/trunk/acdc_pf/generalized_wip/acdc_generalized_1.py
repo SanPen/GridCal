4a61ea031cf215d74351453767a5a3a88f14699d
santi <santiago.penate.vera@gmail.com>
2024-11-12 14:18:01 +0000

    merged Raiyan's changes

--- src/trunk/acdc_pf/generalized_wip/acdc_generalized_1.py
+++ src/trunk/acdc_pf/generalized_wip/acdc_generalized_1.py
@@ -4,7 +4,7 @@ import GridCalEngine as gce
 fname = os.path.join("..", "..", "..", "..", "Grids_and_profiles", "grids", "fubm_caseHVDC_vt.gridcal")
 grid = gce.open_file(fname)
 
-main_nc = gce.compile_numerical_circuit_at(grid)
+main_nc = gce.compile_numerical_circuit_at(grid, consider_vsc_as_island_links=False)
 
 islands = main_nc.split_into_islands(
     ignore_single_node_islands=False,
