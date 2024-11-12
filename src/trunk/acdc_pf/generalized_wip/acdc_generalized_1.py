bda313268968df6f65626acd34975dbfcb3dd45f
santi <santiago.penate.vera@gmail.com>
2024-11-12 09:53:31 +0000

    Added options to split_into_islands in acdc_generalized_1.py

--- src/trunk/acdc_pf/generalized_wip/acdc_generalized_1.py
+++ src/trunk/acdc_pf/generalized_wip/acdc_generalized_1.py
@@ -4,9 +4,12 @@ import GridCalEngine as gce
 fname = os.path.join("..", "..", "..", "..", "Grids_and_profiles", "grids", "fubm_caseHVDC_vt.gridcal")
 grid = gce.open_file(fname)
 
-
 main_nc = gce.compile_numerical_circuit_at(grid)
 
-islands = main_nc.split_into_islands()
+islands = main_nc.split_into_islands(
+    ignore_single_node_islands=False,
+    consider_hvdc_as_island_links=False,
+    consider_vsc_as_island_links=False,
+)
 
 print()
