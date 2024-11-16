3f9cd88e70d3a3f3e8b7683846e4285ceedfbe54
JosepFanals <jfanals13@gmail.com>
2024-11-16 18:14:00 +0100

    Checked indices filling, now move to circuit to data

--- src/GridCalEngine/Topology/generalized_simulation_indices.py
+++ src/GridCalEngine/Topology/generalized_simulation_indices.py
@@ -101,20 +101,18 @@ class GeneralizedSimulationIndices:
 
         self.bus_voltage_used: BoolVec | None = None
 
-    def fill_specified(self, nc: NumericalCircuit,
-                       use_stored_guess: bool = False,
-                       control_remote_voltage: bool = True) -> "GeneralizedSimulationIndices":
+    def fill_specified(self, nc: NumericalCircuit)
         """
         Fill the specified sets by going over each device
         Possible duplicity of controls in setting Vm; we check for this and raise an error if so
+        This function will eventually be removed, we have to move it inside the circuit_to_data
 
         :param nc: numerical circuit
-        :param use_stored_guess:
-        :param control_remote_voltage: true if controlling a remote bus
         :return:
         """
         self.bus_voltage_used = np.zeros(nc.nbus, dtype=bool)
 
+        # DONE
         # -------------- Slack Buses search ----------------
         # Assume they are all set, but probably need some logic when compiling the numerical circuit to
         # enforce we have one slack on each AC island, split by the VSCs
@@ -123,8 +121,9 @@ class GeneralizedSimulationIndices:
             if bus.bus_types == BusMode.Slack_tpe.value:
                 self.add_to_c_va(i)
                 self.set_bus_vm_simple(bus_local=i,
-                                       bus_data=nc.bus_data)
+                                       is_slack=True)
 
+        # DONE
         # -------------- Generators and Batteries search ----------------
         for dev_tpe in (nc.generator_data, nc.battery_data):
             for i, is_controlled in enumerate(dev_tpe.controllable):
@@ -135,7 +134,7 @@ class GeneralizedSimulationIndices:
                     remote_control = ctr_bus_idx != -1
 
                     self.set_bus_vm_simple(bus_local=bus_idx,
-                                           bus_data=nc.bus_data,
+                                           device_name=dev_tpe.names[i],
                                            bus_remote=ctr_bus_idx,
                                            remote_control=remote_control)
 
@@ -145,6 +144,7 @@ class GeneralizedSimulationIndices:
                     self.add_to_c_p_zip(bus_idx)
                     self.add_to_c_q_zip(bus_idx)
 
+        # DONE
         # -------------- ControlledShunts search ----------------
         # Setting the Vm has already been done before
         for i, is_controlled in enumerate(nc.shunt_data.controllable):
@@ -155,168 +155,77 @@ class GeneralizedSimulationIndices:
                 remote_control = ctr_bus_idx != -1
 
                 self.set_bus_vm_simple(bus_local=bus_idx,
-                                       bus_data=nc.bus_data,
+                                       device_name = nc.shunt_data.names[i],
                                        bus_remote=ctr_bus_idx,
                                        remote_control=remote_control)
 
+        # DONE
         # -------------- HvdcLines search ----------------
         # The Pf equation looks like: Pf = Pset + bool_mode * kdroop * (angle_F - angle_T)
         # The control mode does not change the indices sets, only the equation
         # See how to store this bool_mode
         for i, hvdc_dev in enumerate(nc.hvdc_data[:]):
-            branch_idx = nc.branch_dict[hvdc_dev.idtag]  # or assume we can have this index
-            self.add_to_c_Pf(branch_idx)
-            self.add_to_c_hvdc(branch_idx)
-            self.set_bus_vm_simple(bus_local=hvdc_dev.F,
-                                   bus_data=nc.bus_data,
-                                   bus_remote=-1,
-                                   remote_control=False)
-            self.set_bus_vm_simple(bus_local=hvdc_dev.T,
-                                   bus_data=nc.bus_data,
-                                   bus_remote=-1,
-                                   remote_control=False)
 
+            self.add_to_c_Pf(branch_idx)
+
+            self.add_to_c_hvdc(branch_idx)
+
+            self.set_bus_vm_simple(bus_local=hvdc_dev.F,
+                                   device_name=hvdc_dev.name)
+
+            self.set_bus_vm_simple(bus_local=hvdc_dev.T,
+                                   device_name=hvdc_dev.name)
+
+        # DONE
         # -------------- VSCs search ----------------
         for i, vsc_dev in enumerate(nc.vsc_data[:]):
-            branch_idx = nc.branch_dict[vsc_dev.idtag]
-            if vsc_dev.tap_module_control_mode == TapModuleControl.Vm:
-                self.set_bus_vm_simple(bus_local=nc.bus_dict[vsc_dev.regulated_bus],
-                                       bus_data=nc.bus_data,
-                                       bus_remote=-1,
-                                       remote_control=False)
-            elif vsc_dev.tap_module_control_mode == TapModuleControl.Qf:
-                self.add_to_c_Qf(branch_idx)
-            elif vsc_dev.tap_module_control_mode == TapModuleControl.Qt:
-                self.add_to_c_Qt(branch_idx)
-            elif vsc_dev.tap_module_control_mode == TapModuleControl.fixed:
-                pass
 
-            if vsc_dev.tap_phase_control_mode == TapPhaseControl.Pf:
-                self.add_to_c_Pf(branch_idx)
-            elif vsc_dev.tap_phase_control_mode == TapPhaseControl.Pt:
-                self.add_to_c_Pt(branch_idx)
-            elif vsc_dev.tap_phase_control_mode == TapPhaseControl.fixed:
-                pass
+            self.add_tau_control_branch(mode=vsc_dev.tap_phase_control_mode,
+                                        branch_idx=branch_idx)
+
+            self.add_m_control_branch(branch_name=vsc_dev.name,
+                                      mode=vsc_dev.tap_module_control_mode,
+                                      branch_idx=branch_idx,
+                                      bus_idx=bus_idx)
 
             self.add_to_c_acdc(branch_idx)
 
+        # DONE
         # -------------- Transformers search (also applies to windings) ----------------
-        for i, trafo_dev in enumerate(nc.trafo_data[:]):
-            branch_idx = nc.branch_dict[trafo_dev.idtag]
-            if trafo_dev.tap_module_control_mode == TapModuleControl.Vm:
-                self.set_bus_vm_simple(bus_local=nc.bus_dict[trafo_dev.regulated_bus],
-                                       bus_data=nc.bus_data,
-                                       bus_remote=-1,
-                                       remote_control=False)
-            elif trafo_dev.tap_module_control_mode == TapModuleControl.Qf:
-                self.add_to_c_Qf(branch_idx)
-            elif trafo_dev.tap_module_control_mode == TapModuleControl.Qt:
-                self.add_to_c_Qt(branch_idx)
-            elif trafo_dev.tap_module_control_mode == TapModuleControl.fixed:
-                pass
+        for i, trafo_dev in enumerate(nc.transformer_data[:]):
 
-            if trafo_dev.tap_phase_control_mode == TapPhaseControl.Pf:
-                self.add_to_c_Pf(branch_idx)
-            elif trafo_dev.tap_phase_control_mode == TapPhaseControl.Pt:
-                self.add_to_c_Pt(branch_idx)
-            elif trafo_dev.tap_phase_control_mode == TapPhaseControl.fixed:
-                pass
+            self.add_tau_control_branch(mode=trafo_dev.tap_phase_control_mode,
+                                        branch_idx=branch_idx)
 
-            self.add_to_c_tau(branch_idx)
-            self.add_to_c_m(branch_idx)
-
-
-
-
-
-        for i, tpe in enumerate(nc.bus_data.bus_types):
-            if tpe == BusMode.Slack_tpe.value:
-                self.c_va.add(i)
-                self.c_vm.add(i)
-            # elif tpe == BusMode.PQ_tpe.value:
-            #     pass
-            # elif tpe == BusMode.PV_tpe.value:
-            #     self.c_vm.add(i)
-            # elif tpe == BusMode.PQV_tpe.value:
-            #     self.c_vm.add(i)
-            # elif tpe == BusMode.P_tpe.value:
-            #     pass
-
-        for struct in (nc.generator_data, nc.battery_data):
-            for e, is_controlled in enumerate(struct.controllable):
-                bus_idx = struct.bus_idx[e]
-                ctr_bus_idx = struct.controllable_bus_idx[e]
-
-                if is_controlled:
-                    remote_control = ctr_bus_idx != -1
-
-                    self.set_bus_control_voltage(i=bus_idx,
-                                                 j=ctr_bus_idx,
-                                                 remote_control=remote_control and control_remote_voltage,
-                                                 bus_name=nc.bus_data.names[bus_idx],
-                                                 bus_data=nc.bus_data,
-                                                 candidate_Vm=struct.v[e],
-                                                 use_stored_guess=use_stored_guess)
-
-                    # Set voltage magnitude through set_bus_control_voltage
-
-                else:
-                    self.c_p_zip.add(bus_idx)
-                    self.c_q_zip.add(bus_idx)
-
-        for struct in (nc.branch_data, nc.vsc_data):
-            for k, tpe in enumerate(struct.tap_module_control_mode):
-                if tpe == TapModuleControl.fixed:
-                    pass
-                elif tpe == TapModuleControl.Vm:
-                    ii = struct.tap_controlled_buses[k]
-                    if ii > -1:
-                        # if the controlled branch was specified, use that
-                        self.c_vm.add(ii)
-                    else:
-                        # else, we pick the from bus
-                        iii = struct.F[k]
-                        self.c_vm.add(iii)
-
-                elif tpe == TapModuleControl.Qf:
-                    self.c_Qf.add(k)
-
-                elif tpe == TapModuleControl.Qt:
-                    self.c_Qt.add(k)
-
-            for k, tpe in enumerate(struct.tap_phase_control_mode):
-                if tpe == TapPhaseControl.fixed:
-                    pass
-                elif tpe == TapPhaseControl.Pf:
-                    self.c_Pf.add(k)
-
-                elif tpe == TapPhaseControl.Pt:
-                    self.c_Pt.add(k)
-
-        return self
+            self.add_m_control_branch(branch_name=trafo_dev.name,
+                                      mode=trafo_dev.tap_module_control_mode,
+                                      branch_idx=branch_idx,
+                                      bus_idx=bus_idx)
 
     def fill_unspecified(self, nc: NumericalCircuit) -> "GeneralizedSimulationIndices":
         """
         Redo, just negate the specified I guess
+        For this counting the total of buses and branches would come in handy
         :param nc:
         :return:
         """
 
-        for i, tpe in enumerate(nc.bus_data.bus_types):
-            if tpe == BusMode.Slack_tpe.value:
-                self.c_va.add(i)
-                self.c_vm.add(i)
-            elif tpe == BusMode.PQ_tpe.value:
-                pass
-            elif tpe == BusMode.PV_tpe.value:
-                self.c_vm.add(i)
-            elif tpe == BusMode.PQV_tpe.value:
-                self.c_vm.add(i)
-            elif tpe == BusMode.P_tpe.value:
-                pass
+        # for i, tpe in enumerate(nc.bus_data.bus_types):
+        #     if tpe == BusMode.Slack_tpe.value:
+        #         self.c_va.add(i)
+        #         self.c_vm.add(i)
+        #     elif tpe == BusMode.PQ_tpe.value:
+        #         pass
+        #     elif tpe == BusMode.PV_tpe.value:
+        #         self.c_vm.add(i)
+        #     elif tpe == BusMode.PQV_tpe.value:
+        #         self.c_vm.add(i)
+        #     elif tpe == BusMode.P_tpe.value:
+        #         pass
 
         return self
 
+    # Primitive additions
     def add_to_c_va(self, value: int):
         """
 
@@ -425,7 +334,10 @@ class GeneralizedSimulationIndices:
         """
         self.c_hvdc.add(value)
 
-    def add_tap_phase_control(self, mode: TapPhaseControl, branch_idx: int):
+    # Non-primitive additions
+    def add_tau_control_branch(self,
+                               mode: TapPhaseControl = TapPhaseControl.fixed,
+                               branch_idx: int = None):
         """
 
         :param mode:
@@ -442,21 +354,28 @@ class GeneralizedSimulationIndices:
             self.add_to_c_Pt(branch_idx)
 
         else:
-            pass  # it is controllable
+            pass
 
-    def add_tap_module_control(self, mode: TapModuleControl, branch_idx, bus_idx: int):
+    def add_m_control_branch(self,
+                             branch_name: str = "",
+                             mode: TapModuleControl = TapModuleControl.fixed,
+                             branch_idx: int = None,
+                             bus_idx: int = None):
+
         """
-
+        :param branch_name:
         :param mode:
         :param branch_idx:
         :param bus_idx:
         :return:
         """
         if mode == TapModuleControl.fixed:
+            # May get an error eventually with VSCs
             self.add_to_c_m(branch_idx)
 
         elif mode == TapModuleControl.Vm:
-            self.add_to_c_vm(bus_idx)
+            self.set_bus_vm_simple(bus_local=bus_idx,
+                                   device_name=branch_name)
 
         elif mode == TapModuleControl.Qf:
             self.add_to_c_Qf(branch_idx)
@@ -465,27 +384,29 @@ class GeneralizedSimulationIndices:
             self.add_to_c_Qt(branch_idx)
 
         else:
-            pass  # it is controllable
+            pass
 
     def add_generator_behaviour(self, bus_idx: int, is_v_controlled: bool):
         pass
 
     def set_bus_vm_simple(self,
                           bus_local: int,
-                          bus_data: BusData,
+                          device_name="",
                           bus_remote: int = -1,
-                          remote_control: bool = False):
+                          remote_control: bool = False,
+                          is_slack: bool = False):
         """
         Set the bus control voltage checking incompatibilities
         No point in setting the voltage magnitude, already did in the circuit_to_data
 
         :param bus_local: Local bus index
-        :param bus_data: BusData
+        :param device_name: Name to store in the logger
         :param bus_remote: Remote bus index
         :param remote_control: Remote control?
+        :param is_slack: Is it a slack bus?
         """
 
-        if bus_data.bus_types[bus_local] == BusMode.Slack_tpe.value:
+        if is_slack:
             self.add_to_c_vm(bus_local)
             self.bus_voltage_used[bus_local] = True
 
@@ -498,11 +419,11 @@ class GeneralizedSimulationIndices:
                     self.add_to_c_vm(bus_remote)
                 else:
                     self.logger.add_error(msg='Trying to set an already fixed voltage, duplicity of controls',
-                                          device=bus_data.names[bus_remote],
+                                          device=device_name,
                                           device_property='Vm')
             elif remote_control:
                 self.logger.add_error(msg='Remote control without a valid remote bus',
-                                      device=bus_data.names[bus_remote],
+                                      device=device_name,
                                       device_property='Vm')
 
             # Not a remote bus control
@@ -512,7 +433,7 @@ class GeneralizedSimulationIndices:
                 self.add_to_c_vm(bus_local)
             else:
                 self.logger.add_error(msg='Trying to set an already fixed voltage, duplicity of controls',
-                                      device=bus_data.names[bus_local],
+                                      device=device_name,
                                       device_property='Vm')
 
     def set_bus_control_voltage(self,
