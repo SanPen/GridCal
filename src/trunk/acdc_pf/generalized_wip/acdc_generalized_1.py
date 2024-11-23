c266c19b71dfb908bddb01ead233ea43fd8315bc
JosepFanals <jfanals13@gmail.com>
2024-11-23 17:36:39 +0100

    Try with full losses, not yet working

--- src/trunk/acdc_pf/generalized_wip/acdc_generalized_1.py
+++ src/trunk/acdc_pf/generalized_wip/acdc_generalized_1.py
@@ -10,7 +10,7 @@ from GridCalEngine.basic_structures import Logger
 # fname = os.path.join("..", "..", "..", "..", "Grids_and_profiles", "grids", "fubm_caseHVDC_vt.gridcal")
 # fname = os.path.join("..", "..", "..", "..", "Grids_and_profiles", "grids", "fubm_caseHVDC_vt_raiyan.gridcal")
 # fname = os.path.join("..", "..", "..", "..", "Grids_and_profiles", "grids", "fubm_caseHVDC_vt_raiyan_signs.gridcal")
-fname = os.path.join("..", "..", "..", "..", "Grids_and_profiles", "grids", "fubm_caseHVDC_vt_mod4.gridcal")
+fname = os.path.join("..", "..", "..", "..", "Grids_and_profiles", "grids", "fubm_caseHVDC_vt_mod6.gridcal")
 # fname = os.path.join("..", "..", "..", "..", "Grids_and_profiles", "grids", "fubm_caseHVDC_vt_josep.gridcal")
 grid = gce.open_file(fname)
 # run power flow
