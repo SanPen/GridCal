bc315294f3c1652100a1b88cf1d12e00fed2e372
JosepFanals <jfanals13@gmail.com>
2024-11-23 16:59:36 +0100

    Got matching results with lossless vscs

--- src/trunk/acdc_pf/generalized_wip/acdc_generalized_1.py
+++ src/trunk/acdc_pf/generalized_wip/acdc_generalized_1.py
@@ -10,7 +10,7 @@ from GridCalEngine.basic_structures import Logger
 # fname = os.path.join("..", "..", "..", "..", "Grids_and_profiles", "grids", "fubm_caseHVDC_vt.gridcal")
 # fname = os.path.join("..", "..", "..", "..", "Grids_and_profiles", "grids", "fubm_caseHVDC_vt_raiyan.gridcal")
 # fname = os.path.join("..", "..", "..", "..", "Grids_and_profiles", "grids", "fubm_caseHVDC_vt_raiyan_signs.gridcal")
-fname = os.path.join("..", "..", "..", "..", "Grids_and_profiles", "grids", "fubm_caseHVDC_vt_mod.gridcal")
+fname = os.path.join("..", "..", "..", "..", "Grids_and_profiles", "grids", "fubm_caseHVDC_vt_mod2.gridcal")
 # fname = os.path.join("..", "..", "..", "..", "Grids_and_profiles", "grids", "fubm_caseHVDC_vt_josep.gridcal")
 grid = gce.open_file(fname)
 # run power flow
