84c45741a6e1dc027dd6649447af30c90ed95603
santi <santiago.penate.vera@gmail.com>
2024-11-28 18:19:57 +0000

    minimal changes to the versioning

--- src/GridCalServer/__version__.py
+++ src/GridCalServer/__version__.py
@@ -25,25 +25,3 @@ You can obtain one at https://mozilla.org/MPL/2.0/.
 
 The source of GridCal can be found at:
 """ + url + "\n\n"
-
-copyright_msg = 'Copyright (C) 2015-' + str(_current_year_) + ' Santiago Peñate Vera and contributors:'
-
-contributors_msg = 'Michel Lavoie (Transformer automation)\n'
-contributors_msg += 'Bengt Lüers (Better testing)\n'
-contributors_msg += 'Josep Fanals Batllori (HELM, Sequence Short circuit, ACOPF)\n'
-contributors_msg += 'Carlos Alegre (ACOPF)\n'
-contributors_msg += 'Laurens Bliek (MVRSM)\n'
-contributors_msg += 'Jana Soler and Cristina Fray (Better MVRSM and investments)\n'
-contributors_msg += 'Manuel Navarro Catalán (Better documentation)\n'
-contributors_msg += 'Paul Schultz (Grid Generator)\n'
-contributors_msg += 'Andrés Ramiro (Optimal net transfer capacity)\n'
-contributors_msg += 'Ameer Carlo Lubang (Sequence short-circuit)\n'
-contributors_msg += 'Fernando Postigo Marcos (Better contingencies and SRAP)\n'
-contributors_msg += ('Chavdar Ivanov, '
-                     'Bence Szirbik, '
-                     'Mate Zsebehazi (CGMES)\n')
-contributors_msg += ('Rubén Carmona Pardo, '
-                     'Julio González Mejías, '
-                     'Jesús Riquelme Santos (Contingencies and PTDF testing)\n')
-
-about_msg += copyright_msg + '\n' + contributors_msg
