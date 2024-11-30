Fix VSC loading calculation

src/GridCal/Gui/Diagrams/SchematicWidget/schematic_widget.py
@@ -3548,7 +3548,7 @@ class SchematicWidget(BaseDiagramWidget):
         # VSC lines
         if vsc_Pf is not None:
 
-            vsc_sending_power_norm = np.abs(vsc_Pf) / (max_flow + 1e-20)
+            vsc_sending_power_norm = np.abs(vsc_Pt + 1j * vsc_Qt) / (max_flow + 1e-20)
 
             if self.circuit.get_vsc_number() == len(vsc_Pf):
                 for i, elm in enumerate(self.circuit.vsc_devices):

src/GridCalEngine/Simulations/PowerFlow/Formulations/pf_generalized_formulation.py
@@ -1922,7 +1922,7 @@ class PfGeneralizedFormulation(PfFormulationTemplate):
         St_vsc = (self.Pt + 1j * self.Qt)[self.cg_acdc]
         If_vsc = Pf_vsc / np.abs(V[self.nc.vsc_data.F])
         It_vsc = St_vsc / np.conj(V[self.nc.vsc_data.T])
-        loading_vsc = Pf_vsc / (self.nc.vsc_data.rates + 1e-20)
+        loading_vsc = abs(St_vsc) / (self.nc.vsc_data.rates + 1e-20) * self.nc.Sbase
 
         # HVDC
         Sf_hvdc = (self.Pf + 1j * self.Qf)[self.cg_hvdc] * self.nc.Sbase