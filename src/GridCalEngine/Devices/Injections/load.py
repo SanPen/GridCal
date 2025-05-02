b7a0010317c11a97e6b23198a601d437938153a7
Alex Blanco <ablanco@eroots.tech>
2025-05-02 11:16:05 +0200

    3-phase "Load" property and setter

--- src/GridCalEngine/Devices/Injections/load.py
+++ src/GridCalEngine/Devices/Injections/load.py
@@ -18,6 +18,9 @@ class Load(LoadParent):
 
     def __init__(self, name='Load', idtag=None, code='',
                  G=0.0, B=0.0, Ir=0.0, Ii=0.0, P=0.0, Q=0.0, Cost=1200.0,
+                 P1=0.0, P2=0.0, P3=0.0, Q1=0.0, Q2=0.0, Q3=0.0,
+                 G1=0.0, G2=0.0, G3=0.0, B1=0.0, B2=0.0, B3=0.0,
+                 Ir1=0.0, Ir2=0.0, Ir3=0.0, Ii1=0.0, Ii2=0.0, Ii3=0.0,
                  active=True, mttf=0.0, mttr=0.0, capex=0, opex=0,
                  build_status: BuildStatus = BuildStatus.Commissioned):
         """
@@ -28,11 +31,29 @@ class Load(LoadParent):
         :param idtag: UUID code
         :param code: secondary ID code
         :param G: Conductance in equivalent MW
+        :param G1: Conductance in equivalent MW
+        :param G2: Conductance in equivalent MW
+        :param G3: Conductance in equivalent MW
         :param B: Susceptance in equivalent MVAr
+        :param B1: Susceptance in equivalent MVAr
+        :param B2: Susceptance in equivalent MVAr
+        :param B3: Susceptance in equivalent MVAr
         :param Ir: Real current in equivalent MW
+        :param Ir1: Real current in equivalent MW
+        :param Ir2: Real current in equivalent MW
+        :param Ir3: Real current in equivalent MW
         :param Ii: Imaginary current in equivalent MVAr
+        :param Ii1: Imaginary current in equivalent MVAr
+        :param Ii2: Imaginary current in equivalent MVAr
+        :param Ii3: Imaginary current in equivalent MVAr
         :param P: Active power in MW
+        :param P1: Active power in MW
+        :param P2: Active power in MW
+        :param P3: Active power in MW
         :param Q: Reactive power in MVAr
+        :param Q1: Reactive power in MVAr
+        :param Q2: Reactive power in MVAr
+        :param Q3: Reactive power in MVAr
         :param Cost: Cost of load shedding
         :param active: Is the load active?
         :param mttf: Mean time to failure in hours
@@ -46,7 +67,13 @@ class Load(LoadParent):
                             cn=None,
                             active=active,
                             P=P,
+                            P1=P1,
+                            P2=P2,
+                            P3=P3,
                             Q=Q,
+                            Q1=Q1,
+                            Q2=Q2,
+                            Q3=Q3,
                             Cost=Cost,
                             mttf=mttf,
                             mttr=mttr,
@@ -56,23 +83,71 @@ class Load(LoadParent):
                             device_type=DeviceType.LoadDevice)
 
         self.G = float(G)
+        self.G1 = float(G)
+        self.G2 = float(G)
+        self.G3 = float(G)
         self.B = float(B)
+        self.B1 = float(B)
+        self.B2 = float(B)
+        self.B3 = float(B)
         self.Ir = float(Ir)
+        self.Ir1 = float(Ir)
+        self.Ir2 = float(Ir)
+        self.Ir3 = float(Ir)
         self.Ii = float(Ii)
+        self.Ii1 = float(Ii)
+        self.Ii2 = float(Ii)
+        self.Ii3 = float(Ii)
 
         self._G_prof = Profile(default_value=self.G, data_type=float)
+        self._G1_prof = Profile(default_value=self.G, data_type=float)
+        self._G2_prof = Profile(default_value=self.G, data_type=float)
+        self._G3_prof = Profile(default_value=self.G, data_type=float)
         self._B_prof = Profile(default_value=self.B, data_type=float)
+        self._B1_prof = Profile(default_value=self.B, data_type=float)
+        self._B2_prof = Profile(default_value=self.B, data_type=float)
+        self._B3_prof = Profile(default_value=self.B, data_type=float)
         self._Ir_prof = Profile(default_value=self.Ir, data_type=float)
+        self._Ir1_prof = Profile(default_value=self.Ir, data_type=float)
+        self._Ir2_prof = Profile(default_value=self.Ir, data_type=float)
+        self._Ir3_prof = Profile(default_value=self.Ir, data_type=float)
         self._Ii_prof = Profile(default_value=self.Ii, data_type=float)
+        self._Ii1_prof = Profile(default_value=self.Ii, data_type=float)
+        self._Ii2_prof = Profile(default_value=self.Ii, data_type=float)
+        self._Ii3_prof = Profile(default_value=self.Ii, data_type=float)
 
         self.register(key='Ir', units='MW', tpe=float,
                       definition='Active power of the current component at V=1.0 p.u.', profile_name='Ir_prof')
+        self.register(key='Ir1', units='MW', tpe=float,
+                      definition='Active power of the phase 1 current component at V=1.0 p.u.', profile_name='Ir1_prof')
+        self.register(key='Ir2', units='MW', tpe=float,
+                      definition='Active power of the phase 2 current component at V=1.0 p.u.', profile_name='Ir2_prof')
+        self.register(key='Ir3', units='MW', tpe=float,
+                      definition='Active power of the phase 3 current component at V=1.0 p.u.', profile_name='Ir3_prof')
         self.register(key='Ii', units='MVAr', tpe=float,
                       definition='Reactive power of the current component at V=1.0 p.u.', profile_name='Ii_prof')
+        self.register(key='Ii1', units='MVAr', tpe=float,
+                      definition='Reactive power of the phase 1 current component at V=1.0 p.u.', profile_name='Ii1_prof')
+        self.register(key='Ii2', units='MVAr', tpe=float,
+                      definition='Reactive power of the phase 2 current component at V=1.0 p.u.', profile_name='Ii2_prof')
+        self.register(key='Ii3', units='MVAr', tpe=float,
+                      definition='Reactive power of the phase 3 current component at V=1.0 p.u.', profile_name='Ii3_prof')
         self.register(key='G', units='MW', tpe=float,
                       definition='Active power of the impedance component at V=1.0 p.u.', profile_name='G_prof')
+        self.register(key='G1', units='MW', tpe=float,
+                      definition='Active power of the phase 1 impedance component at V=1.0 p.u.', profile_name='G1_prof')
+        self.register(key='G2', units='MW', tpe=float,
+                      definition='Active power of the phase 2 impedance component at V=1.0 p.u.', profile_name='G2_prof')
+        self.register(key='G3', units='MW', tpe=float,
+                      definition='Active power of the phase 3 impedance component at V=1.0 p.u.', profile_name='G3_prof')
         self.register(key='B', units='MVAr', tpe=float,
                       definition='Reactive power of the impedance component at V=1.0 p.u.', profile_name='B_prof')
+        self.register(key='B1', units='MVAr', tpe=float,
+                      definition='Reactive power of the phase 1 impedance component at V=1.0 p.u.', profile_name='B1_prof')
+        self.register(key='B2', units='MVAr', tpe=float,
+                      definition='Reactive power of the phase 2 impedance component at V=1.0 p.u.', profile_name='B2_prof')
+        self.register(key='B3', units='MVAr', tpe=float,
+                      definition='Reactive power of the phase 3 impedance component at V=1.0 p.u.', profile_name='B3_prof')
 
     @property
     def Ir_prof(self) -> Profile:
@@ -92,6 +167,57 @@ class Load(LoadParent):
             raise Exception(str(type(val)) + 'not supported to be set into a Ir_prof')
 
     @property
+    def Ir1_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._Ir1_prof
+
+    @Ir1_prof.setter
+    def Ir1_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._Ir1_prof = val
+        elif isinstance(val, np.ndarray):
+            self._Ir1_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a Ir1_prof')
+
+    @property
+    def Ir2_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._Ir2_prof
+
+    @Ir2_prof.setter
+    def Ir2_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._Ir2_prof = val
+        elif isinstance(val, np.ndarray):
+            self._Ir2_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a Ir2_prof')
+
+    @property
+    def Ir3_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._Ir3_prof
+
+    @Ir3_prof.setter
+    def Ir3_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._Ir3_prof = val
+        elif isinstance(val, np.ndarray):
+            self._Ir3_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a Ir3_prof')
+
+    @property
     def Ii_prof(self) -> Profile:
         """
         Cost profile
@@ -107,6 +233,57 @@ class Load(LoadParent):
             self._Ii_prof.set(arr=val)
         else:
             raise Exception(str(type(val)) + 'not supported to be set into a Ii_prof')
+
+    @property
+    def Ii1_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._Ii1_prof
+
+    @Ii1_prof.setter
+    def Ii1_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._Ii1_prof = val
+        elif isinstance(val, np.ndarray):
+            self._Ii1_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a Ii1_prof')
+
+    @property
+    def Ii2_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._Ii2_prof
+
+    @Ii2_prof.setter
+    def Ii2_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._Ii2_prof = val
+        elif isinstance(val, np.ndarray):
+            self._Ii2_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a Ii2_prof')
+
+    @property
+    def Ii3_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._Ii3_prof
+
+    @Ii3_prof.setter
+    def Ii3_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._Ii3_prof = val
+        elif isinstance(val, np.ndarray):
+            self._Ii3_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a Ii3_prof')
 
     @property
     def G_prof(self) -> Profile:
@@ -126,6 +303,57 @@ class Load(LoadParent):
             raise Exception(str(type(val)) + 'not supported to be set into a G_prof')
 
     @property
+    def G1_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._G1_prof
+
+    @G1_prof.setter
+    def G1_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._G1_prof = val
+        elif isinstance(val, np.ndarray):
+            self._G1_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a G1_prof')
+
+    @property
+    def G2_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._G2_prof
+
+    @G2_prof.setter
+    def G2_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._G2_prof = val
+        elif isinstance(val, np.ndarray):
+            self._G2_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a G2_prof')
+
+    @property
+    def G3_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._G3_prof
+
+    @G3_prof.setter
+    def G3_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._G3_prof = val
+        elif isinstance(val, np.ndarray):
+            self._G3_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a G3_prof')
+
+    @property
     def B_prof(self) -> Profile:
         """
         Cost profile
@@ -141,6 +369,57 @@ class Load(LoadParent):
             self._B_prof.set(arr=val)
         else:
             raise Exception(str(type(val)) + 'not supported to be set into a B_prof')
+
+    @property
+    def B1_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._B1_prof
+
+    @B1_prof.setter
+    def B1_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._B1_prof = val
+        elif isinstance(val, np.ndarray):
+            self._B1_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a B1_prof')
+
+    @property
+    def B2_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._B2_prof
+
+    @B2_prof.setter
+    def B2_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._B2_prof = val
+        elif isinstance(val, np.ndarray):
+            self._B2_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a B2_prof')
+
+    @property
+    def B3_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._B3_prof
+
+    @B3_prof.setter
+    def B3_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._B3_prof = val
+        elif isinstance(val, np.ndarray):
+            self._B3_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a B3_prof')
 
     def plot_profiles(self, time=None, show_fig=True):
         """
--- src/GridCalEngine/Devices/Parents/load_parent.py
+++ src/GridCalEngine/Devices/Parents/load_parent.py
@@ -28,7 +28,13 @@ class LoadParent(InjectionParent):
                  cn: Union[ConnectivityNode, None],
                  active: bool,
                  P: float,
+                 P1: float,
+                 P2: float,
+                 P3: float,
                  Q: float,
+                 Q1: float,
+                 Q2: float,
+                 Q3: float,
                  Cost: float,
                  mttf: float,
                  mttr: float,
@@ -45,7 +51,13 @@ class LoadParent(InjectionParent):
         :param cn: connectivity node
         :param active:active state
         :param P: active power (MW)
+        :param P1: phase 1 active power (MW)
+        :param P2: phase 2 active power (MW)
+        :param P3: phase 3 active power (MW)
         :param Q: reactive power (MVAr)
+        :param Q1: phase 1 reactive power (MVAr)
+        :param Q2: phase 2 reactive power (MVAr)
+        :param Q3: phase 3 reactive power (MVAr)
         :param Cost: cost associated with various actions (dispatch or shedding)
         :param mttf: mean time to failure (h)
         :param mttr: mean time to recovery (h)
@@ -73,11 +85,35 @@ class LoadParent(InjectionParent):
         self.P = float(P)
         self._P_prof = Profile(default_value=self.P, data_type=float)
 
+        self.P1 = float(P1)
+        self._P1_prof = Profile(default_value=self.P1, data_type=float)
+
+        self.P2 = float(P2)
+        self._P2_prof = Profile(default_value=self.P2, data_type=float)
+
+        self.P3 = float(P3)
+        self._P3_prof = Profile(default_value=self.P3, data_type=float)
+
         self.Q = float(Q)
         self._Q_prof = Profile(default_value=self.Q, data_type=float)
 
+        self.Q1 = float(Q1)
+        self._Q1_prof = Profile(default_value=self.Q1, data_type=float)
+
+        self.Q2 = float(Q2)
+        self._Q2_prof = Profile(default_value=self.Q2, data_type=float)
+
+        self.Q3 = float(Q3)
+        self._Q3_prof = Profile(default_value=self.Q3, data_type=float)
+
         self.register(key='P', units='MW', tpe=float, definition='Active power', profile_name='P_prof')
+        self.register(key='P1', units='MW', tpe=float, definition='Phase 1 active power', profile_name='P1_prof')
+        self.register(key='P2', units='MW', tpe=float, definition='Phase 2 active power', profile_name='P2_prof')
+        self.register(key='P3', units='MW', tpe=float, definition='Phase 3 active power', profile_name='P3_prof')
         self.register(key='Q', units='MVAr', tpe=float, definition='Reactive power', profile_name='Q_prof')
+        self.register(key='Q1', units='MVAr', tpe=float, definition='Phase 1 reactive power', profile_name='Q1_prof')
+        self.register(key='Q2', units='MVAr', tpe=float, definition='Phase 2 reactive power', profile_name='Q2_prof')
+        self.register(key='Q3', units='MVAr', tpe=float, definition='Phase 3 reactive power', profile_name='Q3_prof')
 
     @property
     def P_prof(self) -> Profile:
@@ -97,6 +133,57 @@ class LoadParent(InjectionParent):
             raise Exception(str(type(val)) + 'not supported to be set into a pofile')
 
     @property
+    def P1_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._P1_prof
+
+    @P1_prof.setter
+    def P1_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._P1_prof = val
+        elif isinstance(val, np.ndarray):
+            self._P1_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a pofile')
+
+    @property
+    def P2_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._P2_prof
+
+    @P2_prof.setter
+    def P2_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._P2_prof = val
+        elif isinstance(val, np.ndarray):
+            self._P2_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a pofile')
+
+    @property
+    def P3_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._P3_prof
+
+    @P3_prof.setter
+    def P3_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._P3_prof = val
+        elif isinstance(val, np.ndarray):
+            self._P3_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a pofile')
+
+    @property
     def Q_prof(self) -> Profile:
         """
         Cost profile
@@ -112,6 +199,57 @@ class LoadParent(InjectionParent):
             self._Q_prof.set(arr=val)
         else:
             raise Exception(str(type(val)) + 'not supported to be set into a Q_prof')
+
+    @property
+    def Q1_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._Q1_prof
+
+    @Q1_prof.setter
+    def Q1_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._Q1_prof = val
+        elif isinstance(val, np.ndarray):
+            self._Q1_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a Q1_prof')
+
+    @property
+    def Q2_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._Q2_prof
+
+    @Q2_prof.setter
+    def Q2_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._Q2_prof = val
+        elif isinstance(val, np.ndarray):
+            self._Q2_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a Q2_prof')
+
+    @property
+    def Q3_prof(self) -> Profile:
+        """
+        Cost profile
+        :return: Profile
+        """
+        return self._Q3_prof
+
+    @Q3_prof.setter
+    def Q3_prof(self, val: Union[Profile, np.ndarray]):
+        if isinstance(val, Profile):
+            self._Q3_prof = val
+        elif isinstance(val, np.ndarray):
+            self._Q3_prof.set(arr=val)
+        else:
+            raise Exception(str(type(val)) + 'not supported to be set into a Q3_prof')
 
     def get_S(self) -> complex:
         """
