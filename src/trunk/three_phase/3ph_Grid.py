5f326281d6d53866da15edab1a92ff12df07b743
Alex Blanco <ablanco@eroots.tech>
2025-05-02 13:45:50 +0200

    Three-phase test grid

--- src/trunk/three_phase/3ph_Grid.py
+++ src/trunk/three_phase/3ph_Grid.py
@@ -1,4 +1,6 @@
 import GridCalEngine.api as gce
+from GridCalEngine.Compilers.circuit_to_data import compile_numerical_circuit_at
 
-my_grid = gce.open_file("3ph_Grid.gridcal")
+my_grid = gce.open_file("src/trunk/three_phase/3ph_Grid.gridcal")
+nc = compile_numerical_circuit_at(circuit=my_grid, fill_three_phase=True)
 print()
