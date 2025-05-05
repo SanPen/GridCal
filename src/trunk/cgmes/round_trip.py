23dd29d87f01ec4939b4fb7f379f091bebd4efb8
santi <santiago.penate.vera@gmail.com>
2025-05-05 10:26:21 +0100

    Fixed the round trip

--- src/trunk/cgmes/round_trip.py
+++ src/trunk/cgmes/round_trip.py
@@ -18,7 +18,8 @@ import GridCalEngine.api as gce
 
 import_path = "/home/santi/Escritorio/Redes/CGMES/wrong_round_trip_example/DACF_20250216_00_IGM_35.raw"
 export_fname = "/home/santi/Escritorio/Redes/CGMES/wrong_round_trip_example/DACF_20250216_00_IGM_35_cgmes_from_gridcal.zip"
-boundary_set = "/home/santi/Escritorio/Redes/CGMES/wrong_round_trip_example/20241201T0000Z__ENTSOE_DB.zip"
+boundary_set = "/home/santi/Escritorio/Redes/CGMES/wrong_round_trip_example/20241201T0000Z__ENTSOE_BD.zip"
+logger_result_file = "/home/santi/Escritorio/Redes/CGMES/wrong_round_trip_example/gridcal_comparison_logger.xlsx"
 
 # Open -----------------------------------------------------------------------------------------------------------------
 print("Processing raw file ...")
@@ -44,14 +45,12 @@ pf_session_data = DriverToSave(name="powerflow results",
 
 # Export ---------------------------------------------------------------------------------------------------------------
 print("Exporting ...")
-options = FileSavingOptions(cgmes_boundary_set=boundary_set)
-options.sessions_data.append(pf_session_data)
-
-raw_export = FileSave(circuit=circuit,
-                      file_name=export_fname,
-                      options=options)
-
-raw_export.save_raw()
+# save the grid in CGMES mode
+gce.save_cgmes_file(grid=circuit,
+                    filename=export_fname,
+                    cgmes_boundary_set_path=boundary_set,
+                    cgmes_version=CGMESVersions.v2_4_15,
+                    pf_results=pf_res_1)
 
 # Round trip -----------------------------------------------------------------------------------------------------------
 print("Opening the saved cgmes file ...")
@@ -63,6 +62,7 @@ pf_res_2 = gce.power_flow(circuit_2, pf_options)
 ok, logger = circuit.compare_circuits(circuit_2)
 if not ok:
     logger.print()
+    logger.to_xlsx(logger_result_file)
 
 # ok, logger = nc_1.compare(nc_2=nc_2, tol=1e-6)
 # !!! ------------------------------------------------------------
@@ -73,23 +73,23 @@ if not ok:
 # !!! ------------------------------------------------------------
 
 # Comparing ------------------------------------------------------------------------------------------------------------
-print("Comparing ...")
-# Compare Y and S arrays
-ADM_1 = nc_1.get_admittance_matrices().Ybus.toarray()
-ADM_2 = nc_2.get_admittance_matrices().Ybus.toarray()
-adm_ok = np.allclose(ADM_1, ADM_2, atol=1e-5)
-if adm_ok:
-    print("\nAdmitance Ybus matrices are the same!")
-else:
-    print("\nAdmittance Ybus matrices are NOT the same!")
-    print(ADM_1)
-    print(ADM_2)
-
-# Compare power flow voltages
-pf_ok = np.allclose(np.abs(pf_res_1.voltage), np.abs(pf_res_2.voltage), atol=1e-5)
-if pf_ok:
-    print("\nPower flow results are the same!")
-else:
-    print("\nPower flow results are NOT the same!")
-    print(np.abs(pf_res_1.voltage))
-    print(np.abs(pf_res_2.voltage))
+# print("Comparing ...")
+# # Compare Y and S arrays
+# ADM_1 = nc_1.get_admittance_matrices().Ybus.toarray()
+# ADM_2 = nc_2.get_admittance_matrices().Ybus.toarray()
+# adm_ok = np.allclose(ADM_1, ADM_2, atol=1e-5)
+# if adm_ok:
+#     print("\nAdmitance Ybus matrices are the same!")
+# else:
+#     print("\nAdmittance Ybus matrices are NOT the same!")
+#     print(ADM_1)
+#     print(ADM_2)
+#
+# # Compare power flow voltages
+# pf_ok = np.allclose(np.abs(pf_res_1.voltage), np.abs(pf_res_2.voltage), atol=1e-5)
+# if pf_ok:
+#     print("\nPower flow results are the same!")
+# else:
+#     print("\nPower flow results are NOT the same!")
+#     print(np.abs(pf_res_1.voltage))
+#     print(np.abs(pf_res_2.voltage))
