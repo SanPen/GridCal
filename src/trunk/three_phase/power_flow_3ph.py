a6f2498e602c29b8057cf73b71332943df1f8e39
josepfanals <jfanals13@gmail.com>
2025-05-09 16:47:35 +0200

    Debugging stuff

--- src/trunk/three_phase/power_flow_3ph.py
+++ src/trunk/three_phase/power_flow_3ph.py
@@ -13,7 +13,7 @@ def power_flow(grid):
     Y0 = nc.get_admittance_injections_pu()
     Qmax, Qmin = nc.get_reactive_power_limits()
 
-    options = gce.PowerFlowOptions(tolerance=1e-10)
+    options = gce.PowerFlowOptions(tolerance=1e-10, max_iter=1000)
 
     problem = PfBasicFormulation(V0=V0, S0=S0, I0=I0, Y0=Y0, Qmin=Qmin, Qmax=Qmax, nc=nc, options=options)
 
@@ -33,20 +33,28 @@ def power_flow_3ph(grid):
     S0 = nc.get_power_injections_pu()
     Qmax, Qmin = nc.get_reactive_power_limits()
 
-    options = gce.PowerFlowOptions(tolerance=1e-10)
+    options = gce.PowerFlowOptions(tolerance=1e-10, max_iter=1000)
 
-    problem = PfBasicFormulation3Ph(V0=V0, S0=S0, Qmin=Qmin, Qmax=Qmax, nc=nc, options=options)
+    problem = PfBasicFormulation3Ph(V0=V0, S0=S0, Qmin=Qmin*100, Qmax=Qmax*100, nc=nc, options=options)
 
     print('Ybus = \n', problem.Ybus.toarray())
     print('S0 = \n', problem.S0)
     print('I0 = \n', problem.I0)
     print('V0 = \n', problem.V)
 
-    res = newton_raphson_fx(problem=problem)
+    res = newton_raphson_fx(problem=problem, trust=0.1, max_iter=10000)
+
+    Ibus = problem.Ybus.dot(res.V)
+    print('Ibus = \n', Ibus)
+
+    Sbuss = res.V * np.conj(Ibus)
+    print('Sbuss = \n', Sbuss)
 
     return res
 
-grid = gce.open_file("ThreePhase_Test.gridcal")
+# grid = gce.open_file("src/trunk/three_phase/ThreePhase_Test.gridcal")
+# grid = gce.open_file("src/trunk/three_phase/ThreePhase_Test_v2.gridcal")
+grid = gce.open_file("src/trunk/three_phase/ThreePhase_Test_v3.gridcal")
 
 res_1 = power_flow(grid)
 res_3ph = power_flow_3ph(grid)
@@ -60,3 +68,4 @@ print("Converged: ", res_3ph.converged)
 print("Iter: ", res_3ph.iterations)
 print("Vm:\n", np.abs(res_3ph.V))
 print("Va:\n", np.angle(res_3ph.V)*180/np.pi)
+
