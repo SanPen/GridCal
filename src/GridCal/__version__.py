e29030c4ea1d6ccb66fbdb1a630d809ffcc209ae
Santiago Peñate Vera <santiago.penate.vera@gmail.com>
2025-06-04 14:13:33 +0100

    5.3.52

--- src/GridCal/__version__.py
+++ src/GridCal/__version__.py
@@ -6,7 +6,7 @@ import datetime
 _current_year_ = datetime.datetime.now().year
 
 # remember to keep a three-number version!!!
-__GridCal_VERSION__ = "5.3.51"
+__GridCal_VERSION__ = "5.3.52"
 
 url = 'https://github.com/SanPen/GridCal'
 
