b330d0cb287378f4c0e51a78fcee95728486a59e
Santiago Peñate Vera <santiago.penate.vera@gmail.com>
2025-07-09 14:20:41 +0100

    small grid reduction improvement

--- src/GridCalEngine/Topology/grid_reduction.py
+++ src/GridCalEngine/Topology/grid_reduction.py
@@ -10,25 +10,30 @@ import GridCalEngine.Devices as dev
 from GridCalEngine.Devices.multi_circuit import MultiCircuit
 from GridCalEngine.basic_structures import IntVec
 from GridCalEngine.Simulations.PowerFlow.power_flow_results import PowerFlowResults
-from GridCalEngine.Simulations.PowerFlow.power_flow_ts_results import PowerFlowTimeSeriesResults
 
 
 def ward_reduction(grid: MultiCircuit,
                    reduction_bus_indices: IntVec,
-                   pf_res: PowerFlowResults,
-                   pf_ts_res: PowerFlowTimeSeriesResults | None = None,
-                   add_power_loads: bool = True):
+                   pf_res: PowerFlowResults | None = None,
+                   add_power_loads: bool = True,
+                   use_linear: bool = False):
     """
     In-place Grid reduction using the Ward equivalent model
     :param grid: MultiCircuit
     :param reduction_bus_indices: Bus indices of the buses to delete
     :param pf_res: PowerFlowResults
-    :param pf_ts_res: PowerFlowTimeSeriesResults
     :param add_power_loads: If true Ward currents are converted to loads, else currents are added instead
+    :param use_linear: if true, the admittance matrix is used and no voltages are required
     """
     nc = compile_numerical_circuit_at(grid, t_idx=None)
 
-    adm = nc.get_admittance_matrices()
+    if use_linear:
+        indices = nc.get_simulation_indices()
+        adm = nc.get_linear_admittance_matrices(indices)
+        Ybus = adm.Bbus
+    else:
+        adm = nc.get_admittance_matrices()
+        Ybus = adm.Ybus
 
     # external bus indices (to reduce)
     e_buses = reduction_bus_indices
@@ -37,32 +42,58 @@ def ward_reduction(grid: MultiCircuit,
     i_buses = np.arange(nc.nbus)
     np.delete(i_buses, e_buses)
 
-    YII = adm.Ybus[np.ix_(i_buses, i_buses)]
-    YIE = adm.Ybus[np.ix_(i_buses, e_buses)]
-    YEI = adm.Ybus[np.ix_(e_buses, i_buses)]
-    YEE = adm.Ybus[np.ix_(e_buses, e_buses)]
+    YII = Ybus[np.ix_(i_buses, i_buses)]
+    YIE = Ybus[np.ix_(i_buses, e_buses)]
+    YEI = Ybus[np.ix_(e_buses, i_buses)]
+    YEE = Ybus[np.ix_(e_buses, e_buses)]
     YEE_fact = factorized(YEE)
 
     # Ward Y
     Yred = YII - (YIE @ YEE_fact(YEI))
 
-    # Compute the external currents
-    I = np.conj(pf_res.Sbus / pf_res.voltage)
-    Ii = I[i_buses]  # internal buses current
-    Ie = I[e_buses]  # external buses current
+    if use_linear:
+        # Compute the external currents
+        I = nc.get_power_injections_pu().real
+        Ii = I[i_buses]  # internal buses current
+        Ie = I[e_buses]  # external buses current
 
-    # compute the equivalent internal currents
-    Ieq = Ii - (YIE @ YEE_fact(Ie))
+        # compute the equivalent internal currents
+        Ieq = Ii - (YIE @ YEE_fact(Ie))
 
-    # Convert the currents to power
-    Seq = pf_res.voltage[i_buses] * np.conj(Ieq)
+        # Convert the currents to power
+        Seq = Ieq
+    else:
+        # Compute the external currents
+        I = np.conj(pf_res.Sbus / pf_res.voltage)
+        Ii = I[i_buses]  # internal buses current
+        Ie = I[e_buses]  # external buses current
+
+        # compute the equivalent internal currents
+        Ieq = Ii - (YIE @ YEE_fact(Ie))
+
+        # Convert the currents to power
+        Seq = pf_res.voltage[i_buses] * np.conj(Ieq)
 
     # Add loads
     for i in i_buses:
-        load = dev.Load(name=f"Ward equivalent load {i}", P=Seq[i].real, Q=Seq[i].imag)
-        load.comment = "Added because of a Ward reduction of the grid"
-        bus = grid.buses[i]
-        grid.add_load(bus=bus, api_obj=load)
+        if add_power_loads:
+            # add power values
+            P = Seq[i].real
+            Q = Seq[i].imag
+            if P != 0 and Q != 0:
+                load = dev.Load(name=f"Ward equivalent load {i}", P=P, Q=Q)
+                load.comment = "Added because of a Ward reduction of the grid"
+                bus = grid.buses[i]
+                grid.add_load(bus=bus, api_obj=load)
+        else:
+            # add current values
+            Ire = Ieq[i].real
+            Iim = Ieq[i].imag
+            if Ire != 0 and Iim != 0:
+                load = dev.Load(name=f"Ward equivalent load {i}", Ir=Ire, Ii=Iim)
+                load.comment = "Added because of a Ward reduction of the grid"
+                bus = grid.buses[i]
+                grid.add_load(bus=bus, api_obj=load)
 
     # Delete the external buses
     to_be_deleted = [grid.buses[e] for e in e_buses]
