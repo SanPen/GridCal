9f80c8f5795d9bc1ee3512634fa58eb67e24d651
Alex Soler <asoler@eroots.tech>
2025-07-16 19:10:07 +0200

    Substation type configurations updates v4

--- examples/substations_types.py
+++ examples/substations_types.py
@@ -1,7 +1,6 @@
 from GridCalEngine.api import *
 from GridCalEngine.Topology import substation_wizards
 
-
 print('Creating grid...')
 
 # declare a circuit object
@@ -12,14 +11,11 @@ grid.add_country(country)
 
 # subs_vic = substation_wizards.simple_bar('Vic', grid, 2, 1, 220, 41.956664, 2.282089, country=country)
 
-subs_centelles = substation_wizards.double_bar(name='Centelles', grid=grid, n_lines=2, n_trafos=2, v_nom=220,
-                                               lat=41.797790, lon=2.219917, country=country, include_disconnectors=False)
-
+subs_centelles = substation_wizards.breaker_and_a_half(name='Centelles', grid=grid, n_lines=5, v_nom=220,
+                                                       lat=41.797790, lon=2.219917, country=country,
+                                                       include_disconnectors=False)
 
 print()
 
 print('Saving grid...')
 save_file(grid, 'Test_substations_types_Alex.gridcal')
-
-
-
