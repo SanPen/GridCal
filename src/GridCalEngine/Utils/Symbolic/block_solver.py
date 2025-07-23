e1b26b530f1408d59aba07bb3104739e1edce35e
Santiago Peñate Vera <santiago.penate.vera@gmail.com>
2025-07-23 17:20:10 +0100

    Removed duplicated of the _psi_tc function

--- src/GridCalEngine/Utils/Symbolic/block_solver.py
+++ src/GridCalEngine/Utils/Symbolic/block_solver.py
@@ -173,7 +173,7 @@ class BlockSolver:
         self._algebraic_eqs: List[Expr] = list()
         self._state_vars: List[Var] = list()
         self._state_eqs: List[Expr] = list()
-        self._parameters: List[Var] = list()
+        self._parameters: List[Const] = list()
 
         for b in self.block_system.get_all_blocks():
             self._algebraic_vars.extend(b.algebraic_vars)
@@ -699,7 +699,16 @@ class BlockSolver:
         return self._algebraic_eqs, self._state_eqs
 
     def build_params_matrix(self, n_steps: int, params0: np.ndarray, events_list: RmsEvents) -> csr_matrix:
-        events_matrix = np.zeros((n_steps, len(params0)))
+        """
+
+        :param n_steps:
+        :param params0:
+        :param events_list:
+        :return:
+        """
+        # TODO: reconsider this algorithm entirely,
+        #  this all looks like it can be built from the rows, cols, values info
+
         diff_params_matrix = np.zeros((n_steps, len(params0)))
         params_matrix_current = params0.copy()
 
@@ -707,15 +716,13 @@ class BlockSolver:
         rows, cols, values = events_list.build_triplets_list()
 
         # build diff sparse matrix
-        for i, row in enumerate(events_matrix):
-            if i in rows:
-                positions = np.where(rows == i)
-                for position in positions:
-                    time_step = i
+        for time_step in range(n_steps):
+            if time_step in rows:  # TODO: very expensive
+                for position in np.where(rows == time_step)[0]:
                     prop_idx = self.uid2idx_params[cols[position][0].uid]
                     value = values[position]
                     diff_val = value - params_matrix_current[prop_idx]
-                    diff_params_matrix[time_step][prop_idx] += diff_val
+                    diff_params_matrix[time_step, prop_idx] += diff_val
                     params_matrix_current[prop_idx] = value
 
         # make params matrix sparse
@@ -747,15 +754,18 @@ class BlockSolver:
         :param newton_max_iter:
         :return: 1D time array, 2D array of simulated variables
         """
-
-        params_matrix = self.build_params_matrix(int(np.ceil((t_end - t0) / h)), params0, events_list)
         if method == "euler":
             return self._simulate_fixed(t0, t_end, h, x0, params0, stepper="euler")
         if method == "rk4":
             return self._simulate_fixed(t0, t_end, h, x0, params0, stepper="rk4")
         if method == "implicit_euler":
+
+            params_matrix = self.build_params_matrix(n_steps=int(np.ceil((t_end - t0) / h)),
+                                                     params0=params0,
+                                                     events_list=events_list)
+
             return self._simulate_implicit_euler(
-                t0, t_end, h, x0, params0, params_matrix,
+                t0=t0, t_end=t_end, h=h, x0=x0, params0=params0, diff_params_matrix=params_matrix,
                 tol=newton_tol, max_iter=newton_max_iter,
             )
         raise ValueError(f"Unknown method '{method}'")
@@ -793,7 +803,11 @@ class BlockSolver:
             t[i + 1] = tn + h
         return t, y
 
-    def _simulate_implicit_euler(self, t0, t_end, h, x0, params0: np.ndarray, diff_params_matrix, tol=1e-8,
+    def _simulate_implicit_euler(self, t0: float, t_end: float, h: float,
+                                 x0: np.ndarray,
+                                 params0: np.ndarray,
+                                 diff_params_matrix: csr_matrix,  # TODO: Not sure if a CSR is the best thing here
+                                 tol=1e-8,
                                  max_iter=1000):
         """
         :param t0:
@@ -809,11 +823,10 @@ class BlockSolver:
         t = np.empty(steps + 1)
         y = np.empty((steps + 1, self._n_vars))
         params_current = params0
-        diff_params_matrix = diff_params_matrix
         t[0] = t0
         y[0] = x0.copy()
         for step_idx in range(steps):
-            params_current += diff_params_matrix[step_idx, :].toarray().ravel()
+            params_current += diff_params_matrix[step_idx, :].toarray().ravel()  # TODO think of a better way
             xn = y[step_idx]
             x_new = xn.copy()  # initial guess
             converged = False
