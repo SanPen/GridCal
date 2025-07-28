02b7e546ce4366a87431e919736498535b2195ea
Santiago Peñate Vera <santiago.penate.vera@gmail.com>
2025-07-28 11:28:55 +0100

    fixed miss initialization for shift_key profiles

--- src/GridCalEngine/Devices/profile.py
+++ src/GridCalEngine/Devices/profile.py
@@ -245,7 +245,6 @@ class Profile:
         except ValueError:
             self._sparse_array = SparseArray(data_type=self.dtype, default_value=self._sparse_array.default_value)
 
-
         if map_data is None:
             self._sparse_array.create(size=size, default_value=default_value)
         else:
@@ -388,7 +387,7 @@ class Profile:
             if self._dense_array is None:
                 # WTF, initialize sparse
                 self._is_sparse = True
-                self._sparse_array = SparseArray(data_type=self.dtype, default_value = self.default_value)
+                self._sparse_array = SparseArray(data_type=self.dtype, default_value=self.default_value)
                 print("Initializing sparse when querying, this signals a mis initialization")
                 return self.default_value
             else:
@@ -561,7 +560,6 @@ class Profile:
             if not self._is_sparse:
                 if self._dense_array is not None:
                     np.nan_to_num(self._dense_array, nan=default_value)  # this is supposed to happen in-place
-
 
     def copy(self):
         """
