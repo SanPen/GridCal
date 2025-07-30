Building the 13 Node Test Feeder in OpenDSS and corrected the negative sign of the susceptance B in the definition of impedance loads.

src/trunk/unbalanced_pf/IEEE 13-bus Test Feeder Case OpenDSS.py
@@ -133,15 +133,88 @@ y_607 = np.array([
 """
 Loads
 """
-load_634 = gce.Load(P1=0.160,
-                    Q1=0.110,
-                    P2=0.120,
-                    Q2=0.090,
-                    P3=0.120,
-                    Q3=0.090)
-load_634.conn = ShuntConnectionType.GroundedStar
-grid.add_load(bus=bus_634, api_obj=load_634)
+# Three-phase Star Power Load (13 Node Test Feeder)
+# load_634 = gce.Load(P1=0.160,
+#                     Q1=0.110,
+#                     P2=0.120,
+#                     Q2=0.090,
+#                     P3=0.120,
+#                     Q3=0.090)
+# load_634.conn = ShuntConnectionType.GroundedStar
+# grid.add_load(bus=bus_634, api_obj=load_634)
 
+# Three-phase Delta Power Load (13 Node Test Feeder)
+# load_634 = gce.Load(P1=0.160,
+#                     Q1=0.110,
+#                     P2=0.120,
+#                     Q2=0.090,
+#                     P3=0.120,
+#                     Q3=0.090)
+# load_634.conn = ShuntConnectionType.Delta
+# grid.add_load(bus=bus_634, api_obj=load_634)
+
+# Two-phase Delta Impedance Load (13 Node Test Feeder)
+# load_634 = gce.Load(G1=0.0,
+#                     B1=0.0,
+#                     G2=0.0,
+#                     B2=0.0,
+#                     G3=0.160,
+#                     B3=-0.110)
+# load_634.conn = ShuntConnectionType.Delta
+# grid.add_load(bus=bus_634, api_obj=load_634)
+
+# Two-phase Delta Current Load (13 Node Test Feeder)
+# load_634 = gce.Load(Ir1=0.160,
+#                     Ii1=0.110,
+#                     Ir2=0.0,
+#                     Ii2=0.0,
+#                     Ir3=0.0,
+#                     Ii3=0.0)
+# load_634.conn = ShuntConnectionType.Delta
+# grid.add_load(bus=bus_634, api_obj=load_634)
+
+# Two-phase Delta Power Load (Validated with OpenDSS)
+# load_634 = gce.Load(P1=0.0,
+#                     Q1=0.0,
+#                     P2=0.0,
+#                     Q2=0.0,
+#                     P3=0.160,
+#                     Q3=0.110)
+# load_634.conn = ShuntConnectionType.Delta
+# grid.add_load(bus=bus_634, api_obj=load_634)
+
+# Single-phase Star Impedance Load (13 Node Test Feeder)
+# load_634 = gce.Load(G1=0.160,
+#                     B1=-0.110,
+#                     G2=0.0,
+#                     B2=0.0,
+#                     G3=0.0,
+#                     B3=0.0)
+# load_634.conn = ShuntConnectionType.GroundedStar
+# grid.add_load(bus=bus_634, api_obj=load_634)
+
+# Single-phase Star Current Load (13 Node Test Feeder)
+# load_634 = gce.Load(Ir1=0.160,
+#                     Ii1=0.110,
+#                     Ir2=0.0,
+#                     Ii2=0.0,
+#                     Ir3=0.0,
+#                     Ii3=0.0)
+# load_634.conn = ShuntConnectionType.GroundedStar
+# grid.add_load(bus=bus_634, api_obj=load_634)
+
+# Single-phase Star Power Load (13 Node Test Feeder)
+# load_634 = gce.Load(P1=0.160,
+#                     Q1=0.110,
+#                     P2=0.0,
+#                     Q2=0.0,
+#                     P3=0.0,
+#                     Q3=0.0)
+# load_634.conn = ShuntConnectionType.GroundedStar
+# grid.add_load(bus=bus_634, api_obj=load_634)
+"""
+Loads
+"""
 load_645 = gce.Load(P1=0.0,
                     Q1=0.0,
                     P2=0.170,
@@ -154,14 +227,14 @@ grid.add_load(bus=bus_645, api_obj=load_645)
 load_646 = gce.Load(G1=0.0,
                     B1=0.0,
                     G2=0.230,
-                    B2=-0.132,
+                    B2=0.132,
                     G3=0.0,
                     B3=0.0)
 load_646.conn = ShuntConnectionType.Delta
 grid.add_load(bus=bus_646, api_obj=load_646)
 
 load_652 = gce.Load(G1=0.128,
-                    B1=-0.086,
+                    B1=0.086,
                     G2=0.0,
                     B2=0.0,
                     G3=0.0,