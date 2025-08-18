2501abcde3ec97e9a71163aec49b292106e258f4
ankur <ankur.arohi@emsysgrid.de>
2025-08-18 12:48:30 +0200

    WIP

--- src/tests/test_state_estimation_from_pandapower_conversion.py
+++ src/tests/test_state_estimation_from_pandapower_conversion.py
@@ -5,7 +5,6 @@ from GridCalEngine.IO.others.pandapower_parser import Panda2GridCal
 from GridCalEngine.Simulations.StateEstimation.state_stimation_driver import StateEstimation
 
 def test_state_estimation():
-    breakpoint()
     net_wns = pandapower.from_json(
             f"src/tests/data/grids/state-estimation /small_grid_gb_hv_estimate_raw_expected.json"
         )
