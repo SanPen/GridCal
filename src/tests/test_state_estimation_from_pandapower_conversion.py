96e51d2502ef7f47f6ee71bb9cb82cb66f97bf49
ankur <ankur.arohi@emsysgrid.de>
2025-08-20 15:50:16 +0200

    comparing CIM & pp parser

--- src/tests/test_state_estimation_from_pandapower_conversion.py
+++ src/tests/test_state_estimation_from_pandapower_conversion.py
@@ -3,6 +3,8 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 # SPDX-License-Identifier: MPL-2.0
 import os
+
+from GridCalEngine import FileOpen
 from GridCalEngine.IO.others.pandapower_parser import Panda2GridCal, PANDAPOWER_AVAILABLE
 from GridCalEngine.Simulations.StateEstimation.state_stimation_driver import StateEstimation
 import GridCalEngine as gce
@@ -43,6 +45,61 @@ def test_state_estimation_pandapower():
 
         se.logger.print("SE Logger:")
 
+def test_network_objects_consistency():
+    if PANDAPOWER_AVAILABLE:
+        import pandapower
+        # tests/data/grids/state-estimation /small_grid_gb_hv_estimate_raw_expected.json
+        # fname = os.path.join("src", "tests", "data", "grids", "state-estimation", "small_grid_gb_hv_estimate_raw_expected.json")
+        fname_pp = os.path.join("data", "grids", "state-estimation", "small_grid_gb_hv_estimate_raw_expected.json")
+        fname = os.path.join("data", "grids", "state-estimation", "20250605T1315Z_RT_SmallGridTestConfiguration_.zip")
+        net_wns = pandapower.from_json(fname_pp)
+
+        # pandapower.to_pickle(net_wns, "small_grid_gb_hv_estimate_raw_expected.p")
+        file_handler = FileOpen(fname)
+        circuit_cim = file_handler.open()
+
+        g = Panda2GridCal(net_wns)
+        grid = g.get_multicircuit()
+
+        print()
+        g.logger.print("PandaPower conversion logs")
+
+        ok,logger = circuit_cim.compare_circuits(grid)
+        #assert ok
+        # gce.save_file(grid, 'small_grid_gb_hv_estimate_raw_expected.gridcal')
+
+        pf_res = gce.power_flow(grid)
+        print(pf_res.get_bus_df())
+        print(pf_res.get_branch_df())
+
+        se = StateEstimation(circuit=grid)
+        se.run()
+
+        se_res = se.results
+        print(se_res.get_bus_df())
+        print(se_res.get_branch_df())
+
+        print(f"Converged: {se_res.converged}")
+        print(f"Error: {se_res.error}")
+        print(f"Iter: {se_res.iterations}")
+
+        se.logger.print("SE Logger:")
+
+        pf_res_cim = gce.power_flow(circuit_cim)
+        print(pf_res_cim.get_bus_df())
+        print(pf_res_cim.get_branch_df())
+
+        se_cim = StateEstimation(circuit=circuit_cim)
+        se_cim.run()
+
+        se_res_cim = se_cim.results
+        print(se_res_cim.get_bus_df())
+        print(se_res_cim.get_branch_df())
+
+        print(f"Converged: {se_res_cim.converged}")
+        print(f"Error: {se_res_cim.error}")
+        print(f"Iter: {se_res_cim.iterations}")
 
 if __name__ == '__main__':
     test_state_estimation_pandapower()
+    test_network_objects_consistency()
