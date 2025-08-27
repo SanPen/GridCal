731f8404c19fed34191522677c38d5bd89fe4729
PabloDJ <pablool1998@gmail.com>
2025-08-27 14:52:13 +0200

    added differential vars and multilinear models

---
+++ src/trunk/dynamics_tensor/diff_block_testing.py
@@ -0,0 +1,77 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+import sys, os
+file_directory = os.path.dirname(os.path.abspath(__file__))
+up_two_directories = os.path.join(file_directory, '..', '..')
+up_two_directories = os.path.abspath(up_two_directories)
+sys.path.insert(0,up_two_directories)
+print(up_two_directories)
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+
+# from GridCalEngine.Utils.Symbolic.events import Events, Event
+from GridCalEngine.Devices.Dynamic.events import RmsEvents, RmsEvent
+from GridCalEngine.Utils.Symbolic.symbolic import Const, Var, cos, sin
+from GridCalEngine.Utils.Symbolic.block import Block
+from GridCalEngine.Utils.MultiLinear.multilinearize import *
+from GridCalEngine.Utils.MultiLinear.differential_var import DiffVar, LagVar
+from GridCalEngine.Utils.MultiLinear.diff_blocksolver import DiffBlock, DiffBlockSolver
+from GridCalEngine.Utils.Symbolic.block_solver import BlockSolver
+import GridCalEngine.api as gce
+
+w = Var('w')
+x = Var('x')
+y = Var('y')
+z = Var("z")
+
+dt = Const(0.001)
+#dt = Var('dt')
+dx = DiffVar.get_or_create('dx', base_var = x)
+dy = DiffVar.get_or_create('dy', base_var = y)
+dx2 = DiffVar.get_or_create('dx2', base_var = dx)
+dy2 = DiffVar.get_or_create('dy2', base_var = dy)
+dx3 = DiffVar.get_or_create('dx3', base_var = dx2)
+
+if dx2.uid == dx3.uid:
+    print("They have the same uid given that they have the same base var")
+
+lag1 = LagVar.get_or_create('lag1', base_var= x, lag =1)
+lag2 = LagVar.get_or_create('lag2', base_var= x, lag =2)
+dx_approx, b = dx.approximation_expr(dt)
+dy_approx, b = dy.approximation_expr(dt)
+
+block1 = DiffBlock(
+    algebraic_eqs=[
+        dx + y,
+        dy - x + z,
+    ],
+    algebraic_vars=[x,y],
+    diff_vars=[dx, dy, dx2, dy2],
+)
+
+block2 = Block(
+    state_eqs=[
+        dx
+    ],
+    state_vars=[w],
+    algebraic_eqs=[
+        z - 2
+    ],
+    algebraic_vars=[z],
+    parameters=[]
+)
+
+big_block = DiffBlock(
+    children=[block1, block2],
+    in_vars=[]
+)
+
+slv = DiffBlockSolver(big_block)
+print(sys)
