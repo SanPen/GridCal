ff3b1ad5f11f068ac3abce1921f4f460061f1c33
Santiago Peñate Vera <santiago.penate.vera@gmail.com>
2025-08-29 13:38:22 +0100

    Fixed some issues

--- src/VeraGridServer/__version__.py
+++ src/VeraGridServer/__version__.py
@@ -6,7 +6,7 @@ import datetime
 _current_year_ = datetime.datetime.now().year
 
 # remember to keep a three-number version!!!
-__VeraGridServer_VERSION__ = "5.4.0"
+__VeraGridServer_VERSION__ = "5.4.1"
 
 url = 'https://github.com/SanPen/VeraGrid'
 
